248ed2b7c33ae4e559f780ef2d1da2b246a12bd4
Joost De Cock <joost@decock.org>
2018-07-10 19:05:55 +0200

    ♻️ Refactor: Switched to TypeScript for module

--- packages/models/index.js
+++ packages/models/index.js
@@ -1,94 +1,74 @@
-const manSize34 = {
-  bicepsCircumference:   335,
-  centerBackNeckToWaist: 489,
-  chestCircumference:    849,
-  hipsCircumference:     722,
-  naturalWaistToHip:     100,
-  neckCircumference:     366,
-  shoulderSlope:          43,
-  shoulderToShoulder:    419,
-  shoulderToWrist:       670,
-  wristCircumference:    175
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.manSize34 = {
+    bicepsCircumference: 335,
+    centerBackNeckToWaist: 489,
+    chestCircumference: 849,
+    hipsCircumference: 722,
+    naturalWaistToHip: 100,
+    neckCircumference: 366,
+    shoulderSlope: 43,
+    shoulderToShoulder: 419,
+    shoulderToWrist: 670,
+    wristCircumference: 175
 };
-
-const manSize36 = {
-  bicepsCircumference:   290,
-  centerBackNeckToWaist: 492,
-  chestCircumference:    907,
-  hipsCircumference:     780,
-  naturalWaistToHip:     105,
-  neckCircumference:     378,
-  shoulderSlope:          46,
-  shoulderToShoulder:    431,
-  shoulderToWrist:       675,
-  wristCircumference:    180
+exports.manSize36 = {
+    bicepsCircumference: 290,
+    centerBackNeckToWaist: 492,
+    chestCircumference: 907,
+    hipsCircumference: 780,
+    naturalWaistToHip: 105,
+    neckCircumference: 378,
+    shoulderSlope: 46,
+    shoulderToShoulder: 431,
+    shoulderToWrist: 675,
+    wristCircumference: 180
 };
-
-const manSize38 = {
-  bicepsCircumference:   305,
-  centerBackNeckToWaist: 495,
-  chestCircumference:    965,
-  hipsCircumference:     838,
-  naturalWaistToHip:     110,
-  neckCircumference:     391,
-  shoulderSlope:          49,
-  shoulderToShoulder:    444,
-  shoulderToWrist:       680,
-  wristCircumference:    185
+exports.manSize38 = {
+    bicepsCircumference: 305,
+    centerBackNeckToWaist: 495,
+    chestCircumference: 965,
+    hipsCircumference: 838,
+    naturalWaistToHip: 110,
+    neckCircumference: 391,
+    shoulderSlope: 49,
+    shoulderToShoulder: 444,
+    shoulderToWrist: 680,
+    wristCircumference: 185
 };
-
-const manSize40 = {
-  bicepsCircumference:   320,
-  centerBackNeckToWaist: 498,
-  chestCircumference:   1023,
-  hipsCircumference:     896,
-  naturalWaistToHip:     115,
-  neckCircumference:     404,
-  shoulderSlope:          52,
-  shoulderToShoulder:    457,
-  shoulderToWrist:       685,
-  wristCircumference:    190
+exports.manSize40 = {
+    bicepsCircumference: 320,
+    centerBackNeckToWaist: 498,
+    chestCircumference: 1023,
+    hipsCircumference: 896,
+    naturalWaistToHip: 115,
+    neckCircumference: 404,
+    shoulderSlope: 52,
+    shoulderToShoulder: 457,
+    shoulderToWrist: 685,
+    wristCircumference: 190
 };
-
-const manSize42 = {
-  bicepsCircumference:   335,
-  centerBackNeckToWaist: 501,
-  chestCircumference:   1081,
-  hipsCircumference:     895,
-  naturalWaistToHip:     120,
-  neckCircumference:     416,
-  shoulderSlope:          55,
-  shoulderToShoulder:    470,
-  shoulderToWrist:       690,
-  wristCircumference:    195
+exports.manSize42 = {
+    bicepsCircumference: 335,
+    centerBackNeckToWaist: 501,
+    chestCircumference: 1081,
+    hipsCircumference: 895,
+    naturalWaistToHip: 120,
+    neckCircumference: 416,
+    shoulderSlope: 55,
+    shoulderToShoulder: 470,
+    shoulderToWrist: 690,
+    wristCircumference: 195
 };
-
-const manSize44 = {
-  bicepsCircumference:   350,
-  centerBackNeckToWaist: 505,
-  chestCircumference:   1139,
-  hipsCircumference:    1012,
-  naturalWaistToHip:     125,
-  neckCircumference:     429,
-  shoulderSlope:          58,
-  shoulderToShoulder:    483,
-  shoulderToWrist:       700,
-  wristCircumference:    200
+exports.manSize44 = {
+    bicepsCircumference: 350,
+    centerBackNeckToWaist: 505,
+    chestCircumference: 1139,
+    hipsCircumference: 1012,
+    naturalWaistToHip: 125,
+    neckCircumference: 429,
+    shoulderSlope: 58,
+    shoulderToShoulder: 483,
+    shoulderToWrist: 700,
+    wristCircumference: 200
 };
-
-const men = {
-  manSize34,
-  manSize36,
-  manSize38,
-  manSize40,
-  manSize42,
-  manSize44
-}
-
-module.exports.manSize34 = manSize34;
-module.exports.manSize36 = manSize36;
-module.exports.manSize38 = manSize38;
-module.exports.manSize40 = manSize40;
-module.exports.manSize42 = manSize42;
-module.exports.manSize44 = manSize44;
-module.exports.men = men;
