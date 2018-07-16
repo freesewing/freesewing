7b4514acd85c044d01bd912612ace0128e53ebab
Joost De Cock <joost@decock.org>
2018-07-16 20:09:12 +0200

    :construction: Working map proxy constructor function for pointlist

--- lib/part.ts
+++ lib/part.ts
@@ -2,29 +2,24 @@ import { Point } from './point'
 import { Path } from './path'
 import { Snippet } from './snippet'
 import { Attributes } from './attributes'
-import { PointList } from './pointlist'
 
-var pointProxy: any = {
-  id: 'test',
-  get: function(points, key: string, proxy: ProxyHandler<Map<any, any>>): Point {
-    console.log(`getting ${key}`);
+function PointProxy(id: string) {
+  this.id = id;
+  this.get =  function(points, key: string, proxy: ProxyHandler<Map<any, any>>): Point {
     return points.get(key);
-  },
-  set: function(points: Map<string, Point>, key: string, point: Point, proxy: ProxyHandler<Map<any, any>>) {
-    console.log(arguments);
-    console.log(`Args above. Setting ${key} to`, point);
+  };
+  this.set =  function(points: Map<string, Point>, key: string, point: Point, proxy: ProxyHandler<Map<any, any>>) {
     point.attributes.add('data-key', key);
     point.attributes.add('data-part', this.id);
     points.set(key, point);
     return true;
-  }
+  };
 };
-
 
 export class Part {
   id: string;
   render: boolean;
-  points = new Proxy(new Map(), pointProxy);
+  points: any;
   paths: { [index: string]: Path; } = {};
   snippets: { [index: string]: Snippet; } = {};
   attributes = new Attributes();
@@ -33,7 +28,7 @@ export class Part {
   constructor(id: string) {
     this.id = id;
     this.render = (id.substr(0,1) === '_') ? false : true;
-    this.points = new Proxy(new Map(), pointProxy);
+    this.points = new Proxy(new Map(), new PointProxy(id));
     this.points.origin = new Point(0,0);
 
     return this;
