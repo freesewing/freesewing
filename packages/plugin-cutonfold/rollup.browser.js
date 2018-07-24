a9071dbc07b8cd4191db20d09ef1fe39c23fa72b
joostdecock <joost@decock.org>
2018-07-24 09:10:09 +0000

    construction: Switched to Rollup from webpack

---
+++ packages/plugin-cutonfold/rollup.browser.js
@@ -0,0 +1,26 @@
+import { terser } from "rollup-plugin-terser";
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/cutonfold.min.js",
+    format: "iife",
+    name: "freesewing_plugin_cutonfold",
+    banner: `/**\n * Freesewing\n * (c) ${new Date().getFullYear()} Joost De Cock\n * @license MIT\n */`
+  },
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    json(),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**"
+    }),
+    terser()
+  ]
+};
--- packages/plugin-cutonfold/src/index.js
+++ packages/plugin-cutonfold/src/index.js
@@ -1,28 +1,30 @@
-//const VERSION = require('../package.json').version;
+import { version } from "../package.json";
 
 export default {
   hooks: {
     preRenderSvg: function(next) {
-      // Without this, our custom attribute won't be valid
-      this.attributes.add("xmlns:freesewing-plugins", "http://freesewing.org/namespaces/freesewing-plugins");
-      this.attributes.add("freesewing-plugins:macro-cutonfold", 'fixme');
+      this.attributes.add("freesewing:plugin-cutonfold", version);
       next();
     }
-  }
-, macros: {
+  },
+  macros: {
     cutonfold: function(next, so) {
       let points = this.points;
       points.cofStart = so.from.shiftTowards(so.to, 30);
       points.cofEnd = so.to.shiftTowards(so.from, 30);
-      points.cofVia1 = so.from.shiftTowards(so.to, 50).rotate(90,points.cofStart);
-      points.cofVia2 = so.to.shiftTowards(so.from, 50).rotate(-90,points.cofEnd);
+      points.cofVia1 = so.from
+        .shiftTowards(so.to, 50)
+        .rotate(90, points.cofStart);
+      points.cofVia2 = so.to
+        .shiftTowards(so.from, 50)
+        .rotate(-90, points.cofEnd);
       this.paths.cof = new this.path()
         .move(points.cofStart)
         .line(points.cofVia1)
         .line(points.cofVia2)
         .line(points.cofEnd)
-        .attr('class', 'cut-on-fold');
+        .attr("class", "cut-on-fold");
       next();
     }
   }
-}
+};
