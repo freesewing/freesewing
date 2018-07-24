84ae6d5c5d0e66beb94137350779e5f35be3a1ab
joostdecock <joost@decock.org>
2018-07-24 14:06:50 +0000

    Merge branch 'master' of github.com:freesewing-plugins/macro-cutonfold

--- packages/plugin-cutonfold/src/index.js
+++ packages/plugin-cutonfold/src/index.js
@@ -1,29 +1,37 @@
 import meta from "../package.json";
+import markers from "./lib/markers";
+import style from "./lib/style";
 
 export default {
   hooks: {
     preRenderSvg: function(next) {
       this.attributes.add("freesewing:plugin-cutonfold", meta.version);
+      this.defs += markers;
+      this.style += style;
       next();
     }
   },
   macros: {
     cutonfold: function(next, so) {
       let points = this.points;
-      points.cofStart = so.from.shiftTowards(so.to, 30);
-      points.cofEnd = so.to.shiftTowards(so.from, 30);
-      points.cofVia1 = so.from
-        .shiftTowards(so.to, 50)
-        .rotate(90, points.cofStart);
-      points.cofVia2 = so.to
+      points.cutonfoldFrom = so.to.shiftTowards(so.from, 30);
+      points.cutonfoldTo = so.from.shiftTowards(so.to, 30);
+      points.cutonfoldVia1 = so.to
         .shiftTowards(so.from, 50)
-        .rotate(-90, points.cofEnd);
-      this.paths.cof = new this.path()
-        .move(points.cofStart)
-        .line(points.cofVia1)
-        .line(points.cofVia2)
-        .line(points.cofEnd)
-        .attr("class", "cut-on-fold");
+        .rotate(-90, points.cutonfoldFrom);
+      points.cutonfoldVia2 = so.from
+        .shiftTowards(so.to, 50)
+        .rotate(90, points.cutonfoldTo);
+      let text = so.grainline ? "cutOnFoldAndGrainline" : "cutOnFold";
+      this.paths.cutonfold = new this.path()
+        .move(points.cutonfoldFrom)
+        .line(points.cutonfoldVia1)
+        .line(points.cutonfoldVia2)
+        .line(points.cutonfoldTo)
+        .attr("class", "cutonfold")
+        .attr("data-text", text)
+        .attr("data-text-class", "cutonfold")
+        .attr("data-text-dy", -2);
       next();
     }
   }
---
+++ packages/plugin-cutonfold/src/lib/style.js
@@ -0,0 +1,1 @@
+export default "path.cutonfold{stroke:#ff5b77;stroke-width:.6;marker-start:url(#cutonfoldFrom);marker-end:url(#cutonfoldTo)}tspan.cutonfold{fill:#ff5b77;}";
