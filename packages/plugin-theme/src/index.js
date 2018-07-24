2c1514b6f71c29811090ec7cdb3c1c765b318b61
joostdecock <joost@decock.org>
2018-07-24 14:32:47 +0000

    Merge branch 'master' of github.com:joostdecock/theme-default

--- packages/plugin-theme/src/index.js
+++ packages/plugin-theme/src/index.js
@@ -1,17 +1,16 @@
 import header from "./lib/header";
 import style from "./lib/style";
-import markers from "./lib/markers";
 import snippets from "./lib/snippets";
 import logo from "./lib/logo";
-import meta from "../package.json";
+import { version } from "../package.json";
 
 export default {
   hooks: {
     preRenderSvg: function(next) {
       this.header += header;
       this.style += style;
-      this.defs += markers + snippets + logo;
-      this.attributes.add("freesewing:theme-default", meta.version);
+      this.defs += snippets + logo;
+      this.attributes.add("freesewing:plugin-theme", version);
       next();
     }
   }
