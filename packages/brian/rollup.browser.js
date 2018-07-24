9fffa83c0261d7123354e6396870739500df063c
joostdecock <joost@decock.org>
2018-07-24 10:15:37 +0000

    construction: Changes to rollup config

--- packages/brian/rollup.browser.js
+++ packages/brian/rollup.browser.js
@@ -1,19 +1,19 @@
 import { terser } from "rollup-plugin-terser";
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
-import path from 'path';
+import path from "path";
+import meta from "./package.json";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    file: 'dist/brian.min.js',
-    format: 'iife',
-    name: 'freesewing.patterns.brian',
-    banner: `/**\n * Freesewing\n * (c) ${new Date().getFullYear()} Joost De Cock\n * @license MIT\n */`,
+    file: "dist/brian.min.js",
+    format: "iife",
+    name: "freesewing.patterns.brian",
     globals: {
-      freesewing: 'freesewing'
+      freesewing: "freesewing"
     }
   },
   plugins: [
@@ -22,10 +22,20 @@ export default {
     }),
     json(),
     babel({
-      exclude: 'node_modules/**'
+      exclude: "node_modules/**"
     }),
+    terser({
+      output: {
+        preamble: `/**\n * ${meta.name} | v${meta.version}\n * ${
+          meta.description
+        }\n * (c) ${new Date().getFullYear()} ${meta.author}\n * @license ${
+          meta.license
+        }\n */`
+      }
+    })
   ],
   external: [
-    'freesewing', path.resolve('./node_modules/freesewing/dist/freesewing.min.js')
+    "freesewing",
+    path.resolve("./node_modules/freesewing/dist/freesewing.min.js")
   ]
 };
--- packages/brian/rollup.node.js
+++ packages/brian/rollup.node.js
@@ -1,22 +1,28 @@
-import filesize from 'rollup-plugin-filesize'
-import { terser } from "rollup-plugin-terser";
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
+import filesize from "rollup-plugin-filesize";
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import json from "rollup-plugin-json";
+import meta from "./package.json";
 
 export default {
-  input: 'src/index.js'
-, output: {
-    file: 'dist/bundle.js'
-  , format: 'cjs'
-  }
-, plugins: [
+  input: "src/index.js",
+  output: {
+    file: "dist/module.js",
+    format: "cjs",
+    banner: `/**\n * ${meta.name} | v${meta.version}\n * ${
+      meta.description
+    }\n * (c) ${new Date().getFullYear()} ${meta.author}\n * @license ${
+      meta.license
+    }\n */`
+  },
+  plugins: [
     resolve({
       browser: false
-    })
-  , babel({
-      exclude: 'node_modules/**'
-    })
-  , terser()
-  , filesize()
+    }),
+    json(),
+    babel({
+      exclude: "node_modules/**"
+    }),
+    filesize()
   ]
 };
