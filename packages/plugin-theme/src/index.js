dd05c8bce47da7535298dbfd8fe2786c918619d2
joostdecock <joost@decock.org>
2018-07-25 06:56:58 +0000

    construction: Removed unneeded style/snippets/header

--- packages/plugin-theme/src/index.js
+++ packages/plugin-theme/src/index.js
@@ -1,13 +1,10 @@
-import header from "./lib/header";
 import style from "./lib/style";
 import snippets from "./lib/snippets";
-import logo from "./lib/logo";
 import { version } from "../package.json";
 
 export default {
   hooks: {
     preRenderSvg: function(next) {
-      this.header += header;
       this.style += style;
       this.defs += snippets + logo;
       this.attributes.add("freesewing:plugin-theme", version);
