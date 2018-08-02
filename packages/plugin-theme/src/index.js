da818f674dd1989948ddb8a51b5230582252106b
Joost De Cock <joost@decock.org>
2018-08-02 07:57:42 +0200

    sparkles: Paperless grid

--- packages/plugin-theme/src/index.js
+++ packages/plugin-theme/src/index.js
@@ -1,13 +1,40 @@
 import style from "./lib/style";
-import snippets from "./lib/snippets";
+import notch from "./lib/notch";
+import gridMetric from "./lib/grid-metric";
+import gridImperial from "./lib/grid-imperial";
 import { version } from "../package.json";
 
 export default {
   hooks: {
     preRenderSvg: function(next) {
       this.style += style;
-      this.defs += snippets;
+      this.defs += notch;
       this.attributes.add("freesewing:plugin-theme", version);
+      if (this.pattern.settings.paperless) {
+        this.style += "path.gridbox{fill: url(#grid)}";
+        if (this.pattern.settings.units === "imperial")
+          this.defs += gridImperial;
+        else this.defs += gridMetric;
+        for (let key in this.pattern.parts) {
+          let part = this.pattern.parts[key];
+          let anchor = new this.pattern.point(0, 0);
+          if (typeof part.points.gridAnchor !== "undefined")
+            anchor = part.points.gridAnchor;
+          else if (typeof part.points.anchor !== "undefined")
+            anchor = part.points.anchor;
+          this.defs += `<pattern id="${part.id}grid" xlink:href="#grid" x="${
+            anchor.x
+          }" y="${anchor.y}"></pattern>`;
+          part.paths[this.getUid()] = new this.pattern.path()
+            .move(part.topLeft)
+            .line(new this.pattern.point(part.topLeft.x, part.bottomRight.y))
+            .line(part.bottomRight)
+            .line(new this.pattern.point(part.bottomRight.x, part.topLeft.y))
+            .close()
+            .attr("class", "grid")
+            .attr("style", `fill: url(#${part.id}grid)`);
+        }
+      }
       next();
     }
   }
--- packages/plugin-theme/src/lib/snippets.js
+++
@@ -1,1 +0,0 @@
-export default `<g id="notch"><circle cy="0" cx="0" r="1.4" class="fill-note" /><circle cy="0" cx="0" r="2.8" class="note stroke-xl" /></g>`;
