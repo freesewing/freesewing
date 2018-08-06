a76d1780f6e0d67f2b06613b95b997d55bf50e3a
Joost De Cock <joost@decock.org>
2018-08-06 13:58:47 +0200

    sparkles: No longer using part.id and checking part.render

--- packages/plugin-theme/src/index.js
+++ packages/plugin-theme/src/index.js
@@ -17,22 +17,24 @@ export default {
         else this.defs += gridMetric;
         for (let key in this.pattern.parts) {
           let part = this.pattern.parts[key];
-          let anchor = new this.pattern.Point(0, 0);
-          if (typeof part.points.gridAnchor !== "undefined")
-            anchor = part.points.gridAnchor;
-          else if (typeof part.points.anchor !== "undefined")
-            anchor = part.points.anchor;
-          this.defs += `<pattern id="${part.id}grid" xlink:href="#grid" x="${
-            anchor.x
-          }" y="${anchor.y}"></pattern>`;
-          part.paths[part.getUid()] = new this.pattern.Path()
-            .move(part.topLeft)
-            .line(new this.pattern.Point(part.topLeft.x, part.bottomRight.y))
-            .line(part.bottomRight)
-            .line(new this.pattern.Point(part.bottomRight.x, part.topLeft.y))
-            .close()
-            .attr("class", "grid")
-            .attr("style", `fill: url(#${part.id}grid)`);
+          if (part.render) {
+            let anchor = new this.pattern.Point(0, 0);
+            if (typeof part.points.gridAnchor !== "undefined")
+              anchor = part.points.gridAnchor;
+            else if (typeof part.points.anchor !== "undefined")
+              anchor = part.points.anchor;
+            this.defs += `<pattern id="grid_${key}" xlink:href="#grid" x="${
+              anchor.x
+            }" y="${anchor.y}"></pattern>`;
+            part.paths[part.getUid()] = new this.pattern.Path()
+              .move(part.topLeft)
+              .line(new this.pattern.Point(part.topLeft.x, part.bottomRight.y))
+              .line(part.bottomRight)
+              .line(new this.pattern.Point(part.bottomRight.x, part.topLeft.y))
+              .close()
+              .attr("class", "grid")
+              .attr("style", `fill: url(#grid_${key})`);
+          }
         }
       }
       next();
