7657537c3f3a6ccca7ba89df5d54b1864f1cc878
Joost De Cock <joost@decock.org>
2018-08-13 11:25:18 +0200

    :white_check_mark: Updated fixtures for new version

--- tests/fixtures/render.js
+++ tests/fixtures/render.js
@@ -4,7 +4,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="0mm" height="0mm" viewBox="0 0 0 0"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="0mm" height="0mm" viewBox="0 0 0 0"
 >
 <style type="text/css"> <![CDATA[
 
@@ -33,7 +33,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="20mm" height="20mm" viewBox="0 0 20 20"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="20mm" height="20mm" viewBox="0 0 20 20"
 >
 <style type="text/css"> <![CDATA[
 
@@ -67,7 +67,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
 >
 <style type="text/css"> <![CDATA[
 
@@ -102,7 +102,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="20mm" height="20mm" viewBox="0 0 20 20"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="20mm" height="20mm" viewBox="0 0 20 20"
 >
 <style type="text/css"> <![CDATA[
 
@@ -138,7 +138,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
 >
 <style type="text/css"> <![CDATA[
 
@@ -175,7 +175,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
 >
 <style type="text/css"> <![CDATA[
 
@@ -212,7 +212,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="60mm" height="72.45mm" viewBox="0 0 60 72.45"
 >
 <style type="text/css"> <![CDATA[
 
@@ -249,7 +249,7 @@ var render = {
 <!--
 
 --><svg
- xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.6.2" width="20mm" height="20mm" viewBox="0 0 20 20"
+ xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:freesewing="http://freesewing.org/namespaces/freesewing" freesewing="0.7.0" width="20mm" height="20mm" viewBox="0 0 20 20"
 >
 <style type="text/css"> <![CDATA[
 
