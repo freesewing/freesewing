2a2c05598139fd5f379ccc1d75c27e0d334b4a1c
Joost De Cock <joost@decock.org>
2018-08-17 20:24:10 +0200

    :fire: Removed snippet comments

--- src/snippet.js
+++ src/snippet.js
@@ -1,17 +1,24 @@
 import Attributes from "./attributes";
 
-function Snippet(def, anchor, description = "") {
+function Snippet(def, anchor) {
   this.def = def;
   this.anchor = anchor;
-  this.description = description;
   this.attributes = new Attributes();
 
   return this;
 }
 
+/** Adds an attribute. This is here to make this call chainable in assignment */
+Snippet.prototype.attr = function(name, value, overwrite = false) {
+  if (overwrite) this.attributes.set(name, value);
+  else this.attributes.add(name, value);
+
+  return this;
+};
+
 /** Returns a deep copy of this */
 Snippet.prototype.clone = function() {
-  let clone = new Snippet(this.def, this.anchor.clone(), this.description);
+  let clone = new Snippet(this.def, this.anchor.clone());
   clone.attributes = this.attributes.clone();
 
   return clone;
