1bc01381ef4045b0530d5d97e1d7192d6a67d63a
Joost De Cock <joost@decock.org>
2018-08-28 09:14:21 +0200

    sparkles: Added translations for Aaron

--- packages/plugin-i18n/locales/patterns.js
+++ packages/plugin-i18n/locales/patterns.js
@@ -1,10 +1,13 @@
+import aaron from "./patterns/aaron";
 import brian from "./patterns/brian";
 
 export default {
   en: {
+    ...aaron.en,
     ...brian.en,
   },
   nl: {
+    ...aaron.nl,
     ...brian.nl,
   }
 };
--- packages/plugin-i18n/locales/patterns/brian.js
+++ packages/plugin-i18n/locales/patterns/brian.js
@@ -1,12 +1,12 @@
 export default {
   en: {
-    front: 'Front',
-    back: 'Back',
-    sleeve: 'Sleeve'
+    front: "Front",
+    back: "Back",
+    sleeve: "Sleeve"
   },
   nl: {
-    front: 'Voorzijde',
-    back: 'Rug',
-    sleeve: 'Mouw'
+    front: "Voorzijde",
+    back: "Rug",
+    sleeve: "Mouw"
   }
 }
