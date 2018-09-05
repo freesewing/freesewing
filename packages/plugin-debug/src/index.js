6105b184672dfa9ea9810477a575ff0ac2ff2333
Joost De Cock <joost@decock.org>
2018-09-05 16:48:26 +0200

    sparkles: Added ability to format message prefix

--- packages/plugin-debug/src/index.js
+++ packages/plugin-debug/src/index.js
@@ -5,15 +5,26 @@ export default {
   version: version,
   hooks: {
     debug: function(next, d = "", e = "", b = "", u = "", g = "") {
-      console.log(
-        "%cDebug",
-        "color: #dd69dd; font-weight: bold",
-        d,
-        e,
-        b,
-        u,
-        g
+      if(typeof d === 'object' && d.debug === 'custom') {
+        console.log(
+          "%c"+d.text,
+          d.style,
+          e,
+          b,
+          u,
+          g
       );
+      } else {
+        console.log(
+          "%cDebug",
+          "color: #dd69dd; font-weight: bold",
+          d,
+          e,
+          b,
+          u,
+          g
+        );
+      }
       next();
     }
   }
