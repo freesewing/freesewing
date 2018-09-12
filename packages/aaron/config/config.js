48139f5f8c54a0283f4f95bbb256f6828dbbb2c2
Joost De Cock <joost@decock.org>
2018-09-12 17:44:54 +0200

    wrench: Updated configuration syntax

--- packages/aaron/config/config.js
+++ packages/aaron/config/config.js
@@ -23,15 +23,15 @@ export default {
     shoulderSlopeReduction: 0,
 
     // Percentages
-    armholeDrop:            { val:  10, min:  1, max:  75 },
-    backlineBend:           { val:  50, min: 50, max: 100 },
-    chestEase:              { val:   8, min:  0, max:  20 },
-    hipsEase:               { val:   8, min:  0, max:  20 },
-    lengthBonus:            { val:  10, min:-20, max:  60 },
-    necklineBend:           { val: 100, min: 40, max: 100 },
-    necklineDrop:           { val:  20, min: 10, max:  35 },
-    stretchFactor:          { val:   5, min:  0, max:  15 },
-    shoulderStrapWidth:     { val:  15, min: 10, max:  40 },
-    shoulderStrapPlacement: { val:  40, min: 20, max:  80 },
+    armholeDrop:            { pct:  10, min:  1, max:  75 },
+    backlineBend:           { pct:  50, min: 50, max: 100 },
+    chestEase:              { pct:   8, min:  0, max:  20 },
+    hipsEase:               { pct:   8, min:  0, max:  20 },
+    lengthBonus:            { pct:  10, min:-20, max:  60 },
+    necklineBend:           { pct: 100, min: 40, max: 100 },
+    necklineDrop:           { pct:  20, min: 10, max:  35 },
+    stretchFactor:          { pct:   5, min:  0, max:  15 },
+    shoulderStrapWidth:     { pct:  15, min: 10, max:  40 },
+    shoulderStrapPlacement: { pct:  40, min: 20, max:  80 },
   }
 };
