2a0fce1fdc000031dc8234b7d2fefb096776d9b1
Joost De Cock <joost@decock.org>
2018-12-08 17:49:21 +0100

    sparkles: Added check to avoid double-loading plugin

--- packages/plugin-cutonfold/src/index.js
+++ packages/plugin-cutonfold/src/index.js
@@ -6,8 +6,10 @@ export default {
   version: version,
   hooks: {
     preRender: function(svg) {
-      svg.defs += markers;
-      svg.attributes.set("freesewing:plugin-cutonfold", version);
+      if (svg.attributes.get("freesewing:plugin-cutonfold") === false) {
+        svg.attributes.set("freesewing:plugin-cutonfold", version);
+        svg.defs += markers;
+      }
     }
   },
   macros: {
