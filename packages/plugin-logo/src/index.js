5d5e0381ef6fda6ee93be0687ee02fe45886a21a
Joost De Cock <joost@decock.org>
2018-12-08 15:04:29 +0100

    boom: Updated to new plugin structure in freesewing v0.20

--- packages/plugin-logo/src/index.js
+++ packages/plugin-logo/src/index.js
@@ -5,11 +5,10 @@ export default {
   name: name,
   version: version,
   hooks: {
-    preRender: function(next) {
-      this.attributes.add("freesewing:plugin-logo", version);
-      this.defs += logo;
-      this.style += "path.logo{stroke:none;fill:#000;}";
-      next();
+    preRender: function(svg) {
+      svg.attributes.add("freesewing:plugin-logo", version);
+      svg.defs += logo;
+      svg.style += "path.logo{stroke:none;fill:#000;}";
     }
   }
 };
