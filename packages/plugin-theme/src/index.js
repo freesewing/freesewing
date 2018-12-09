3d4957dc6a4891c962ddaba602ed074da6dae54b
Joost De Cock <joost@decock.org>
2018-12-09 10:46:18 +0100

    sparkles: Fixed theme plugin after merge fiasco

--- packages/plugin-theme/src/index.js
+++ packages/plugin-theme/src/index.js
@@ -1,4 +1,6 @@
-import baseCss from "./lib/base.css.js";
+import draftCss from "./lib/draft.css.js";
+import sampleCss from "./lib/sample.css.js";
+import paperlessCss from "./lib/paperless.css.js";
 import notch from "./lib/notch";
 import gridMetric from "./lib/grid-metric";
 import gridImperial from "./lib/grid-imperial";
@@ -8,37 +10,41 @@ export default {
   name: name,
   version: version,
   hooks: {
-    preRender: function(next) {
-      this.style += baseCss;
-      this.defs += notch;
-      this.attributes.add("freesewing:plugin-theme", version);
-      if (this.pattern.settings.paperless) {
-        if (this.pattern.settings.units === "imperial")
-          this.defs += gridImperial;
-        else this.defs += gridMetric;
-        for (let key in this.pattern.parts) {
-          let part = this.pattern.parts[key];
-          if (part.render && this.pattern.needs(key)) {
-            let anchor = new this.pattern.Point(0, 0);
-            if (typeof part.points.gridAnchor !== "undefined")
-              anchor = part.points.gridAnchor;
-            else if (typeof part.points.anchor !== "undefined")
-              anchor = part.points.anchor;
-            this.defs += `<pattern id="grid_${key}" `;
-            this.defs += `xlink:href="#grid" x="${anchor.x}" y="${anchor.y}">`;
-            this.defs += "</pattern>";
-            part.paths[part.getId()] = new this.pattern.Path()
-              .move(part.topLeft)
-              .line(new this.pattern.Point(part.topLeft.x, part.bottomRight.y))
-              .line(part.bottomRight)
-              .line(new this.pattern.Point(part.bottomRight.x, part.topLeft.y))
-              .close()
-              .attr("class", "grid")
-              .attr("style", `fill: url(#grid_${key})`);
+    preRender: function(svg) {
+      if (svg.attributes.get("freesewing:plugin-theme") === false) {
+        svg.defs += notch;
+        svg.pattern.is === "sample"
+          ? (svg.style += sampleCss)
+          : (svg.style += draftCss);
+        if (svg.pattern.settings.paperless) {
+          svg.style += paperlessCss;
+          svg.pattern.settings.units === "imperial"
+            ? (svg.defs += gridImperial)
+            : (svg.defs += gridMetric);
+          for (let key in this.pattern.parts) {
+            let part = svg.pattern.parts[key];
+            if (part.render && svg.pattern.needs(key)) {
+              let anchor = new svg.pattern.Point(0, 0);
+              if (typeof part.points.gridAnchor !== "undefined")
+                anchor = part.points.gridAnchor;
+              else if (typeof part.points.anchor !== "undefined")
+                anchor = part.points.anchor;
+              svg.defs += `<pattern id="grid_${key}" `;
+              svg.defs += `xlink:href="#grid" x="${anchor.x}" y="${anchor.y}">`;
+              svg.defs += "</pattern>";
+              part.paths[part.getId()] = new svg.pattern.Path()
+                .move(part.topLeft)
+                .line(new svg.pattern.Point(part.topLeft.x, part.bottomRight.y))
+                .line(part.bottomRight)
+                .line(new svg.pattern.Point(part.bottomRight.x, part.topLeft.y))
+                .close()
+                .attr("class", "grid")
+                .attr("style", `fill: url(#grid_${key})`);
+            }
           }
         }
+        svg.attributes.add("freesewing:plugin-theme", version);
       }
-      next();
     }
   }
 };
