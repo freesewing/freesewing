72d5be91ad997136c440df1c21130be403e75aa2
Joost De Cock <joost@decock.org>
2018-12-10 14:41:39 +0100

    art: Rewrote index file to avoid use of JS classes

--- packages/aaron/src/index.js
+++ packages/aaron/src/index.js
@@ -1,37 +1,47 @@
 import freesewing from "freesewing";
 import Brian from "@freesewing/brian";
 import pluginBundle from "@freesewing/plugin-bundle";
-
 import config from "../config/config";
 import { version } from "../package.json";
-
 import back from "./back";
 import front from "./front";
 
-export default class Aaron extends freesewing.Pattern {
-
-  constructor(settings = false) {
-    super({ version: version, ...config }).with(pluginBundle);
-    if(settings !== false) {
-      for (let key of Object.keys(settings)) this.settings[key] = settings[key];
+const Aaron = function(settings = false) {
+  // Make this a new freesewing.Pattern instance
+  freesewing.Pattern.call(this, { version: version, ...config });
+  // Load plugins
+  this.with(pluginBundle);
+  // Inject settings passed to the constructor
+  if (settings !== false) {
+    for (let key of Object.keys(settings)) {
+      this.settings[key] = settings[key];
     }
-    this.Part.prototype.context = this.context;
   }
 
-  _draft() {
-    this.parts.base = this.draftBase(this.createPart());
-    if (!this.needs("base", true)) this.parts.base.render = false;
-    if (this.needs(["back", "front"])) {
-      this.parts.front = this.draftFront(this.createPart().copy(this.parts.base));
-    }
-    if (this.needs(["back"])) {
-      this.parts.back = this.draftBack(this.createPart().copy(this.parts.front));
-    }
+  return this;
+};
 
-    return this;
-  };
+// Setup inheritance
+Aaron.prototype = Object.create(freesewing.Pattern.prototype);
+Aaron.prototype.constructor = Aaron;
 
-  draftBase(part)  { return new Brian(this.settings).draftBase(part); }
-  draftFront(part) { return front.draft(part); }
-  draftBack(part)  { return back.draft(part); }
-}
+// Draft method
+Aaron.prototype._draft = function() {
+  this.parts.base = this.draftBase(this.createPart());
+  if (!this.needs("base", true)) this.parts.base.render = false;
+  if (this.needs(["back", "front"])) {
+    this.parts.front = this.draftFront(this.createPart().copy(this.parts.base));
+  }
+  if (this.needs(["back"])) {
+    this.parts.back = this.draftBack(this.createPart().copy(this.parts.front));
+  }
+
+  return this;
+};
+
+// Per-part draft methods
+Aaron.prototype.draftBase = function (part)  { return new Brian(this.settings).draftBase(part); }
+Aaron.prototype.draftFront = function (part) { return front.draft(part); }
+Aaron.prototype.draftBack = function (part)  { return back.draft(part); }
+
+export default Aaron;
