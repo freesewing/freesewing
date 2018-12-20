f12b94d6d27baac98d6dc821da75d45992055cc5
Joost De Cock <joost@decock.org>
2018-12-20 16:56:41 +0100

    fire: Removed unused files

--- packages/svgtopdf/src/controllers/converter.js
+++
@@ -1,8 +0,0 @@
-function ConverterController() { }
-
-ConverterController.prototype.convert = function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-  return res.send({convert: "this"});
-}
-
-export default ConverterController;
