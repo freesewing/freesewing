090a9b6d5a6ba38a38471b2f78e8ccefe184eacf
Joost De Cock <joost@decock.org>
2018-12-28 15:40:46 +0100

    bug: Fixed incorrect macro name and lacking this keyword

--- packages/plugin-banner/src/index.js
+++ packages/plugin-banner/src/index.js
@@ -10,15 +10,15 @@ export default {
     }
   },
   macros: {
-    round: function(so) {
+    banner: function(so) {
       let defaults = { text: [], dy: -1, spaces: 8, repeat: 25 };
       so = { ...defaults, ...so };
-      paths[so.path]
+      this.paths[so.path]
         .attr("data-text-dy", so.dy)
         .attr("data-text-class", "center");
       for(let i=0; i<so.repeat;i++) {
-        for(let t of so.text) paths[so.path].attr("data-text", t);
-          paths[so.path].attr("&nbsp;".repeat(so.repeat));
+        for(let t of so.text) this.paths[so.path].attr("data-text", t);
+          this.paths[so.path].attr("data-text", "&nbsp;".repeat(so.spaces));
       }
     }
   }
