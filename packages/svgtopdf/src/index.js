42a03f4cfbeaf3c1090e3f87beb46526b84512ce
Joost De Cock <joost@decock.org>
2019-01-05 12:47:06 +0100

    construction: Added api endpoint for frontend tiling

--- packages/svgtopdf/src/index.js
+++ packages/svgtopdf/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import fs from "fs";
 import formidable from "formidable";
 import shellExec from "shell-exec";
 
@@ -19,6 +20,65 @@ app.post("/", async (req, res) => {
       sizes.indexOf(fields.size) === -1
       ) return res.sendFile(path.resolve(__dirname+'/form.html'));
     let upload = files.svg.path;
+    let cmd;
+    if(fields.size === "full") { // Do not tile
+      let target = `/tmp/pattern.${fields.format}`;
+      cmd = `/usr/bin/inkscape --export-${fields.format}=${target} ${upload}`;
+      shellExec(cmd).then(() => {
+        return res.sendFile(target);
+      });
+    } else { // Do tile
+      let untiled = "/tmp/untiled.ps";
+      let tiled = "/tmp/tiled.ps";
+      cmd = `/usr/bin/inkscape --export-ps=${untiled} ${upload}`;
+      shellExec(cmd).then(() => {
+        cmd = `/usr/local/bin/tile -a -m${fields.size} -s1 -t"On-demand tiler" ${untiled} > ${tiled}`;
+        shellExec(cmd).then(() => {
+          if(fields.format === "ps") return res.sendFile(tiled);
+          cmd = `/usr/bin/ps2pdf14 ${tiled} ${tiled}.pdf`;
+          shellExec(cmd).then(() => {
+            return res.sendFile(tiled+'.pdf');
+          });
+        });
+      });
+    }
+  });
+});
+
+app.post("/api", async (req, res) => {
+  if(
+    typeof req.svg === "undefined" ||
+    typeof req.format === "undefined" ||
+    typeof req.size === "undefined")
+    return res.sendStatus(400);
+
+  // Save svg to disk
+  fs.writeFile("/tmp/draft.svg", req.svg, err => {
+  	if(err) return res.sendStatus(500);
+  	let cmd;
+  	if(req.size === "full") { // Do not tile
+  	  let target = `/tmp/pattern.${req.format}`;
+  	  cmd = `/usr/bin/inkscape --export-${req.format}=${target} /tmp/draft.svg`;
+  	  shellExec(cmd).then(() => {
+  	    return res.sendFile(target);
+  	  });
+  	} else { // Do tile
+    	let untiled = "/tmp/untiled.ps";
+    	let tiled = "/tmp/tiled.ps";
+    	cmd = `/usr/bin/inkscape --export-ps=${untiled} /tmp/draft.svg`;
+    	shellExec(cmd).then(() => {
+    	  cmd = `/usr/local/bin/tile -a -m${req.size} -s1 -t"On-demand tiler" ${untiled} > ${tiled}`;
+    	  shellExec(cmd).then(() => {
+    	    if(req.format === "ps") return res.sendFile(tiled);
+    	    cmd = `/usr/bin/ps2pdf14 ${tiled} ${tiled}.pdf`;
+    	    shellExec(cmd).then(() => {
+    	      return res.sendFile(tiled+'.pdf');
+    	    });
+    	  });
+  		});
+		}
+	});
+});
     let cmd;
     if(fields.size === "full") { // Do not tile
       let target = `/tmp/pattern.${fields.format}`;
