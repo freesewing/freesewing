35f40400e9f788d31ed3454a63861de23346a752
Joost De Cock <joost@decock.org>
2019-01-27 15:04:45 +0100

    construction: Adding source files to NPM package

--- packages/examples/src/point_attr.js
+++ packages/examples/src/point_attr.js
@@ -2,11 +2,10 @@ import { box } from "./shared";
 
 export default part => {
   let { Point, points, Snippet, snippets } = part.shorthand();
-
-  box(part);
+  box(part); // Needed for demo as this part has no paths
 
   points.anchor = new Point(50, 25)
-    .attr("data-text", "Hello world!\nThis is\na line break.")
+    .attr("data-text", "msg")
     .attr("data-text-class", "text-xl center")
     .attr("data-text-lineheight", 10);
 
