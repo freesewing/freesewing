5bae1ab438a03e61560dbedf5fef0758690eed48
Joost De Cock <joost@decock.org>
2019-02-11 07:40:09 +0100

    wrench: Disabled terser plugin to investigate gatsby build issue

--- packages/pattern-bundle/rollup.js
+++ packages/pattern-bundle/rollup.js
@@ -14,12 +14,12 @@ export default {
     json(),
     babel({
       exclude: "node_modules/**"
-    }),
-    terser({
-      output: {
-        preamble: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
-      }
     })
+    //terser({
+    //  output: {
+    //    preamble: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
+    //  }
+    //})
   ],
   external: ["freesewing", "@freesewing/plugin-bundle"],
   output: {
