8c29fe96f01bc2d004a35367eb8ccf1c7c4b8a7e
Joost De Cock <joost@decock.org>
2019-02-16 16:15:54 +0100

    wrench: Swapped terser for babel-minify to sidestep mangling conflicts

--- packages/pattern-bundle/rollup.js
+++ packages/pattern-bundle/rollup.js
@@ -1,27 +1,23 @@
-import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import json from "rollup-plugin-json";
+import minify from "rollup-plugin-babel-minify";
 import path from "path";
 import { name, version, description, author, license } from "./package.json";
 
 export default {
   input: "src/index.js",
   plugins: [
-    resolve({
-      browser: true
-    }),
+    resolve({ browser: true }),
     json(),
-    babel({
-      exclude: "node_modules/**"
-    }),
-    terser({
-      output: {
-        preamble: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
-      }
+    babel({ exclude: "node_modules/**" }),
+    minify({
+      comments: false,
+      sourceMap: true,
+      banner: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
     })
   ],
-  //external: ["freesewing", "@freesewing/plugin-bundle"],
+  external: ["freesewing", "@freesewing/plugin-bundle"],
   output: {
     globals: {
       freesewing: "freesewing",
--- packages/pattern-bundle/src/info/index.js
+++ packages/pattern-bundle/src/info/index.js
@@ -1,21 +1,21 @@
-import aaron from "./aaron";
+//import aaron from "./aaron";
 import brian from "./brian";
-import bruce from "./bruce";
-import cathrin from "./cathrin";
-import hugo from "./hugo";
-import simon from "./simon";
-import sven from "./sven";
-import tamiko from "./tamiko";
-import trayvon from "./trayvon";
+//import bruce from "./bruce";
+//import cathrin from "./cathrin";
+//import hugo from "./hugo";
+//import simon from "./simon";
+//import sven from "./sven";
+//import tamiko from "./tamiko";
+//import trayvon from "./trayvon";
 
 export default {
-  aaron,
-  brian,
-  bruce,
-  cathrin,
-  hugo,
-  simon,
-  sven,
-  tamiko,
-  trayvon
+  //  aaron,
+  brian
+  //  bruce,
+  //  cathrin,
+  //  hugo,
+  //  simon,
+  //  sven,
+  //  tamiko,
+  //  trayvon
 };
