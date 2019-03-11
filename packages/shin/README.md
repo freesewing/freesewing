c75544ba7c98a6322c5b40cd756bd606a84621ce
Joost De Cock <joost@decock.org>
2019-03-11 12:22:17 +0100

    tada: Initial commit

--- packages/shin/README.md
+++ packages/shin/README.md
@@ -1,2 +1,20 @@
-# shin
-A port of the Shin Swimtrunks pattern to JavaScript
+<p align="center">
+  <a title="Go to freesewing.org" href="https://freesewing.org/"><img src="https://freesewing.org/img/logo/black.svg" align="center" width="150px" alt="Freesewing logo"/></a>
+</p>
+<h4 align="center"><em>&nbsp;<a title="Go to freesewing.org" href="https://freesewing.org/">freesewing</a></em>
+<br><sup>a library for made-to-measure sewing patterns</sup>
+</h4>
+
+# freesewing / patterns / shin
+
+Shin is a [freesewing](https://github.com/freesewing/freesewing) pattern
+for swim trunks.
+
+See [freesewing.org/patterns/shin](https://freesewing.org/patterns/huey)
+for more info and examples.
+
+## Install
+
+```
+npm i --save @freesewing/shin
+```
---
+++ packages/shin/config/index.js
@@ -0,0 +1,30 @@
+import { version } from "../package.json";
+
+export default {
+  name: "shin",
+  version: version,
+  measurements: [
+    "hipsCircumference",
+    "upperLegCircumference",
+    "hipsToUpperLeg"
+  ],
+  parts: ["back", "front", "waistband"],
+  options: {
+    // Constants
+    frontFactor: 0.58,
+    legFrontFactor: 0.48,
+    gussetFactor: 0.0714,
+    angle: 10,
+
+    // Millimeters
+    elasticWidth: { mm: 35, min: 15, max: 60 },
+
+    // Percentages
+    stretch: { pct: 20, min: 10, max: 30 },
+    bulge: { pct: 2.5, min: 0, max: 5 },
+    legReduction: { pct: 5, min: 0, max: 10 },
+    lengthBonus: { pct: 0, min: 0, max: 50 },
+    rise: { pct: 0, min: 0, max: 25 },
+    backRise: { pct: 5, min: 0, max: 10 }
+  }
+};
