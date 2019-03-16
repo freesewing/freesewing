59b4f67ba8b96b1d546fb0f4d6ff5ce0ba925016
Joost De Cock <joost@decock.org>
2019-03-16 17:41:53 +0100

    sparkles: Finalized undercollar

--- packages/jaeger/src/undercollar.js
+++ packages/jaeger/src/undercollar.js
@@ -1,8 +1,7 @@
 /*
  * FIXME
  *
- * This collar was ported as-is from the original (PHP) design
- * I did a few years ago. I think it would benefit from a redesign
+ * This collar would benefit from a redesign
  * but I find collar design to be rather tricky business and
  * would love the input from someone with more pattern design
  * experience, or more tailoring exprience.
@@ -11,14 +10,30 @@
 export default function(part) {
   let { sa, snippets, Snippet, utils, store, complete, points, measurements, options, macro, Point, paths, Path } = part.shorthand();
 
+  // Clean up
+  for (let i of Object.keys(paths)) delete paths[i];
+  for (let i of Object.keys(snippets)) delete snippets[i];
+
   paths.seam = new Path()
     .move(points.collarCbTop)
     .curve_(points.collarCbTopCp, points.notchTip)
     .line(points.notch)
     .line(points.collarstandTip)
     ._curve(points.collarstandCbTopCp, points.collarstandCbTop)
+    .line(points.collarCbTop)
     .close()
-    .attr("class", "fabric");
+    .attr("class", "various");
 
+  if (complete) {
+    // Title
+    points.title = points.collarCbTopCp.shiftFractionTowards(points.collarstandCbTopCp, 0.5);
+    macro("title", {
+      at: points.title,
+      nr: 6,
+      title: "underCollar"
+    });
+
+    if (sa) paths.sa = paths.seam.offset(sa).attr("class", "various sa");
+  }
   return part;
 }
