860933ea6f1c3d3d250a1dbbcc9045778e3d564b
Joost De Cock <joost@decock.org>
2019-03-27 08:02:55 +0100

    sparkles: Paperless back

--- packages/carlton/src/back.js
+++ packages/carlton/src/back.js
@@ -58,21 +58,26 @@ export default function(part) {
   for (let i in snippets) delete snippets[i]
 
   // Paths
-  paths.seam = new Path()
+  paths.seam1 = new Path()
     .move(points.cbNeck)
     .line(points.bpStart)
     .line(points.bpTop)
     .line(points.bpBottom)
-    .line(points.dartLeft)
+  paths.dart = new Path()
+    .move(points.dartLeft)
     .curve_(points.dartLeftCp, points.dartTip)
     ._curve(points.dartRightCp, points.dartRight)
-    .line(points.waist)
+  paths.seam2 = new Path()
+    .move(points.waist)
     .curve_(points.waistCp2, points.armhole)
     .curve(points.armholeCp2, points.armholeHollowCp1, points.armholeHollow)
     .curve(points.armholeHollowCp2, points.armholePitchCp1, points.armholePitch)
     .curve(points.armholePitchCp2, points.shoulderCp1, points.shoulder)
     .line(points.neck)
     .curve_(points.neckCp2, points.cbNeck)
+  paths.seam = paths.seam1
+    .join(paths.dart)
+    .join(paths.seam2)
     .close()
     .attr("class", "fabric");
 
@@ -81,7 +86,146 @@ export default function(part) {
     .curve(points.bsCp1, points.bsCp2, points.armhole)
     .attr("class", "canvas lashed");
 
+  paths.triangle = new Path()
+    .move(points.bpTriangleTip)
+    .line(points.bpTriangleEdge)
+    .line(points.bpStart)
+    .attr("class", "dashed");
+
   if (complete) {
+    macro("sprinkle", {
+      snippet: "notch",
+      on: [
+        "armholePitch",
+        "bpTriangleTip"
+      ]
+    });
+
+    macro("grainline", {
+      from: points.cbWaist,
+      to: points.bpStart
+    });
+
+    points.logo = new Point(
+      points.armhole.x * 0.7,
+      points.dartTip.y
+    );
+    snippets.logo = new Snippet("logo", points.logo);
+
+    if (sa) {
+      paths.sa =
+        paths.seam1
+        .line(points.waist)
+        .offset(sa)
+        .join(paths.seam2.offset(sa))
+        .close()
+        .trim()
+        .close()
+        .attr("class", "fabric sa");
+    }
+
+    if (paperless) {
+      macro("hd", {
+        from: points.bpBottom,
+        to: points.cbWaist,
+        y: points.cbWaist.y + 15 + sa
+      });
+      macro("hd", {
+        from: points.cbWaist,
+        to: points.dartLeft,
+        y: points.cbWaist.y + 15 + sa
+      });
+      macro("hd", {
+        from: points.dartLeft,
+        to: points.dartRight,
+        y: points.cbWaist.y + 15 + sa
+      });
+      macro("hd", {
+        from: points.dartRight,
+        to: points.waist,
+        y: points.cbWaist.y + 15 + sa
+      });
+      macro("hd", {
+        from: points.cbWaist,
+        to: points.waist,
+        y: points.cbWaist.y + 30 + sa
+      });
+      macro("hd", {
+        from: points.bpBottom,
+        to: points.waist,
+        y: points.cbWaist.y + 45 + sa
+      });
+      macro("vd", {
+        from: points.waist,
+        to: points.armhole,
+        x: points.armhole.x + 15 + sa
+      });
+      macro("vd", {
+        from: points.armhole,
+        to: points.armholePitch,
+        x: points.armhole.x + 15 + sa
+      });
+      macro("vd", {
+        from: points.armhole,
+        to: points.shoulder,
+        x: points.armhole.x + 30 + sa
+      });
+      macro("vd", {
+        from: points.waist,
+        to: points.shoulder,
+        x: points.armhole.x + 45 + sa
+      });
+      macro("vd", {
+        from: points.dartRight,
+        to: points.dartTip,
+        x: points.dartRight.x + 15
+      });
+      macro("vd", {
+        from: points.bpBottom,
+        to: points.bpTop,
+        x: points.bpTop.x - 15 - sa
+      });
+      macro("vd", {
+        from: points.bpTop,
+        to: points.cbNeck,
+        x: points.bpTop.x - 15 - sa
+      });
+      macro("vd", {
+        from: points.bpBottom,
+        to: points.neck,
+        x: points.bpTop.x - 30 - sa
+      });
+      macro("vd", {
+        from: points.bpStart,
+        to: points.bpTriangleTip,
+        x: points.bpTriangleEdge.x + 15
+      });
+      macro("hd", {
+        from: points.bpStart,
+        to: points.bpTriangleEdge,
+        y: points.bpTriangleEdge.y + 15
+      });
+      macro("hd", {
+        from: points.cbNeck,
+        to: points.neck,
+        y: points.neck.y - 15 - sa
+      });
+      macro("hd", {
+        from: points.cbNeck,
+        to: points.armholePitch,
+        y: points.neck.y - 30 - sa
+      });
+      macro("hd", {
+        from: points.cbNeck,
+        to: points.shoulder,
+        y: points.neck.y - 45 - sa
+      });
+      macro("hd", {
+        from: points.cbNeck,
+        to: points.armhole,
+        y: points.neck.y - 60 - sa
+      });
+    }
   }
 
   return part;
