04d90c5eda8f24156588c5ab3a3bb9136468ee28
Joost De Cock <joost@decock.org>
2019-03-29 07:29:30 +0100

    sparkles: Paperless sleeve parts

--- packages/carlton/src/topsleeve.js
+++ packages/carlton/src/topsleeve.js
@@ -18,6 +18,9 @@ export default function(part) {
   store.set("cuffLength", length);
   store.set("cuffRadius", length/3);
 
+  // Clean up
+  for (let i in paths) delete paths[i];
+
   // Paths
   paths.seam = new Path()
     .move(points.tsLeftEdge)
@@ -35,6 +38,89 @@ export default function(part) {
     .curve(points.frontPitchPointCpBottom, points.tsLeftEdgeCpRight, points.tsLeftEdge)
     .close()
     .attr("class", "fabric");
+
+  if (complete) {
+
+    macro("grainline", {
+      from: points.boxBottom,
+      to: points.top
+    });
+
+    if (sa) {
+      paths.sa = paths.seam.offset(sa).attr("class", "fabric sa");
+    }
+
+    if (paperless) {
+      macro("ld", {
+        from: points.tsWristLeft,
+        to: points.tsWristRight,
+        d: -15
+      });
+      macro("vd", {
+        from: points.tsWristLeft,
+        to: points.tsElbowLeft,
+        x: points.tsLeftEdge.x - sa - 15
+      });
+      macro("vd", {
+        from: points.tsWristLeft,
+        to: points.tsLeftEdge,
+        x: points.tsLeftEdge.x - sa - 30
+      });
+      macro("ld", {
+        from: points.cuffBottomLeft,
+        to: points.tsWristLeft,
+        d: 15 + sa
+      });
+      macro("vd", {
+        from: points.cuffBottomRight,
+        to: points.usWristRight,
+        x: points.usWristRight.x + 15 + sa
+      });
+      macro("vd", {
+        from: points.usWristRight,
+        to: points.elbowRight,
+        x: points.elbowRight.x + 15 + sa
+      });
+      macro("vd", {
+        from: points.usWristRight,
+        to: points.tsRightEdge,
+        x: points.elbowRight.x + 30 + sa
+      });
+      macro("vd", {
+        from: points.usWristRight,
+        to: points.backPitchPoint,
+        x: points.elbowRight.x + 45 + sa
+      });
+      macro("vd", {
+        from: points.usWristRight,
+        to: points.top,
+        x: points.elbowRight.x + 60 + sa
+      });
+      macro("ld", {
+        from: points.tsElbowLeft,
+        to: points.elbowRight
+      });
+      macro("ld", {
+        from: points.tsLeftEdge,
+        to: points.tsRightEdge
+      });
+      macro("hd", {
+        from: points.tsLeftEdge,
+        to: points.top,
+        y: points.top.y - sa - 15
+      });
+      macro("hd", {
+        from: points.tsLeftEdge,
+        to: points.backPitchPoint,
+        y: points.top.y - sa - 30
+      });
+      macro("hd", {
+        from: points.tsLeftEdge,
+        to: points.tsRightEdge,
+        y: points.top.y - sa - 45
+      });
+    }
+  }
 
   return part;
 }
--- packages/carlton/src/undersleeve.js
+++ packages/carlton/src/undersleeve.js
@@ -16,6 +16,9 @@ export default function(part) {
   });
   store.set("underCuffWidth", points.usWristLeft.dist(points.usWristRight));
 
+  // Clean up
+  for (let i in paths) delete paths[i];
+
   // Paths
   paths.seam = new Path()
     .move(points.usLeftEdge)
@@ -32,6 +35,78 @@ export default function(part) {
     .line(points.usLeftEdge)
     .close()
     .attr("class", "fabric");
+
+  if (complete) {
+
+    macro("grainline", {
+      from: points.boxBottom,
+      to: new Point(
+        points.top.x,
+        points.usLeftEdge.y
+      )
+    });
+
+    if (sa) {
+      paths.sa = paths.seam.offset(sa).attr("class", "fabric sa");
+    }
+
+    if (paperless) {
+      macro("ld", {
+        from: points.usWristLeft,
+        to: points.usWristRight,
+        d: -15
+      });
+      macro("vd", {
+        from: points.usWristLeft,
+        to: points.usElbowLeft,
+        x: points.usLeftEdge.x - sa - 15
+      });
+      macro("vd", {
+        from: points.usWristLeft,
+        to: points.usLeftEdge,
+        x: points.usLeftEdge.x - sa - 30
+      });
+      macro("ld", {
+        from: points.cuffBottomLeft,
+        to: points.usWristLeft,
+        d: 15 + sa
+      });
+      macro("vd", {
+        from: points.cuffBottomRight,
+        to: points.usWristRight,
+        x: points.usWristRight.x + 15 + sa
+      });
+      macro("vd", {
+        from: points.usWristRight,
+        to: points.elbowRight,
+        x: points.elbowRight.x + 15 + sa
+      });
+      macro("vd", {
+        from: points.usWristRight,
+        to: points.usTip,
+        x: points.elbowRight.x + 30 + sa
+      });
+      macro("ld", {
+        from: points.usElbowLeft,
+        to: points.elbowRight
+      });
+      macro("ld", {
+        from: points.usLeftEdge,
+        to: points.usRightEdge,
+        d: -15
+      });
+      macro("hd", {
+        from: points.usLeftEdge,
+        to: points.usTip,
+        y: points.usTip.y - sa - 15
+      });
+      macro("vd", {
+        from: points.usLeftEdge,
+        to: points.usTip,
+        x: points.usLeftEdge.x - sa - 15
+      });
+    }
+  }
 
   return part;
 }
