7414e9a7ab3524d8275f0dc5b03af28ca9a084e0
Joost De Cock <joost@decock.org>
2019-04-13 15:10:32 +0200

    wrench: Updated rollup config

--- packages/plugin-banner/src/index.js
+++ packages/plugin-banner/src/index.js
@@ -18,7 +18,7 @@ export default {
         .attr("data-text-class", "center");
       for(let i=0; i<so.repeat;i++) {
         for(let t of so.text) this.paths[so.path].attr("data-text", t);
-          this.paths[so.path].attr("data-text", "&nbsp;".repeat(so.spaces));
+          this.paths[so.path].attr("data-text", "&#160;".repeat(so.spaces));
       }
     }
   }
