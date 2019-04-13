ff3482186256f9db3b662896b52d943242b5238f
Joost De Cock <joost@decock.org>
2019-04-13 15:01:18 +0200

    boom: Updated rollup config, no more browserbuild

--- packages/aaron/rollup.config.js
+++ packages/aaron/rollup.config.js
@@ -7,22 +7,20 @@ import { name, version, description, author, license } from "./package.json";
 
 export default {
   input: "src/index.js",
+  output: {
+    sourcemap: true
+  },
   plugins: [
     resolve({ browser: true }),
     json(),
-    babel({ exclude: "node_modules/**" }),
+    babel({
+      exclude: "node_modules/**",
+      plugins: ["@babel/plugin-proposal-object-rest-spread"]
+    }),
     minify({
       comments: false,
       sourceMap: true,
       banner: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
     })
-  ],
-  external: ["freesewing", "@freesewing/plugin-bundle", "@freesewing/brian"],
-  output: {
-    globals: {
-      freesewing: "freesewing",
-      "@freesewing/plugin-bundle": "freesewing.plugins.bundle",
-      "@freesewing/brian": "freesewing.patterns.brian"
-    }
-  }
+  ]
 };
