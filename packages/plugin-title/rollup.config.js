0afbb3c84d7efe79ce8ef2fe5008d663f736a3d4
Joost De Cock <joost@decock.org>
2019-04-13 15:54:33 +0200

    wrench: Updated rollup config

--- packages/plugin-title/rollup.config.js
+++ packages/plugin-title/rollup.config.js
@@ -1,8 +1,9 @@
-import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import json from "rollup-plugin-json";
-import { version, name, description, author, license } from "./package.json";
+import minify from "rollup-plugin-babel-minify";
+import path from "path";
+import { name, version, description, author, license } from "./package.json";
 
 export default {
   input: "src/index.js",
@@ -10,18 +11,16 @@ export default {
     sourcemap: true
   },
   plugins: [
-    resolve({
-      browser: true
-    }),
+    resolve({ browser: true }),
     json(),
     babel({
       exclude: "node_modules/**",
       plugins: ["@babel/plugin-proposal-object-rest-spread"]
     }),
-    terser({
-      output: {
-        preamble: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
-      }
+    minify({
+      comments: false,
+      sourceMap: true,
+      banner: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
     })
   ]
 };
