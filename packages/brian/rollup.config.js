a98558f8aa7cd1cad7438a53a19f96de561ad4fd
Joost De Cock <joost@decock.org>
2019-04-13 15:59:10 +0200

    wrench: Updated rollup config

--- packages/brian/rollup.config.js
+++ packages/brian/rollup.config.js
@@ -2,25 +2,25 @@ import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import json from "rollup-plugin-json";
 import minify from "rollup-plugin-babel-minify";
+import path from "path";
 import { name, version, description, author, license } from "./package.json";
 
 export default {
   input: "src/index.js",
+  output: {
+    sourcemap: true
+  },
   plugins: [
     resolve({ browser: true }),
     json(),
-    babel({ exclude: "node_modules/**" }),
+    babel({
+      exclude: "node_modules/**",
+      plugins: ["@babel/plugin-proposal-object-rest-spread"]
+    }),
     minify({
       comments: false,
       sourceMap: true,
       banner: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
     })
-  ],
-  external: ["freesewing", "@freesewing/plugin-bundle"],
-  output: {
-    globals: {
-      freesewing: "freesewing",
-      "@freesewing/plugin-bundle": "freesewing.plugins.bundle"
-    }
-  }
+  ]
 };
