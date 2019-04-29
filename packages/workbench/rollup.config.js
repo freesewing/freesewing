7a0dfdce38f9677118dfcc9aaf0971970d3227bf
Joost De Cock <joost@decock.org>
2019-04-29 11:12:41 +0200

    :construction: Reconfigure

--- packages/workbench/rollup.config.js
+++ packages/workbench/rollup.config.js
@@ -1,39 +1,48 @@
 import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
 import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import minify from "rollup-plugin-babel-minify";
+import yaml from "rollup-plugin-yaml";
 import url from "rollup-plugin-url";
+import postcss from "rollup-plugin-postcss";
 import svgr from "@svgr/rollup";
-
-import pkg from "./package.json";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { name, version, description, author, license } from "./package.json";
 
 export default {
   input: "src/index.js",
   output: [
     {
-      file: pkg.main,
+      file: "dist/index.js",
       format: "cjs",
       sourcemap: true
     },
     {
-      file: pkg.module,
+      file: "dist/index.mjs",
       format: "es",
       sourcemap: true
     }
   ],
   plugins: [
-    external(),
+    peerDepsExternal(),
+    resolve({ modulesOnly: true }),
+    url(),
+    commonjs(),
+    json(),
+    yaml(),
+    svgr(),
     postcss({
       modules: true
     }),
-    url(),
-    svgr(),
     babel({
       exclude: "node_modules/**",
-      plugins: ["@babel/external-helpers"]
+      plugins: ["@babel/plugin-proposal-object-rest-spread"]
     }),
-    resolve(),
-    commonjs()
+    minify({
+      comments: false,
+      sourceMap: true,
+      banner: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
+    })
   ]
 };
