b18b117948d8795489d0d86af32df815966a4241
Joost De Cock <joost@decock.org>
2019-05-28 07:44:07 +0200

    :sparkles: Added pattern examples

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -7,11 +7,10 @@ import { name, version, description, author, license } from "./package.json";
 import components from "./src/index.js";
 
 const createConfig = (component, module) => {
-  console.log(component);
   return {
-    input: `./src/${component}/index.js`,
+    input: `./src/${component === "index" ? "" : component + "/"}index.js`,
     output: {
-      file: `dist/${component}/index` + (module ? ".mjs" : ".js"),
+      file: `./${component}/index` + (module ? ".mjs" : ".js"),
       format: module ? "es" : "cjs",
       sourcemap: true
     },
@@ -37,5 +36,5 @@ for (let component of components) {
   config.push(createConfig(component, false));
   config.push(createConfig(component, true));
 }
-console.log(JSON.stringify(config, null, 2));
+
 export default config;
