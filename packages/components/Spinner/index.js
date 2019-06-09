0d827c9d805d8b326fed482c52c0cf9982b87c35
Joost De Cock <joost@decock.org>
2019-06-09 13:43:28 +0200

    :wrench: Reconfigure packages

--- packages/components/Spinner/index.js
+++ packages/components/Spinner/index.js
@@ -1,5 +1,5 @@
 /**
- * @freesewing/components/Spinner | v2.0.0-beta.18
+ * @freesewing/components/Spinner | v2.0.0-beta.19
  * A collection of React components for FreeSewing web UIs
  * (c) 2019 Joost De Cock <joost@decock.org> (https://github.com/joostdecock)
  * @license MIT
