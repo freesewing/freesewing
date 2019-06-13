98ce2210e8eeb4fae1184a7dd9b7f99cb3b4375e
Joost De Cock <joost@decock.org>
2019-06-13 07:59:28 +0200

    :sparkles: Made Robot component take embed option

--- packages/components/src/Robot/index.js
+++ packages/components/src/Robot/index.js
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 import poses from "./poses";
 
 const Robot = props => {
+  console.log("roboprops", props);
   return (
     <svg
       className={props.className}
       xmlns="http://www.w3.org/2000/svg"
-      width={props.size}
-      height={props.size}
+      width={props.embed ? "" : props.size}
+      height={props.embed ? "" : props.size}
       viewBox={props.viewBox}
     >
       <path
@@ -23,7 +24,8 @@ const Robot = props => {
 Robot.propTypes = {
   size: PropTypes.number,
   viewBox: PropTypes.string,
-  pose: PropTypes.string
+  pose: PropTypes.string,
+  embed: PropTypes.bool
 };
 
 Robot.defaultProps = {
