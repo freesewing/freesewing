9e575ca956984e6fd02b5ec5215c1afc1c892452
Joost De Cock <joost@decock.org>
2019-06-23 18:02:33 +0200

    :sparkles: Added required measurements to patter-info

--- packages/pattern-info/src/index.js
+++ packages/pattern-info/src/index.js
@@ -1,6 +1,7 @@
 export { default as options } from "./prebuild/options";
 export { default as optionGroups } from "./prebuild/option-groups";
 export { default as parts } from "./prebuild/parts";
+export { default as measurements } from "./prebuild/measurements";
 export { default as versions } from "./prebuild/versions";
 
 export const list = [
