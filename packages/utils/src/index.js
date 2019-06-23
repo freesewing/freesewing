79b0ff20bafb27f31ec7c7434cf0c86de4200f4a
Joost De Cock <joost@decock.org>
2019-06-23 18:01:22 +0200

    :sparkles: Added tiler and capitalize to utils

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,5 +1,6 @@
 export { default as backend } from "./backend";
 export { default as camelCase } from "./camelCase";
+export { default as capitalize } from "./capitalize";
 export { default as cloneObject } from "./cloneObject";
 export { default as defaultGist } from "./defaultGist";
 export { default as defaultSa } from "./defaultSa";
@@ -13,6 +14,7 @@ export { default as roundMmUp } from "./roundMmUp";
 export { default as sliderStep } from "./sliderStep";
 export { default as smallestImperialStep } from "./smallestImperialStep";
 export { default as storage } from "./storage";
+export { default as tiler } from "./tiler";
 export { default as measurementAsMm } from "./measurementAsMm";
 export { default as validateEmail } from "./validateEmail";
 export { default as validateTld } from "./validateTld";
