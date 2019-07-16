c97c7b564749841094aaa868efd905b5a3588e54
Joost De Cock <joost@decock.org>
2019-07-16 16:18:39 +0200

    :sparkles: Added way to remove grainline from part

--- packages/plugin-grainline/src/index.js
+++ packages/plugin-grainline/src/index.js
@@ -6,14 +6,20 @@ export default {
   version: version,
   hooks: {
     preRender: function(svg) {
-      if(svg.attributes.get('freesewing:plugin-grainline') === false) {
-        svg.attributes.set('freesewing:plugin-grainline', version);
+      if (svg.attributes.get("freesewing:plugin-grainline") === false) {
+        svg.attributes.set("freesewing:plugin-grainline", version);
         svg.defs += markers;
       }
     }
   },
   macros: {
     grainline: function(so) {
+      if (so === false) {
+        delete this.points.grainlineFrom;
+        delete this.points.grainlineTo;
+        delete this.paths.grainline;
+        return true;
+      }
       let points = this.points;
       points.grainlineFrom = so.from.shiftFractionTowards(so.to, 0.05);
       points.grainlineTo = so.to.shiftFractionTowards(so.from, 0.05);
