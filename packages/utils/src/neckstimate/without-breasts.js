1e37f1147c80d83b221d66bea71163e124f8fad5
Joost De Cock <joost@decock.org>
2019-08-24 18:25:05 +0200

    :sparkles: Added everything for Model Graph component

---
+++ packages/utils/src/neckstimate/without-breasts.js
@@ -0,0 +1,94 @@
+export default {
+  ankleCircumference: {
+    slope: 0.15,
+    base: 256
+  },
+  bicepsCircumference: {
+    slope: 0.45,
+    base: 325
+  },
+  centerBackNeckToWaist: {
+    slope: 0.15,
+    base: 453
+  },
+  chestCircumference: {
+    slope: 2.0,
+    base: 1040
+  },
+  headCircumference: {
+    slope: 0.15,
+    base: 579
+  },
+  hipsCircumference: {
+    slope: 2.0,
+    base: 920
+  },
+  hipsToUpperLeg: {
+    slope: 0.1,
+    base: 220
+  },
+  inseam: {
+    slope: 0.2,
+    base: 850
+  },
+  kneeCircumference: {
+    slope: 0.15,
+    base: 410
+  },
+  naturalWaist: {
+    slope: 2.0,
+    base: 880
+  },
+  naturalWaistToFloor: {
+    slope: 0.1,
+    base: 1300
+  },
+  naturalWaistToHip: {
+    slope: 0.05,
+    base: 180
+  },
+  naturalWaistToKnee: {
+    slope: 0.07,
+    base: 580
+  },
+  naturalWaistToSeat: {
+    slope: 0.05,
+    base: 280
+  },
+  neckCircumference: {
+    slope: 1,
+    base: 420
+  },
+  seatCircumference: {
+    slope: 2,
+    base: 1060
+  },
+  seatDepth: {
+    slope: 0.02,
+    base: 300
+  },
+  shoulderSlope: {
+    slope: 0.01,
+    base: 50
+  },
+  shoulderToElbow: {
+    slope: 0.1,
+    base: 345
+  },
+  shoulderToShoulder: {
+    slope: 0.3,
+    base: 460
+  },
+  shoulderToWrist: {
+    slope: 0.15,
+    base: 640
+  },
+  upperLegCircumference: {
+    slope: 0.35,
+    base: 599
+  },
+  wristCircumference: {
+    slope: 0.1,
+    base: 180
+  }
+}
