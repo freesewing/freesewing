e7b36708f89fc66264d08298c517ace35b6874d8
Wouter van Wageningen <wouter.vdub@yahoo.com>
2019-09-17 15:15:33 -0700

    Add files via upload

--- packages/penelope/src/shape.js
+++ packages/penelope/src/shape.js
@@ -18,7 +18,7 @@ function BuildMainShape(part, frontPart) {
   } = part.shorthand();
 
   let skirtLength =
-    measurements.naturalWaistToKnee + options.lengthBonus + options.hem;
+    (measurements.naturalWaistToKnee * (1+ options.lengthBonus)); // + options.hem;
 
   store.set("skirtLength", skirtLength);
 
@@ -34,11 +34,10 @@ function BuildMainShape(part, frontPart) {
     measurements.seatCircumference > waist
       ? measurements.seatCircumference
       : waist;
-  /*let hip =
+  let hip =
     measurements.hipsCircumference > waist
       ? measurements.hipsCircumference
       : waist;
-  */
 
   dartCalc(options, seat, seatEase, waist, waistEase);
 
@@ -76,7 +75,7 @@ function BuildMainShape(part, frontPart) {
   waist += waistEase;
 
   let sideSeam = seat / 4 + sideSeamShift;
-  //let hipSeam = hip / 4 + sideSeamShift;
+  let hipSeam = hip / 4 + sideSeamShift;
 
   points.lWaist = new Point(0, 0);
   points.lLeg = new Point(0, skirtLength);
@@ -212,18 +211,8 @@ function BuildMainShape(part, frontPart) {
       sideSeamLength = sideSeamPath.length();
       sdelta = store.get("sideSeamLength") - sideSeamLength;
     }
-    /*
-    console.log({
-      iteration: iteration,
-      wdelta: wdelta,
-      waistFactor: waistFactor,
-      sdelta: sdelta,
-      sideFactor: sideFactor
-    });
-    */
   } while ((Math.abs(wdelta) > 1 || Math.abs(sdelta) > 1) && iteration++ < 100);
 
-  //paths.waist0 = waistCurveHelper.translate( 0, -10 ).attr('class', 'lining dashed');
   paths.waist1 = waistCurve.translate(0, 10).attr("class", "lining dashed");
 
   if (iteration >= 100) {
@@ -233,9 +222,6 @@ function BuildMainShape(part, frontPart) {
   if (frontPart) {
     sideSeamLength = sideSeamPath.length();
     store.set("sideSeamLength", sideSeamLength);
-    //console.log("Front sideseam length: " + sideSeamLength);
-  } else {
-    //console.log("back sideseam length: " + sideSeamLength);
   }
 
   points.lHem = points.lLeg;
