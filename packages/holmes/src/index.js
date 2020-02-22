7d02c76bc806b1a9a59bf5bad09681d8d28b3579
Joost De Cock <joost@decock.org>
2020-02-22 14:18:13 +0100

    refactor(holmes): Using gore-plugin package, rather than local code

--- packages/holmes/src/index.js
+++ packages/holmes/src/index.js
@@ -1,17 +1,18 @@
 import freesewing from '@freesewing/core'
 import plugins from '@freesewing/plugin-bundle'
-import gore from './plugin-gore'
+import gorePlugin from '@freesewing/plugin-gore'
 import config from '../config'
+
 import draftGore from './gore'
 import draftBrim from './brim'
 import draftEar from './ear'
 
 // Create new design
-const Pattern = new freesewing.Design(config, [plugins,gore])
+const Pattern = new freesewing.Design(config, [plugins, gorePlugin])
 
 // Attach the draft methods to the prototype
-Pattern.prototype.draftGore  = draftGore
-Pattern.prototype.draftBrim  = draftBrim
-Pattern.prototype.draftEar  = draftEar
+Pattern.prototype.draftGore = draftGore
+Pattern.prototype.draftBrim = draftBrim
+Pattern.prototype.draftEar = draftEar
 
 export default Pattern
