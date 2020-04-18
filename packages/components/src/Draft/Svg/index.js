a833496e0484ae7bce091b7f21a860e437aed402
Joost De Cock <joost@decock.org>
2020-04-18 18:14:09 +0200

    chore(components): Renamed Render to Draft
    Note that this was already exported as Draft, and the Draft folder
    was a symlink to Render. Now it's just called Draft.

---
+++ packages/components/src/Draft/Svg/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Svg = (props) => {
+  let attributes = {
+    xmlns: 'http://www.w3.org/2000/svg',
+    'xmlns:svg': 'http://www.w3.org/2000/svg',
+    xmlnsXlink: 'http://www.w3.org/1999/xlink',
+    xmlLang: props.language,
+    viewBox: `0 0 ${props.width} ${props.height}`,
+    className: props.className,
+    style: props.style
+  }
+
+  if (!props.embed) {
+    attributes.width = props.width + 'mm'
+    attributes.height = props.height + 'mm'
+  }
+  if (props.design) attributes.className += ' design'
+
+  return <svg {...attributes}>{props.children}</svg>
+}
+
+Svg.propTypes = {
+  embed: PropTypes.bool,
+  className: PropTypes.string,
+  language: PropTypes.string,
+  design: PropTypes.bool
+}
+
+Svg.defaultProps = {
+  embed: true,
+  design: false,
+  language: 'en',
+  className: 'freesewing draft',
+  style: {}
+}
+
+export default Svg
---
+++ packages/components/src/Draft/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Svg from './Svg'
+import Defs from './Defs'
+import Part from './Part'
+
+const Draft = (props) => (
+  <Svg
+    embed={props.settings.embed}
+    width={props.width}
+    height={props.height}
+    language={props.settings.locale}
+    id={props.settings.idPrefix + 'svg'}
+    design={props.design}
+    style={props.style}
+  >
+    <Defs
+      units={props.settings.units}
+      parts={props.parts}
+      paperless={props.settings.paperless}
+      design={props.design}
+    />
+    <g>
+      {Object.keys(props.parts).map((name) => (
+        <Part
+          part={props.parts[name]}
+          language={props.settings.locale}
+          paperless={props.settings.paperless}
+          units={props.settings.units}
+          key={name}
+          name={name}
+          focus={props.focus}
+          design={props.design}
+          raiseEvent={props.raiseEvent}
+        />
+      ))}
+    </g>
+  </Svg>
+)
+
+Draft.propTypes = {
+  parts: PropTypes.object.isRequired,
+  settings: PropTypes.object.isRequired,
+  design: PropTypes.bool
+}
+
+Draft.defaultProps = {
+  design: false,
+  focus: false,
+  style: {}
+}
+
+export default Draft
