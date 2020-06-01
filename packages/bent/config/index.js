4d5f02b86e67f4ff01eb17bd6885d8894284b56a
Joost De Cock <joost@decock.org>
2020-06-01 13:08:23 +0200

    chore(bent): Ported bent to new measurements. See #416

--- packages/bent/config/index.js
+++ packages/bent/config/index.js
@@ -33,8 +33,8 @@ export default {
   measurements: [
     'bicepsCircumference',
     'chestCircumference',
-    'hpsToHipsBack',
-    'naturalWaistToHip',
+    'hpsToWaistBack',
+    'waistToHips',
     'neckCircumference',
     'shoulderSlope',
     'shoulderToElbow',
