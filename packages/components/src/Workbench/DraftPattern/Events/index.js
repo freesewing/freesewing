981846170deb1609b0b090f2d08d8e506fe30b66
Joost De Cock <joost@decock.org>
2020-07-19 18:40:32 +0200

    chore: More work on events

--- packages/components/src/Workbench/DraftPattern/Events/index.js
+++ packages/components/src/Workbench/DraftPattern/Events/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Event from './event'
 
-const DraftEvents = ({ events, app }) => (
+const DraftEvents = ({ events }) => (
   <div className="draft-events">
     {['error', 'warning', 'info', 'debug'].map((type) => (
       <div className={`events-${type}`}>
         {events[type].map((event, index) => (
-          <Event event={event} app={app} type={type} key={index} />
+          <Event event={event} type={type} key={index} />
         ))}
       </div>
     ))}
