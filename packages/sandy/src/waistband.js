aa5e37e14add90f4a0fab4b9c799d1b591102cf4
Joost De Cock <joost@decock.org>
2020-08-23 17:37:01 +0200

    chore(sandy): Fixed linter warnings

--- packages/sandy/src/waistband.js
+++ packages/sandy/src/waistband.js
@@ -1,8 +1,8 @@
 import straightWaistband from './straight-waistband'
 import curvedWaistband from './curved-waistband'
 
-export default part => {
-  let { store, options } = part.shorthand()
+export default (part) => {
+  let { options } = part.shorthand()
 
   if (options.waistbandShape === 'curved') return curvedWaistband(part)
   else return straightWaistband(part)
