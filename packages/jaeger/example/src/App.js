a2ab19c78d0a27033ff7395b928c9db460814aff
Joost De Cock <joost@joost.at>
2020-12-12 14:41:04 +0100

    fix(cfp): Uninitialize variable and change in recreate gist url

--- packages/jaeger/example/src/App.js
+++ packages/jaeger/example/src/App.js
@@ -17,9 +17,9 @@ const App = (props) => {
   */
 
   // Adds support for loading an external pattern configuration
-  let recreate
+  let recreate = false
   if (window) recreate = window.location.pathname.substr(1).split('/')
-  if (recreate.length === 3 && recreate[0] === 'from')
+  if (recreate.length === 3 && recreate[0] === 'recreate')
     recreate = { from: recreate[1], id: recreate[2] }
   else recreate = false
 
