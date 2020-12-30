5f734e3223e0540e83212524137afe64f61cad29
Joost De Cock <joost@joost.at>
2020-12-30 14:45:48 +0100

    breaking(components): Removed the Emblem, Navbar, and Ogol components

--- packages/components/src/Ogol/stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Ogol from ".";
-import { dark as backgrounds } from "../../.storybook/backgrounds";
-
-storiesOf("Ogol", module)
-  .add("Default", () => <Ogol />, { backgrounds })
-  .add("Custom color", () => <Ogol color="yellow" />, { backgrounds })
-  .add("Custom size", () => <Ogol size={200} />, { backgrounds });
