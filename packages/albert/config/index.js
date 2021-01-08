352a461eba52eea7182f0e74d65e234b5b47e3b0
Joost De Cock <joost@joost.at>
2021-01-08 12:15:05 +0100

    fix: Author names for consistency. Closes #764

--- packages/albert/config/index.js
+++ packages/albert/config/index.js
@@ -8,17 +8,7 @@ export default {
   department: 'unisex',
   type: 'pattern',
   difficulty: 3,
-  tags: [
-    'freesewing',
-    'design',
-    'diy',
-    'fashion',
-    'made to measure',
-    'parametric design',
-    'pattern',
-    'sewing',
-    'sewing pattern'
-  ],
+  tags: ['accessories'],
   optionGroups: {
     fit: ['backOpening', 'chestDepth'],
     style: ['lengthBonus', 'bibLength', 'bibWidth', 'strapWidth']
