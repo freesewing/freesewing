2e2a495bb5b77ce024fff6c23f0113841516ee1e
Wouter van Wageningen <wouter.vdub@yahoo.com>
2021-01-30 13:15:25 +0100

    wip[hortensia]: Initial commit of Hortensia code

---
+++ packages/hortensia/config/index.js
@@ -0,0 +1,44 @@
+import { version } from "../package.json";
+
+// ?? 🤔 ?? --> https://en.freesewing.dev/packages/core/config
+
+export default {
+  name: "hortensia",
+  version,
+  design: "Stoffsuchti/WouterVdub",
+  code: "Stoffsuchti/WouterVdub",
+  department: "accessories",
+  type: "pattern",
+  difficulty: 3,
+  tags: [
+    "freesewing",
+    "design",
+    "diy",
+    "fashion",
+    "made to measure",
+    "parametric design",
+    "pattern",
+    "sewing",
+    "sewing pattern"
+  ],
+  optionGroups: {
+    options: ["size", "zipperSize","strapLength","handleWidth"]
+  },
+  measurements: [],
+  dependencies: {},
+  inject: {},
+  hide: [],
+  parts: ["sidepanel","frontpanel","bottompanel","zipperpanel","sidepanelreinforcement","strap"],
+  options: {
+    width: 230,
+    height: 330,
+    minHandleSpaceWidth: 80,
+    maxHandleSpaceWidth: 250,
+    pctHandleSpace: 50,
+    pctHandleVert: 42,
+    strapLength: { pct: 160, min: 75, max: 250 },
+    handleWidth: { mm: 20, min: 7, max: 30 },
+    size: { pct: 50, min: 20, max: 200 },
+    zipperSize: { dflt: '#5', list: ['#3','#4','#4.5','#5','#6','#8','#10','Invisible']} 
+  }
+};
