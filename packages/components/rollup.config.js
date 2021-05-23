4a72b581d8147e41cdae8c71459532f9b75e557b
Joost De Cock <joost@joost.at>
2021-05-23 11:56:50 +0200

    fix(components): Add sass plugin to rollup

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
+import sass from 'rollup-plugin-sass';
 import { terser } from 'rollup-plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import { name, version, description, author, license } from './package.json'
@@ -23,18 +24,19 @@ const createConfig = (component, module) => {
       peerDepsExternal(),
       resolve({ modulesOnly: true }),
       json(),
-      //terser({
-      //  output: {
-      //    preamble: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
-      //  }
-      //})
+      sass(),
+      terser({
+        output: {
+          preamble: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
+        }
+      })
     ]
   }
 }
 
 const config = []
 // When developing, you can use this to only rebuild the components you're working on
-let dev = true
+let dev = false
 let only = ['Workbench']
 for (let component of components) {
   if (!dev || only.indexOf(component) !== -1) config.push(createConfig(component, false))
