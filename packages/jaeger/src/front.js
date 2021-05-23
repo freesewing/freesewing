8cc557789670058bff34ce23b0ae5eaa00cf8ef1
Joost De Cock <joost@joost.at>
2021-05-23 13:17:37 +0200

    fix(jaeger): Ignore type difference in buttons option. Fixes #973

--- packages/jaeger/src/front.js
+++ packages/jaeger/src/front.js
@@ -403,7 +403,7 @@ export default function (part) {
     if (options.buttons > 1) {
       points.button2 = new Point(points.cfWaist.x, points.cutawayPoint.y)
       buttons.push('button2')
-      if (options.buttons === 3) {
+      if (options.buttons == 3) {
         points.button3 = points.button1.shiftFractionTowards(points.button2, 0.5)
         buttons.push('button3')
       }
