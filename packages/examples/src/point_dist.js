8a86cdc677d51f719a83a842734de64bde0bf245
starfetch <80041179+starfetch@users.noreply.github.com>
2021-06-15 21:58:20 +0200

    minor fix: clean-up of dist() example
    fix indentation

--- packages/examples/src/point_dist.js
+++ packages/examples/src/point_dist.js
@@ -1,18 +1,19 @@
 export default (part) => {
-    let { Point, points, Path, paths} = part.shorthand()
 
-  points.from = new Point(10, 10)
-  points.to = new Point(80, 70)
+let { Point, points, Path, paths } = part.shorthand()
+
+points.from = new Point(10, 10)
+points.to = new Point(80, 70)
 
 points.text = points.from
-	.shiftFractionTowards(points.to, 0.6)
-	.attr("data-text", points.from.dist(points.to)+"mm")
-	.attr("data-text-class", "text-sm fill-note center");
+  .shiftFractionTowards(points.to, 0.6)
+  .attr("data-text", points.from.dist(points.to)+"mm")
+  .attr("data-text-class", "text-sm fill-note center")
     
-    paths.line = new Path()
-	.move(points.from)
-	.line(points.to)
-	.attr("class", "dashed");
-    
-    return part
+paths.line = new Path()
+  .move(points.from)
+  .line(points.to)
+  .attr("class", "dashed")
+
+return part
 }
