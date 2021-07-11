65eeddbf6ee8e6c17a13d110d97ba7bca0c08a07
Joost De Cock <joost@joost.at>
2021-07-11 10:34:45 +0200

    fix(cathrin): Fix for issue with panel 4
    This issue was spotted by @nataliabarnhart and reported on Discord.
    Rather than fix it outright, I'm creating a pull request so she can
    approve/merge it.

--- packages/cathrin/src/panels.js
+++ packages/cathrin/src/panels.js
@@ -110,6 +110,8 @@ export default function (part) {
   paths.panel4
     .curve(points.underbustGap3RightCp, points.waistGap3RightCp1, points.waistGap3Right)
     .curve(points.waistGap3RightCp2, points.hipsGap3Cp, points.hipsGap3)
+  if (options.panels === '11') paths.panel4.line(points.hipRise)
+  paths.panel4
     .close()
     .attr('class', 'fabric')
   points.underbustGap4Right = topBack.intersectsX(points.underbustGap4Right.x).pop()
