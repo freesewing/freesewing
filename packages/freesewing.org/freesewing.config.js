0aa139f28e6c50f37abe904537b1e4551670a217
joostdecock <joost@joost.at>
2021-07-28 17:11:05 +0200

    fix: Handle missing author

--- packages/freesewing.org/freesewing.config.js
+++ packages/freesewing.org/freesewing.config.js
@@ -14,18 +14,6 @@ module.exports = {
     key: '589c7a7e4d9c95a4f12868581259bf3a',
     index: 'en_freesewing_org',
   },
-  strapi: {
-    host: 'https://posts.freesewing.org',
-    sizes: [ 'thumbnail', 'small', 'medium', 'large' ],
-    author: {
-      displayname: 'FIXME',
-      about: 'There is no author/maker associated with this post. Please fix that in Strapi',
-      picture: {
-        url: '/uploads/avatar_0438a97c7a.svg',
-        width: 400,
-        height: 400,
-      }
-    }
-  },
+  strapi,
 }
 
--- packages/freesewing.shared/components/strapi/author.js
+++ packages/freesewing.shared/components/strapi/author.js
@@ -1,45 +1,49 @@
 import config from '@/site/freesewing.config'
 import MDX from "@mdx-js/runtime";
 
-const Author = ({author=config.strapi.author, type, t=false}) => (
-  <div id="author" className="shadow flex flex-col lg:flex-row rounded-xl border border-base-300 max-w-prose py-4 mx-auto lg:p-0">
-    <div
-      className={`
-        w-1/2 h-auto bg-cover bg-center rounded-l-lg hidden
-        lg:block
-      `}
-      style={{backgroundImage: `url(${config.strapi.host}${author?.picture?.url})`}}
-    >
-    </div>
-    <img
-      className={`
-        block w-32 h-32 mx-auto rounded-full
-        lg:hidden
-      `}
-      src={`${config.strapi.host}${author?.picture?.url}`}
-      alt={author?.displayname}
-      width={author?.picture?.width}
-      height={author?.picture?.height}
-    />
-    <div className={`
-        text-center p-4 rounded-r-lg bg-opacity-50
-        lg:text-left
-      `}
-    >
-      <p className="text-xl mb-2">
-        <span className="font-semibold"> {author.displayname}</span>
-        <span className="text-base-300 text-sm pl-2">
-          {t
-            ? t('xThis', {x: t(type === 'blog' ? 'wrote' : 'made')})
-            : 'Wrote this'
-          }
-        </span>
-      </p>
-      <div className="prose">
-        <MDX>{author.about}</MDX>
+const Author = props => {
+  const author = props.author || config.strapi.author
+
+  return  (
+    <div id="author" className="shadow flex flex-col lg:flex-row rounded-xl border border-base-300 max-w-prose py-4 mx-auto lg:p-0">
+      <div
+        className={`
+          w-1/2 h-auto bg-cover bg-center rounded-l-lg hidden
+          lg:block
+        `}
+        style={{backgroundImage: `url(${config.strapi.host}${author?.picture?.url})`}}
+      >
+      </div>
+      <img
+        className={`
+          block w-32 h-32 mx-auto rounded-full
+          lg:hidden
+        `}
+        src={`${config.strapi.host}${author?.picture?.url}`}
+        alt={author?.displayname}
+        width={author?.picture?.width}
+        height={author?.picture?.height}
+      />
+      <div className={`
+          text-center p-4 rounded-r-lg bg-opacity-50
+          lg:text-left
+        `}
+      >
+        <p className="text-xl mb-2">
+          <span className="font-semibold"> {author?.displayname}</span>
+          <span className="text-base-300 text-sm pl-2">
+            {props.t
+              ? props.t('xThis', {x: props.t(props.type === 'blog' ? 'wrote' : 'made')})
+              : 'Wrote this'
+            }
+          </span>
+        </p>
+        <div className="prose">
+          <MDX>{author?.about}</MDX>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Author
