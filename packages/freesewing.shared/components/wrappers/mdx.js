a3b84f9762f446f3ed02dc1a7ad8d7fe3a920bdb
joostdecock <joost@joost.at>
2021-08-07 13:14:49 +0000

    fix(shared): Optional chaining for image src

--- packages/freesewing.shared/components/wrappers/mdx.js
+++ packages/freesewing.shared/components/wrappers/mdx.js
@@ -18,7 +18,7 @@ const DesignIterator = props => {
 
 const Figure = props => (
   <figure>
-    <img src={props.src} alt={props?.alt || ''} title={props?.title || ''} className="shadow-md"/>
+    <img src={props?.src} alt={props?.alt || ''} title={props?.title || ''} className="shadow-md"/>
     <figcaption className="text-center italic">{props.title || 'FIXME: No title property set on this image'}</figcaption>
   </figure>
 )
