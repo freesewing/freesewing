e7317877d49021c4ac02997de68a56c6f07a2e1c
unknown <bobgeorgethe3rd@googlemail.com>
2021-10-12 17:03:05 +0100

    Adding Bee

---
+++ packages/bee/example/src/pattern/README.md
@@ -0,0 +1,31 @@
+# bee
+
+> bikini top
+
+[![NPM](https://img.shields.io/npm/v/bee.svg)](https://www.npmjs.com/package/bee) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save bee
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'bee'
+
+class Example extends Component {
+  render () {
+    return (
+      <MyComponent />
+    )
+  }
+}
+```
+
+## License
+
+ © [bobgeorgethe3rd](https://github.com/bobgeorgethe3rd)
---
+++ packages/bee/example/src/pattern/rollup.config.js
@@ -0,0 +1,43 @@
+import url from '@rollup/plugin-url'
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import { terser } from 'rollup-plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+// import postcss from 'rollup-plugin-postcss'
+import { main, module, name, version, description, author, license } from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: main,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'default'
+    },
+    {
+      file: module,
+      format: 'es',
+      sourcemap: true,
+      exports: 'default'
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    // postcss({ modules: true }),
+    url({ exclude: ['**/*.svg'] }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve({ browser: true }),
+    json(),
+    commonjs(),
+    terser({
+      output: {
+        preamble: `/**\n * ${name} | v${version}\n * ${description}\n * (c) ${new Date().getFullYear()} ${author}\n * @license ${license}\n */`
+      }
+    })
+  ]
+}
