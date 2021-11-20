0b7cc0be81b31b13099e55936f2ec5094f331e01
Chris Barrett <cmbarrett@gmail.com>
2021-11-20 14:12:19 +0100

    fixing unused variable error

--- packages/plugin-title/tests/title.test.js
+++ packages/plugin-title/tests/title.test.js
@@ -7,13 +7,13 @@ const expect = require("chai").expect;
 const plugin = require("../dist/index.js");
 
 
-// it("Should set the plugin name:version attribute", () => {
-//   let pattern = new freesewing.Pattern().with(plugin);
-//   pattern.render();
-//   expect(pattern.svg.attributes.get("freesewing:plugin-title")).to.equal(
-//     version
-//   );
-// });
+it("Should set the plugin name:version attribute", () => {
+  let pattern = new freesewing.Pattern().use(plugin);
+  pattern.render();
+  expect(pattern.svg.attributes.get("freesewing:plugin-title")).to.equal(
+    version
+  );
+});
 
 it("Should import the style", () => {
   let pattern = new freesewing.Pattern();
