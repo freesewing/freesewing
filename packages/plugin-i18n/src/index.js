79682706486aec2c9e45015b7ff8e6de70f3bf7a
Joost De Cock <joost@joost.at>
2021-11-21 18:06:26 +0100

    chore(plugin-i18n): Using new attributes.setIfUnset() method

--- packages/plugin-i18n/src/index.js
+++ packages/plugin-i18n/src/index.js
@@ -4,21 +4,18 @@ export default {
   name: name,
   version: version,
   hooks: {
-    preRender: function (svg) {
-      if (svg.attributes.get('freesewing:plugin-i18n') === false) {
-        svg.attributes.set('freesewing:plugin-i18n', version)
-      }
-    },
-    insertText: function (locale, text, data) {
-      if (data === false) {
+    preRender: (svg) => svg.attributes.setIfUnset('freesewing:plugin-i18n', version),
+    insertText: (locale, text, data) => {
+      if (!data) {
         console.log(
           "No data was passed to the i18n plugin. This plugin won't do much without injecting data into it"
         )
         return text
       }
-      let prefix = data.prefix || ''
-      if (typeof data.strings[locale][prefix + text] === 'undefined') return text
-      else return data.strings[locale][prefix + text]
+      const prefix = data.prefix || ''
+      return typeof data.strings[locale][prefix + text] === 'undefined'
+        ? text
+        : data.strings[locale][prefix + text]
     },
   },
 }
--- packages/plugin-i18n/tests/i18n.test.js
+++ packages/plugin-i18n/tests/i18n.test.js
@@ -1,28 +1,24 @@
-import freesewing from "freesewing";
-import { version } from "../package.json";
-let chai = require("chai");
-let expect = chai.expect;
-chai.use(require("chai-string"));
-let plugin = require("../dist/index.js");
+import freesewing from '@freesewing/core'
+import { strings } from '@freesewing/i18n'
+import { version } from '../package.json'
 
-it("Should set the plugin name:version attribute", () => {
-  let pattern = new freesewing.Pattern().with(plugin);
-  pattern.render();
-  expect(pattern.svg.attributes.get("freesewing:plugin-i18n")).to.equal(
-    version
-  );
-});
+const chai = require('chai')
+const expect = chai.expect
+const plugin = require('../dist/index.js')
 
-it("Should translate text on insert", () => {
-  let pattern = new freesewing.Pattern().with(plugin);
-  pattern.settings.locale = "nl";
-  pattern.draft = function() {};
-  pattern.with(plugin);
-  pattern.parts.test = new pattern.Part();
+it('Should set the plugin name:version attribute', () => {
+  const pattern = new freesewing.Pattern().use(plugin, {})
+  pattern.draft().render()
+  expect(pattern.svg.attributes.get('freesewing:plugin-i18n')).to.equal(version)
+})
+
+it('Should translate text on insert', () => {
+  const pattern = new freesewing.Pattern().use(plugin, { strings })
+  pattern.parts.test = new pattern.Part()
   pattern.parts.test.points.anchor = new pattern.Point(-12, -34).attr(
-    "data-text",
-    "cutTwoStripsToFinishTheArmholes"
-  );
-  let svg = pattern.render();
-  expect(svg).to.contain("Knip twee repels om de armsgaten af te werken");
-});
+    'data-text',
+    'plugin.cutTwoStripsToFinishTheArmholes'
+  )
+  const svg = pattern.draft().render()
+  expect(svg).to.contain('Cut two strips to finish the armholes')
+})
