ac570543325b67377c346639c070fac5b0b47c6a
Joost De Cock <joost@joost.at>
2021-11-21 16:59:48 +0100

    chore: Improve unit test

--- packages/plugin-buttons/tests/buttons.test.js
+++ packages/plugin-buttons/tests/buttons.test.js
@@ -5,8 +5,19 @@ const chai = require('chai')
 const expect = chai.expect
 const plugin = require('../dist/index.js')
 
+const pattern = new freesewing.Pattern().use(plugin)
+pattern.draft().render()
+
 it('Should set the plugin name:version attribute', () => {
-  const pattern = new freesewing.Pattern().use(plugin)
-  pattern.draft().render()
   expect(pattern.svg.attributes.get('freesewing:plugin-buttons')).to.equal(version)
 })
+
+for (const snippet of ['button', 'buttonhole', 'snap-stud', 'snap-socket']) {
+  it(`Should add the ${snippet} snippet to defs`, () => {
+    //const pattern = new freesewing.Pattern().use(plugin)
+    //pattern.parts.test = new pattern.Part()
+    //pattern.draft().render()
+    expect(pattern.svg.defs.indexOf(`<g id="${snippet}">`)).to.not.equal(-1)
+  })
+}
+
