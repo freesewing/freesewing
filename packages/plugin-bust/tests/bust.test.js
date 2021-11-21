01cfae50afd4a78e4af918aa37816da7236fc2f6
Joost De Cock <joost@joost.at>
2021-11-21 16:38:41 +0100

    chore: formatting with prettier

--- packages/plugin-bust/tests/bust.test.js
+++ packages/plugin-bust/tests/bust.test.js
@@ -1,21 +1,19 @@
-import freesewing from "@freesewing/core";
-import { version } from "../package.json";
+import freesewing from '@freesewing/core'
+import { version } from '../package.json'
 
-const  chai = require("chai");
-const  expect = chai.expect;
-const plugin = require("../dist/index.js");
+const chai = require('chai')
+const expect = chai.expect
+const plugin = require('../dist/index.js')
 
-describe("plugin-bust",function() {
-  it("Should set the plugin name:version attribute", () => {
-    const pattern = new freesewing.Pattern().use(plugin);
-    pattern.parts.test = new pattern.Part();
-    const { macro } = pattern.parts.test.shorthand();
-    macro("bust", {
-      measurements: {}
-    });
-    pattern.render();
-    expect(pattern.svg.attributes.get("freesewing:plugin-bust")).to.equal(
-      version
-    );
-  });
-});
+describe('plugin-bust', function () {
+  it('Should set the plugin name:version attribute', () => {
+    const pattern = new freesewing.Pattern().use(plugin)
+    pattern.parts.test = new pattern.Part()
+    const { macro } = pattern.parts.test.shorthand()
+    macro('bust', {
+      measurements: {},
+    })
+    pattern.render()
+    expect(pattern.svg.attributes.get('freesewing:plugin-bust')).to.equal(version)
+  })
+})
