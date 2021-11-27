d2da193663136929a010125738b139b0ca1b7f90
Joost De Cock <joost@joost.at>
2021-11-27 16:41:53 +0100

    chore(plugin-banner): Added (esm) unit tests. See #408

--- packages/plugin-banner/tests/plugin.test.mjs
+++ packages/plugin-banner/tests/plugin.test.mjs
@@ -1,16 +1,10 @@
-import freesewing from '@freesewing/core'
-import { version } from '../package.json'
 import chai from 'chai'
-import plugin from '../dist/index.js'
+import freesewing from '@freesewing/core'
+import plugin from '../dist/index.mjs'
 
 const expect = chai.expect
 
-describe('Banner Plugin', () => {
-  it('Should set the plugin name:version attribute', () => {
-    const pattern = new freesewing.Pattern().use(plugin)
-    pattern.render()
-    expect(pattern.svg.attributes.get('freesewing:plugin-banner')).to.equal(version)
-  })
+describe('Banner Plugin Tests', () => {
 
   const pattern = new freesewing.Pattern()
   pattern.use(plugin).render()
