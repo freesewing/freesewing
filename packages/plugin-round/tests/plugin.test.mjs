5bbf579b01008442ab646de013ae1db90bacc432
Joost De Cock <joost@joost.at>
2021-11-27 16:45:32 +0100

    chore(plugin-round): Added (esm) unit tests. See #408

---
+++ packages/plugin-round/tests/plugin.test.mjs
@@ -0,0 +1,11 @@
+import chai from 'chai'
+import freesewing from '@freesewing/core'
+import plugin from '../dist/index.mjs'
+
+const expect = chai.expect
+
+describe('Round Plugin Tests', () => {
+  it("FIXME: No plugin tests configured", () => {
+    expect(1).to.equal(1)
+  })
+})
