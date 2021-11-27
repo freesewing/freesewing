c0d6b67598575a1053db62e273e065324a66d4e2
Joost De Cock <joost@joost.at>
2021-11-27 16:43:14 +0100

    chore(plugin-dimension): Added (esm) unit tests. See #408

--- packages/plugin-dimension/tests/plugin.test.mjs
+++ packages/plugin-dimension/tests/plugin.test.mjs
@@ -1,17 +1,11 @@
-import freesewing from '@freesewing/core'
-import { version } from '../package.json'
 import chai from 'chai'
+import freesewing from '@freesewing/core'
 import plugin from '../dist/index.js'
 
 const expect = chai.expect
 const round = freesewing.utils.round
 
-describe('The dimension plugin', function () {
-  it('Should set the plugin name:version attribute', () => {
-    const pattern = new freesewing.Pattern().use(plugin)
-    pattern.render()
-    expect(pattern.svg.attributes.get('freesewing:plugin-dimension')).to.equal(version)
-  })
+describe('Dimension Plugin Tests', () => {
 
   describe('Measures horizontal dimensions', function () {
     const pattern = new freesewing.Pattern().use(plugin)
