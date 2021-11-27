7f31606f14d3b1ca5e8d18e8ce23c615b1cc34a1
Joost De Cock <joost@joost.at>
2021-11-27 13:09:45 +0100

    chore(plugin-bust): Use only imports in tests

--- packages/plugin-bust/tests/bust.test.js
+++ packages/plugin-bust/tests/bust.test.js
@@ -1,9 +1,9 @@
 import freesewing from '@freesewing/core'
 import { version } from '../package.json'
+import chai from 'chai'
+import plugin from '../dist/index.js'
 
-const chai = require('chai')
 const expect = chai.expect
-const plugin = require('../dist/index.js')
 
 describe('plugin-bust', function () {
   it('Should set the plugin name:version attribute', () => {
