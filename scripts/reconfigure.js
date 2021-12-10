1ad8c9e3102a571518cc7ad7551915e3ae818b0d
Joost De Cock <joost@joost.at>
2021-12-10 08:41:58 +0100

    chore: Updated scripts and structure for new sites

--- scripts/reconfigure.js
+++ scripts/reconfigure.js
@@ -504,24 +504,30 @@ function configurePkgUnitTests(type, pkg, config) {
  * New: Adds unit tests for patterns
  */
 function reconfigure(pkgs, config) {
-  for (let pkg of pkgs) {
+  for (const pkg of pkgs) {
     console.log(chalk.blueBright(`Reconfiguring ${pkg}`))
-    let pkgConfig = packageConfig(pkg, config)
-    fs.writeFileSync(
-      path.join(config.repoPath, 'packages', pkg, 'package.json'),
-      JSON.stringify(pkgConfig, null, 2) + '\n'
-    )
+    if (config.exceptions.customPackageJson.indexOf(pkg) === -1) {
+      const pkgConfig = packageConfig(pkg, config)
+      fs.writeFileSync(
+        path.join(config.repoPath, 'packages', pkg, 'package.json'),
+        JSON.stringify(pkgConfig, null, 2) + '\n'
+      )
+    }
     if (config.exceptions.customRollup.indexOf(pkg) === -1) {
       fs.writeFileSync(
         path.join(config.repoPath, 'packages', pkg, 'rollup.config.js'),
         config.templates.rollup
       )
     }
-    fs.writeFileSync(path.join(config.repoPath, 'packages', pkg, 'README.md'), readme(pkg, config))
-    fs.writeFileSync(
-      path.join(config.repoPath, 'packages', pkg, 'CHANGELOG.md'),
-      changelog(pkg, config)
-    )
+    if (config.exceptions.customReadme.indexOf(pkg) === -1) {
+      fs.writeFileSync(path.join(config.repoPath, 'packages', pkg, 'README.md'), readme(pkg, config))
+    }
+    if (config.exceptions.customChangelog.indexOf(pkg) === -1) {
+      fs.writeFileSync(
+        path.join(config.repoPath, 'packages', pkg, 'CHANGELOG.md'),
+        changelog(pkg, config)
+      )
+    }
     const type = packageType(pkg, config)
     if (type === 'pattern') configurePatternExample(pkg, config)
     if (['pattern', 'plugin'].indexOf(type) !== -1) configurePkgUnitTests(type, pkg, config)
