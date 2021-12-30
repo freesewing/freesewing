43a89a85ade8ebcb3bb49f1776c60f37074ca9c2
Joost De Cock <joost@joost.at>
2021-12-30 17:55:46 +0100

    feat(fs.dev): moved theme picker to header

--- packages/freesewing.shared/components/theme-picker.js
+++ packages/freesewing.shared/components/theme-picker.js
@@ -4,16 +4,17 @@ import ThemeIcon from 'shared/components/icons/theme.js'
 const ThemePicker = ({ app, className='' }) => {
   return (
       <div className="dropdown">
-        <div tabIndex="0" className="m-0 btn flex flex-row gap-2">
+        <div tabIndex="0" className="m-0 btn flex flex-row gap-2 hover:bg-neutral hover:border-neutral-content">
           <ThemeIcon />
-          <span>Theme:</span>
           <span>{app.theme}</span>
+          <span>Theme</span>
         </div>
         <ul tabIndex="0" className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
           {Object.keys(themes).map(theme => (
             <li key={theme}>
-              <button onClick={() => app.setTheme(theme)} className="btn btn-link">
+              <button onClick={() => app.setTheme(theme)} className="btn btn-ghost text-base-content hover:bg-base-200">
                 {theme}
+                <span>&nbsp;Theme</span>
               </button>
             </li>
           ))}
