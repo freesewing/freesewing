384d87a87e9559582d262527b59267776b1f6ffd
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2022-01-08 12:17:57 +0000

    feat: Clones tutorial package to make bob folder to adapt

---
+++ packages/bob/CHANGELOG.md
@@ -0,0 +1,27 @@
+# Change log for: @freesewing/tutorial
+
+
+## 2.17.0 (2021-07-01)
+
+### Fixed
+
+ - Set department in the config to one of the newly accepted values
+
+## 2.7.0 (2020-07-12)
+
+### Changed
+
+ - Removed `Circumference` suffix from measurement names
+
+## 2.0.0 (2019-08-25)
+
+### Added
+
+ - Initial release
+
+
+This is the **initial release**, and the start of this change log.
+
+> Prior to version 2, FreeSewing was not a JavaScript project.
+> As such, that history is out of scope for this change log.
+
---
+++ packages/bob/config/index.js
@@ -0,0 +1,36 @@
+import { version } from '../package.json'
+
+// ?? 🤔 ?? --> https://en.freesewing.dev/packages/core/config
+
+export default {
+  name: 'tutorial',
+  version,
+  design: 'joostdecock',
+  code: 'joostdecock',
+  department: 'accessorties',
+  type: 'pattern',
+  difficulty: 1,
+  tags: ['example'],
+  optionGroups: {
+    fit: ['neckRatio', 'widthRatio', 'lengthRatio'],
+    box: ['size'],
+  },
+  measurements: ['head'],
+  inject: {
+    step4: 'step3',
+    step5: 'step4',
+    step6: 'step5',
+    step7: 'step6',
+    step8: 'step7',
+    step9: 'step8',
+    step10: 'step9',
+    step11: 'step10',
+  },
+  parts: ['step1', 'step2', 'bib'],
+  options: {
+    size: { pct: 50, min: 10, max: 100 },
+    neckRatio: { pct: 80, min: 70, max: 90 },
+    widthRatio: { pct: 45, min: 35, max: 55 },
+    lengthRatio: { pct: 75, min: 55, max: 85 },
+  },
+}
