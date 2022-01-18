85041b23e5be77088be47f8c1da5d36e0d6d576c
Joost De Cock <joost@joost.at>
2022-01-18 12:00:19 +0100

    fix(i18n): Fix plugin translations
    In a misguided attempt to keep the code DRY, the file that
    builds the plugin translations was symlinked from various
    languages to the English version.
    
    Alas, due to the relative imports, this means that all
    languages imported the English translations for plugins,
    meaning that on-pattern translation was de-facto broken.
    
    This fixes that.

--- packages/i18n/src/locales/fr/plugin/index.js
+++ packages/i18n/src/locales/fr/plugin/index.js
@@ -1,1 +1,33 @@
-../../en/plugin/index.js
+import brian from './patterns/brian.yaml'
+import aaron from './patterns/aaron.yaml'
+import bruce from './patterns/bruce.yaml'
+import hugo from './patterns/hugo.yaml'
+import simon from './patterns/simon.yaml'
+import teagan from './patterns/teagan.yaml'
+import cfp from './patterns/cfp.yaml'
+import cutonfold from './plugins/cutonfold.yaml'
+import grainline from './plugins/grainline.yaml'
+import scalebox from './plugins/scalebox.yaml'
+import title from './plugins/title.yaml'
+
+const files = {
+  brian,
+  aaron,
+  bruce,
+  hugo,
+  simon,
+  teagan,
+  cfp,
+  cutonfold,
+  grainline,
+  scalebox,
+  title,
+}
+
+const messages = {}
+
+for (const file in files) {
+  for (const [key, val] of Object.entries(files[file])) messages[key] = val
+}
+
+export default messages
