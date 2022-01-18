5a2ae9a20a45ca8a38afc82b0d1a44f1134fdb34
Joost De Cock <joost@joost.at>
2022-01-18 14:25:56 +0100

    chore(plugin-notches): Reduced bnotch stroke width

--- packages/plugin-notches/src/index.js
+++ packages/plugin-notches/src/index.js
@@ -1,7 +1,7 @@
 import { version, name } from '../package.json'
 
 const notches =
-  '<g id="notch"><circle cy="0" cx="0" r="1.4" class="fill-note" /><circle cy="0" cx="0" r="2.8" class="note" /></g><g id="bnotch"><path d="M -1.1 -1.1 L 1.1 1.1 M 1.1 -1.1 L -1.1 1.1" class="note stroke-xl" /><circle cy="0" cx="0" r="2.8" class="note" /></g>'
+  '<g id="notch"><circle cy="0" cx="0" r="1.4" class="fill-note" /><circle cy="0" cx="0" r="2.8" class="note" /></g><g id="bnotch"><path d="M -1.1 -1.1 L 1.1 1.1 M 1.1 -1.1 L -1.1 1.1" class="note" /><circle cy="0" cx="0" r="2.8" class="note" /></g>'
 
 export default {
   name: name,
