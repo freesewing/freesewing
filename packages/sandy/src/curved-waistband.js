b1647a7371f4b90466baa58c7743ce205ae1cc30
Joost De Cock <joost@joost.at>
2022-01-19 16:23:01 +0100

    chore(sandy): Make it prettier

--- packages/sandy/src/curved-waistband.js
+++ packages/sandy/src/curved-waistband.js
@@ -27,19 +27,23 @@ export default function (part) {
     utils.rad2deg(store.get('bottomCircumference') - store.get('topCircumference')) /
     absoluteOptions.waistbandWidth
 
-
   const rad = store.get('topCircumference') / utils.deg2rad(an)
 
   // Extra angle to extend the waistband to overlap according to waistbandOverlap
-  const anExtra = utils.rad2deg(store.get('waistbandOverlap') / (rad + absoluteOptions.waistbandWidth))
+  const anExtra = utils.rad2deg(
+    store.get('waistbandOverlap') / (rad + absoluteOptions.waistbandWidth)
+  )
 
   // The curved waistband is shown with no rotation
   const rot = 0
   // Call draftRingSector to draft the part
-  paths.seam = draftRingSector(part, rot, an + anExtra, rad, rad + absoluteOptions.waistbandWidth).attr(
-    'class',
-    'fabric'
-  )
+  paths.seam = draftRingSector(
+    part,
+    rot,
+    an + anExtra,
+    rad,
+    rad + absoluteOptions.waistbandWidth
+  ).attr('class', 'fabric')
 
   // Anchor samples to the centre of the waist
   points.gridAnchor = points.in1.clone()
