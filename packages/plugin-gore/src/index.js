54def5e898e5ef8cfbb35afa35f2f6ea32b0bbbb
Joost De Cock <joost@joost.at>
2022-01-25 09:28:56 +0100

    fix(plugin-gore): Use default import for JSON

--- packages/plugin-gore/src/index.js
+++ packages/plugin-gore/src/index.js
@@ -1,14 +1,14 @@
-import { name, version } from '../package.json'
+import pkg from '../package.json'
 
 const deprecated =
   "The 'goreNumber' property for the gore macro is deprecated " +
   "and will be removed in a future version. Please use 'gores' instead"
 
 export default {
-  name: name,
-  version: version,
+  name: pkg.name,
+  version: pkg.version,
   hooks: {
-    preRender: (svg) => svg.attributes.setIfUnset('freesewing:plugin-gore', version),
+    preRender: (svg) => svg.attributes.setIfUnset('freesewing:plugin-gore', pkg.version),
   },
   macros: {
     gore: function (so) {
