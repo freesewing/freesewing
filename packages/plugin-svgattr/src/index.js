f5883aebbf7a9320a21b48bd8b406520fd90802f
Joost De Cock <joost@joost.at>
2022-01-25 09:25:25 +0100

    fix(plugin-svgattr): Use default import for JSON

--- packages/plugin-svgattr/src/index.js
+++ packages/plugin-svgattr/src/index.js
@@ -1,13 +1,13 @@
-import { version, name } from '../package.json'
+import pkg from '../package.json'
 
 export default {
-  name: name,
-  version: version,
+  name: pkg.name,
+  version: pkg.version,
   hooks: {
     preRender: function (svg, attributes = {}) {
       if (svg.attributes.get('freesewing:plugin-svgattr') === false) {
         for (const key of Object.keys(attributes)) svg.attributes.add(key, attributes[key])
-        svg.attributes.add('freesewing:plugin-svgattr', version)
+        svg.attributes.add('freesewing:plugin-svgattr', pkg.version)
       }
     },
   },
