5f5d818132d739b69f524a4ff726d7af419a582d
Nick Dower <nicholasdower@gmail.com>
2022-01-28 00:03:43 +0100

    Remove Sass from plugin-theme.
    See #1656

---
+++ packages/plugin-theme/src/lib/sample.js
@@ -0,0 +1,9 @@
+export default `
+  /* Sample classes */
+  svg.freesewing path.sample {
+    stroke-width: 0.75
+  }
+  svg.freesewing path.sample-focus {
+    stroke-width: 1.5; fill: rgba(0,0,0,0.1)
+  }
+`;
