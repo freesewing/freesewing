7ae2c4b1eb7002178b13c518a6373dca12c4f0d8
Joost De Cock <joost@joost.at>
2022-01-29 22:00:27 +0100

    fix(lab): Prebuild for lab

--- packages/freesewing.shared/prebuild/index.mjs
+++ packages/freesewing.shared/prebuild/index.mjs
@@ -7,9 +7,11 @@ import { prebuildPatrons } from './patrons.mjs'
 const SITE = process.env.SITE
 
 const run = async () => {
-  const mdxPages = await prebuildMdx(SITE)
-  const [posts, authors] = await prebuildStrapi(SITE)
-  prebuildNavigation(mdxPages, posts, SITE)
+  if (SITE !== 'lab') {
+    const mdxPages = await prebuildMdx(SITE)
+    const [posts, authors] = await prebuildStrapi(SITE)
+    prebuildNavigation(mdxPages, posts, SITE)
+  }
   await prebuildContributors(SITE)
   await prebuildPatrons(SITE)
   console.log()
