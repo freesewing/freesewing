460e970c3597e0b1540b9038736bfb3ffe859750
Joost De Cock <joost@joost.at>
2022-01-29 10:50:02 +0100

    wip(lab): Xray for points and paths

--- packages/freesewing.shared/components/workbench/draft/path/index.js
+++ packages/freesewing.shared/components/workbench/draft/path/index.js
@@ -1,16 +1,32 @@
 import TextOnPath from '../text-on-path'
 import { getProps } from '../utils'
 
+const XrayPath = props => (
+  <g>
+    <path
+      d={props.path.asPathstring()}
+      {...getProps(props.path)}
+      className="opacity-0 stroke-4xl stroke-contrast hover:opacity-25 hover:cursor-pointer"
+      onClick={() => props.updateGist(
+        ['xray', 'parts', props.partName, 'paths', props.pathName],
+        props.path
+      )}
+    />
+  </g>
+)
+
+
 const Path = props => {
-  const { path, part, name } = props
+  const { path, partName, pathName } = props
   if (!path.render) return null
   const output = []
-  const pathId = 'path-' + part + '-' + name
+  const pathId = 'path-' + partName + '-' + pathName
   output.push(
     <path id={pathId} key={pathId} d={path.asPathstring()} {...getProps(path)} />
   )
   if (path.attributes.get('data-text'))
     output.push(<TextOnPath key={'text-on-path-' + name} pathId={pathId} {...props} />)
+  if (props.gist.xray) output.push(<XrayPath {...props} key={'xpath'+pathId} />)
 
   return output
 }
--- packages/freesewing.shared/components/workbench/draft/point/index.js
+++ packages/freesewing.shared/components/workbench/draft/point/index.js
@@ -12,9 +12,9 @@ const XrayPoint = props => (
       cx={props.point.x}
       cy={props.point.y}
       r={7.5 * props.gist.scale}
-      className="fillhovertrap"
+      className="opacity-0 stroke-lining fill-lining hover:opacity-25 hover:cursor-pointer"
       onClick={() => props.updateGist(
-        ['xray', parts, 'props.partName', 'points', props.name],
+        ['xray', 'parts', props.partName, 'points', props.pointName],
         props.point
       )}
     />
@@ -23,13 +23,13 @@ const XrayPoint = props => (
 
 
 const Point = props => {
-  const { point, name } = props
+  const { point, pointName } = props
   const output = []
-  if (props.gist.xray) output.push(<XrayPoint {...props} key={'xp-' + props.name} />)
+  if (props.gist.xray) output.push(<XrayPoint {...props} key={'xp-' + pointName} />)
   if (point.attributes && point.attributes.get('data-text'))
-    output.push(<Text {...props} key={'point-' + name} />)
+    output.push(<Text {...props} key={'point-' + pointName} />)
   if (point.attributes && point.attributes.get('data-circle'))
-    output.push(<Circle point={point} key={'circle-' + name} />)
+    output.push(<Circle point={point} key={'circle-' + pointName} />)
 
   return output.length < 1 ? null : output
 }
