c70775b873af296c25d2139ed2848d83748c61f2
Joost De Cock <joost@joost.at>
2022-02-05 21:19:08 +0100

    fix(dev): Updated algolia index

--- packages/freesewing.dev/freesewing.config.js
+++ packages/freesewing.dev/freesewing.config.js
@@ -1,7 +1,7 @@
 const config = {
   algolia: {
     app: 'MA0Y5A2PF0', // Application ID
-    index: 'canary_freesewing.dev',
+    index: 'freesewing.dev',
     key: '589c7a7e4d9c95a4f12868581259bf3a', // Search-only API key
   },
   strapi: 'https://posts.freesewing.org',
