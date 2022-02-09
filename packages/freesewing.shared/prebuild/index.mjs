bd6c62782835943850ceb4bef953cb412e7865ac
Joost De Cock <joost@joost.at>
2022-02-09 18:58:16 +0100

    feat(lab): Prebuild pattern pages

--- packages/freesewing.shared/prebuild/index.mjs
+++ packages/freesewing.shared/prebuild/index.mjs
@@ -4,6 +4,7 @@ import { prebuildNavigation } from './navigation.mjs'
 import { prebuildContributors } from './contributors.mjs'
 import { prebuildPatrons } from './patrons.mjs'
 import { prebuildI18n } from './i18n.mjs'
+import { prebuildLab } from './lab.mjs'
 
 const SITE = process.env.SITE
 
@@ -12,6 +13,8 @@ const run = async () => {
     const mdxPages = await prebuildMdx(SITE)
     const [posts, authors] = await prebuildStrapi(SITE)
     prebuildNavigation(mdxPages, posts, SITE)
+  } else {
+    await prebuildLab()
   }
   if (SITE !== 'dev') await prebuildI18n(SITE)
   await prebuildContributors(SITE)
