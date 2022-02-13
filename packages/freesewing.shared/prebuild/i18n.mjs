d87180b99d3c5bc7106045953d4c5ff11714d628
Joost De Cock <joost@joost.at>
2022-02-13 13:00:03 +0100

    chore(shared): Updated i18n prebuild to new code

--- packages/freesewing.shared/prebuild/i18n.mjs
+++ packages/freesewing.shared/prebuild/i18n.mjs
@@ -1,10 +1,8 @@
 import path from 'path'
 import fs from 'fs'
-import { en, de, es, fr, nl, languages } from '../../i18n/dist/index.js'
+import { en, de, es, fr, nl, languages } from '../../i18n/src/next.mjs'
 
 const locales = { en, de, es, fr, nl }
-const l1 = ['patterns', 'settings']
-const l2 = ['options']
 
 const writeJson = (site, locale, namespace, content) => fs.writeFileSync(
   path.resolve(
@@ -18,32 +16,6 @@ const writeJson = (site, locale, namespace, content) => fs.writeFileSync(
   JSON.stringify(content)
 )
 
-const flattenL1 = content => {
-  const flat = {}
-  for (const l1 in content) {
-    flat[`${l1}.t`] = content[l1].title
-    flat[`${l1}.d`] = content[l1].description
-  }
-
-  return flat
-}
-
-const flattenL2 = content => {
-  const flat = {}
-  for (const l1 in content) {
-    flat[l1] = {}
-    for (const l2 in content[l1]) {
-      flat[l1][`${l2}.t`] = content[l1][l2].title
-      flat[l1][`${l2}.d`] = content[l1][l2].description
-      if (content[l1][l2].options) {
-        flat[l1][`${l2}.o`] = content[l1][l2].options
-      }
-    }
-  }
-
-  return flat
-}
-
 /*
  * Main method that does what needs doing
  */
@@ -53,24 +25,11 @@ export const prebuildI18n = async (site) => {
     console.log('Generating translation files for', locale)
     const loc = locales[locale]
     // Fan out into namespaces
-    for (const namespace in loc.topics) {
-      if (l1.indexOf(namespace) !== -1) {
-        writeJson(
-          site, locale, namespace,
-          flattenL1(loc.topics[namespace])
-        )
-      }
-      else if (l2.indexOf(namespace) !== -1) {
-        // Further fan out the (large) options namespace
-        const subNamespaces  = flattenL2(loc.topics[namespace])
-        for (const subNamespace in subNamespaces) {
-          writeJson(
-            site, locale, 'o_'+subNamespace,
-            subNamespaces[subNamespace]
-          )
-        }
-      }
-      else writeJson(site, locale, namespace, loc.topics[namespace])
+    for (const namespace in loc) {
+      writeJson(
+        site, locale, namespace,
+        loc[namespace]
+      )
     }
     writeJson(site, locale, 'locales', languages)
   }
