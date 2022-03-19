7eae745ce6ada26377533a4d5f8363be9b6bd5d3
Zee <zwartezwaluw@outlook.com>
2022-03-19 15:58:52 +0100

    initial pocket setup

---
+++ lucy/README.md
@@ -0,0 +1,31 @@
+# lucy
+
+> historical tie on pocket
+
+[![NPM](https://img.shields.io/npm/v/lucy.svg)](https://www.npmjs.com/package/lucy) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save lucy
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'lucy'
+
+class Example extends Component {
+  render () {
+    return (
+      <MyComponent />
+    )
+  }
+}
+```
+
+## License
+
+ © [SeaZeeZee](https://github.com/SeaZeeZee)
---
+++ lucy/config/index.js
@@ -0,0 +1,35 @@
+import { version } from '../package.json'
+
+// ?? 🤔 ?? --> https://en.freesewing.dev/packages/core/config
+
+export default {
+  name: 'lucy',
+  version,
+  design: 'SeaZeeZee',
+  code: 'SeaZeeZee',
+  department: 'accessories',
+  type: 'pattern',
+  difficulty: 3,
+  tags: [
+    'freesewing',
+    'design',
+    'diy',
+    'fashion',
+    'made to measure',
+    'parametric design',
+    'pattern',
+    'sewing',
+    'sewing pattern',
+  ],
+  optionGroups: {
+    fit: ['size'],
+  },
+  measurements: [],
+  dependencies: {},
+  inject: {},
+  hide: [],
+  parts: ['box'],
+  options: {
+    size: { pct: 50, min: 10, max: 100 },
+  },
+}
