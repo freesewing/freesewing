108d83b834acfbc5619341a13745e50e90c5855f
Joost De Cock <joost@joost.at>
2022-05-20 15:59:27 +0200

    chore(dev): Updated skip script

--- packages/freesewing.dev/skip_build.sh
+++ packages/freesewing.dev/skip_build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-if git diff HEAD^ HEAD --quiet ../freesewing.shared || git diff --quiet HEAD^ HEAD  . ; then
-  # We have local changes, go ahead and build
-  echo "✅ - Changed detected; Let's build this thing"
-  exit 1;
-else
-  # No changes, do not waste time building this commit
-  echo "🛑 - No changes detected; Let's just not"
-  exit 0;
-fi
+check=( \
+  "." \
+  "../../markdown/dev" \
+  "../freesewing.shared" \
+)
+build=0
+for d in ${check[@]}; do
+  if `git diff HEAD^ HEAD --quiet $d`; then
+    # We have changes, go ahead and build
+    echo "✅ - Changed detected in $d, let's build this thing"
+    exit 1;
+  fi
+done
+
+# No changes, do not waste time building this commit
+echo "🛑 - No changes detected; Let's just not"
+exit 0;
 
