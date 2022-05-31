b9b65394dad6c5fe6d6faa6f9effa2d8fafcc474
Joost De Cock <joost@joost.at>
2022-05-31 15:53:46 +0200

    chore(lucy): Don't destructure JSON imports

--- packages/lucy/config/index.js
+++ packages/lucy/config/index.js
@@ -1,15 +1,15 @@
-import { version } from '../package.json'
+import pkg from '../package.json'
 
 // ?? 🤔 ?? --> https://en.freesewing.dev/packages/core/config
 
 export default {
   name: 'lucy',
-  version,
+  version: pkg.version,
   design: 'SeaZeeZee',
   code: 'SeaZeeZee',
   department: 'accessories',
   type: 'pattern',
-  difficulty: 3,
+  difficulty: 2,
   tags: [
     'freesewing',
     'design',
@@ -33,6 +33,5 @@ export default {
     width: { pct: 50, min: 30, max: 100 },
     length: {pct: 50, min: 30, max:100},
     edge: { pct: 25, min: 20, max: 50 },
-
   },
 }
