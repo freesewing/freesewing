703a3a257c8e7db6980568653d737ac9594603f7
Zee <zwartezwaluw@outlook.com>
2022-06-01 14:53:18 +0200

    adds src index changes

--- packages/jane/src/index.js
+++ packages/jane/src/index.js
@@ -1,12 +1,14 @@
 import freesewing from '@freesewing/core'
 import plugins from '@freesewing/plugin-bundle'
 import config from '../config'
-import draftBox from './box'
-
+import draftBody from './body'
+import draftSleeve from './sleeve'
+import draftGusset from './gusset'
 // Create new design
 const Pattern = new freesewing.Design(config, plugins)
 
 // Attach the draft methods to the prototype
-Pattern.prototype.draftBox = draftBox
-
+Pattern.prototype.draftBody = draftBody
+Pattern.prototype.draftSleeve  = draftSleeve
+Pattern.prototype.draftGusset = draftGusset
 export default Pattern
