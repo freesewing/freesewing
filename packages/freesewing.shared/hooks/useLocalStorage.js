9f7452dcc7176c0b811485733ead0ca4eb7873ba
Enoch Riese <enoch.riese@gmail.com>
2022-06-06 13:20:24 -0500

    add some comments

--- packages/freesewing.shared/hooks/useLocalStorage.js
+++ packages/freesewing.shared/hooks/useLocalStorage.js
@@ -4,19 +4,22 @@ import { useState, useEffect } from 'react'
 function useLocalStorage(key, initialValue) {
   const prefix = 'fs_'
   const [storedValue, setStoredValue] = useState(initialValue);
+  // use this to track whether it's mounted. useful for doing other effects outside this hook
   const [ready, setReady] = useState(false);
 
   const setValue = (value) => {
-    if (typeof window === 'undefined') return null // SSR has no window object
+    if (!ready) return null
     try {
       const valueToStore = value instanceof Function ? value(storedValue) : value
       setStoredValue(valueToStore)
+
       window.localStorage.setItem(prefix + key, JSON.stringify(valueToStore))
     } catch (error) {
       console.log(error)
     }
   }
 
+  // get the item from localstorage after the component has mounted. empty brackets mean it runs one time
   useEffect(() => {
     const item = window.localStorage.getItem(prefix + key)
     if (item) {
--- packages/freesewing.shared/hooks/useTheme.js
+++ packages/freesewing.shared/hooks/useTheme.js
@@ -2,9 +2,11 @@ import useLocalStorage from 'shared/hooks/useLocalStorage';
 import { useState, useEffect } from 'react'
 
 function useTheme() {
+  // make a local storage item for the theme
   const [storedTheme, setStoredTheme, ready] = useLocalStorage('theme', undefined);
 
   useEffect(() => {
+    // set the default theme based on user prefence after mounting
     if (ready && storedTheme === undefined) {
       const prefersDarkMode = (typeof window !== 'undefined' && typeof window.matchMedia === 'function')
         ? window.matchMedia(`(prefers-color-scheme: dark`).matches
