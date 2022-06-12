b0aa9f38952992903fd1a59aede669590fb44124
Joost De Cock <joost@joost.at>
2022-06-12 22:37:04 +0200

    chore: Supress webpack warnings about named imports from JSON
    This suppresses a warning in webpack v5 when importing the
    version from the package.json file like a named export.
    
    This use to be fine in webpack v4 but in v5 it's deprecated to
    be more inline with the way it works in NodeJS.
    
    That being said, it's very usefull as there's no need to pull in
    the entire package.json file.
    Futhermore, esbuild supports this out of the box so we also use
    it in our build scripts.
    
    So for now I'm supressing this warning. In FreeSewing v3 we'll
    re-evaluate this.

--- packages/freesewing.dev/next.config.mjs
+++ packages/freesewing.dev/next.config.mjs
@@ -4,4 +4,12 @@ import i18nConfig from './next-i18next.config.js'
 const config = configBuilder('dev')
 config.i18n = i18nConfig.i18n
 
+// Suppress warnings about importing version from package.json
+// We'll deal with it in v3 of FreeSewing
+config.ignoreWarnings = [
+  /only default export is available soon/
+]
+
+
+
 export default config
