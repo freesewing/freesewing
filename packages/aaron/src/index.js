63d7b604a62519d174182ee64724ca7d5c1cbbfd
Joost De Cock <joost@joost.at>
2022-06-14 13:02:13 +0200

    chore(aaron): Migrated to esbuild & removed legacy devenv

--- packages/aaron/src/index.js
+++ packages/aaron/src/index.js
@@ -2,19 +2,24 @@ import freesewing from '@freesewing/core'
 import Brian from '@freesewing/brian'
 import plugins from '@freesewing/plugin-bundle'
 import config from '../config'
+
 // Parts
 import draftBack from './back'
 import draftFront from './front'
 
 // Create design
-const Pattern = new freesewing.Design(config(freesewing), plugins)
+const Aaron = new freesewing.Design(config, plugins)
 
 // Attach draft methods to prototype
-Pattern.prototype.draftBase = function (part) {
+Aaron.prototype.draftBase = function (part) {
   // Getting the base part from Brian
   return new Brian(this.settings).draftBase(part)
 }
-Pattern.prototype.draftFront = (part) => draftFront(part)
-Pattern.prototype.draftBack = (part) => draftBack(part)
+Aaron.prototype.draftFront = (part) => draftFront(part)
+Aaron.prototype.draftBack = (part) => draftBack(part)
 
-export default Pattern
+// Named exports
+export { config, Aaron }
+
+// Default export
+export default Aaron
