1bec1b5fab3bfc1f9517ec897bfaa00be72024b5
anna-puk <100537439+anna-puk@users.noreply.github.com>
2022-06-17 22:28:41 +0200

    add markers for elastic
    cherry-picked from ursula-update branch, with conflicts resolved and superfluous changes removed

--- designs/ursula/src/elastic.js
+++ designs/ursula/src/elastic.js
@@ -28,6 +28,67 @@ export default function (part) {
       y: points.elasticInfo.y + sa + 15,
     })
   }
+  
+  console.log('elastic complete')
+  
+  // determine where the markers should be drawn
+  
+  // waist band: start at center front
+  if (store.get('frontWaistBandLength') >= waistBandLength / 2) {
+    store.set('numWaistMarkersFront',3);
+    store.set('waistMarkerFrac', 0.25 * waistBandLength / store.get('frontWaistBandLength'));
+  } else {
+    store.set('numWaistMarkersFront',1);
+    store.set('waistMarkerFrac', 0.25 * waistBandLength / store.get('backWaistBandLength'));
+  }
+  
+  // leg opening: start at side seam (marker 0, does not count towards total)
+  // leg marker 1: at 25%, on either front or gusset
+  // leg marker 2: at 50%, on front, gusset or back
+  // leg marker 3: at 75%, on gusset or back
+  // for any part, 'legMarker#Frac' gives the relative position along the path *on that part*, measured from front/back to gusset and from front of gusset to back of gusset
+  
+  // markers on front part
+  if (store.get('frontLegOpeningLength') / legOpeningLength < 0.25 ) {
+    store.set('numLegMarkersFront',0); // side seam only (count markers per leg opening)
+  } else {
+    store.set('numLegMarkersFront',1);
+    store.set('legMarker1Frac',0.25 * legOpeningLength / store.get('frontLegOpeningLength'));
+  }
+  if (store.get('frontLegOpeningLength') / legOpeningLength >= 0.5 ) {
+    // seems unlikely: front leg opening is at least 50% of total
+    store.set('numLegMarkersFront',2);
+    store.set('legMarker2Frac',0.5 * legOpeningLength / store.get('frontLegOpeningLength'));
+  }  
+  
+  // markers on gusset
+  store.set('numLegMarkersGusset',0)
+  if (store.get('frontLegOpeningLength') / legOpeningLength < 0.25 & 
+    (store.get('frontLegOpeningLength') + store.get('gussetSideLength')) / legOpeningLength > 0.25) {
+    store.set('numLegMarkersGusset',1);
+    store.set('legMarker1Frac', (0.25 * legOpeningLength - store.get('frontLegOpeningLength')) / store.get('gussetSideLength'))
+  }
+  if (store.get('frontLegOpeningLength') / legOpeningLength < 0.5 & 
+    (store.get('frontLegOpeningLength') + store.get('gussetSideLength')) / legOpeningLength >= 0.5) {
+    // place second marker on gusset
+    store.set('numLegMarkersGusset',1 + store.get('numLegMarkersGusset'));
+    store.set('legMarker2Frac', (0.5 * legOpeningLength - store.get('frontLegOpeningLength')) / store.get('gussetSideLength'))
+  }    
+  if (store.get('frontLegOpeningLength') / legOpeningLength < 0.75 & 
+    (store.get('frontLegOpeningLength') + store.get('gussetSideLength')) / legOpeningLength >= 0.75) {
+    // place second marker on gusset
+    store.set('numLegMarkersGusset',1 + store.get('numLegMarkersGusset'));
+    store.set('legMarker3Frac', (0.75 * legOpeningLength - store.get('frontLegOpeningLength')) / store.get('gussetSideLength'))
+  }        
+  
+  // markers on back part
+  if (store.get('backLegOpeningLength') / legOpeningLength > 0.25 ) {
+    store.set('legMarker3Frac',0.25 * legOpeningLength / store.get('backLegOpeningLength'));
+  }
+  if (store.get('backLegOpeningLength') / legOpeningLength > 0.5 ) {
+    store.set('legMarker2Frac',0.5 * legOpeningLength / store.get('backLegOpeningLength'));
+  }  
+  
 
   return part
 }
