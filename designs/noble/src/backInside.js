174e7d60bb907a248bf0519fc6b34563b4c87687
Wouter van Wageningen <wouter.vdub@yahoo.com>
2022-06-20 20:54:07 +0000

    Paperless.....

--- designs/noble/src/backInside.js
+++ designs/noble/src/backInside.js
@@ -16,26 +16,9 @@ export default function (part) {
     macro,
   } = part.shorthand()
 
-  // delete points.armholePitch
-  // delete points.armholePitchCp1
-  // delete points.armholePitchCp2
-  // delete points.armholeCpTarget
-  // delete points.armholeCp2
-  // delete points.armhole
-  // delete points.bustSide
-  // delete points.waistSide
-  // delete points.waistSideCp2
-  // delete points.dartBottomRight
-  // delete points.dartBottomCenter
-  // delete points.dartRightCp
-  // delete points.bustDartRight
-  
   // Hide Bella paths
   for (let key of Object.keys(paths)) paths[key].render = false
   for (let i in snippets) delete snippets[i]
-  //removing macros not required from Bella
-  
-  console.log( 'Noble back inside' )
     
   paths.insideSeam = new Path()
     .move(points.cbNeck)
@@ -49,7 +32,8 @@ export default function (part) {
     .attr('class', 'fabric')
   
   if (complete) {
-    // points.titleAnchor = points.waistDartRight.shiftFractionTowards( points.armhole, .5 )
+    snippets.dartTip = new Snippet('notch', points.dartTip)
+
     macro('title', {
       at: points.titleAnchor,
       nr: 3,
@@ -61,6 +45,71 @@ export default function (part) {
     })
 
     if (sa) paths.sa = paths.insideSeam.offset(sa).attr('class', 'fabric sa')
+
+    if( paperless ) {
+      macro('hd', {
+        from: points.waistCenter,
+        to: points.shoulderDart,
+        y: points.waistCenter.y +sa + 15,
+      })
+      macro('hd', {
+        from: points.waistCenter,
+        to: points.dartTip,
+        y: points.waistCenter.y +sa + 25,
+      })
+      macro('hd', {
+        from: points.waistCenter,
+        to: points.dartBottomLeft,
+        y: points.waistCenter.y +sa + 35,
+      })
+      macro('hd', {
+        from: points.cbNeck,
+        to: points.dartBottomLeft,
+        y: points.waistCenter.y +sa + 45,
+      })
+      macro('hd', {
+        from: points.cbNeck,
+        to: points.hps,
+        y: points.hps.y -sa - 15,
+      })
+      macro('hd', {
+        from: points.hps,
+        to: points.shoulderDart,
+        y: points.hps.y -sa - 15,
+      })
+
+      macro('vd', {
+        from: points.shoulderDart,
+        to: points.dartTip,
+        x: points.shoulderDart.x +sa + 15,
+      })
+      macro('vd', {
+        from: points.shoulderDart,
+        to: points.dartBottomLeft,
+        x: points.shoulderDart.x +sa + 25,
+      })
+      macro('vd', {
+        from: points.shoulderDart,
+        to: points.waistCenter,
+        x: points.shoulderDart.x +sa + 35,
+      })
+      macro('vd', {
+        from: points.hps,
+        to: points.waistCenter,
+        x: points.shoulderDart.x +sa + 45,
+      })
+      macro('vd', {
+        from: points.waistCenter,
+        to: points.cbNeck,
+        x: points.cbNeck.x -sa - 15,
+      })
+      macro('vd', {
+        from: points.waistCenter,
+        to: points.hps,
+        x: points.cbNeck.x -sa - 25,
+      })
+
+    }
   }
   
   return part
--- designs/noble/src/backOutside.js
+++ designs/noble/src/backOutside.js
@@ -17,10 +17,12 @@ export default function (part) {
   } = part.shorthand()
 
   
-
-  console.log( 'Noble back outside' )
-
-  console.log( {part:part})
+  paths.dart = new Path()
+    .move(points.shoulderDart)
+    .curve(points.shoulderDart, points.shoulderDartCpUp, points.dartTip)
+    .curve(points.shoulderDartCpDown, points.dartRightCp, points.dartBottomRight)
+    .setRender(false)
+  
   paths.outsideSeam = new Path()
     .move(points.dartBottomRight)
     .line(points.waistSide)
@@ -28,13 +30,14 @@ export default function (part) {
     .curve(points.armholeCp2, points.armholePitchCp1, points.armholePitch)
     .curve_(points.armholePitchCp2, points.shoulder)
     .line(points.shoulderDart)
-    .curve(points.shoulderDart, points.shoulderDartCpUp, points.dartTip)
-    .curve(points.shoulderDartCpDown, points.dartRightCp, points.dartBottomRight)
+    .join(paths.dart)
     .close()
     .attr('class', 'fabric')
   
     if (complete) {
-      points.titleAnchor = points.dartBottomRight.shiftFractionTowards( points.waistSide, .35 ).shiftFractionTowards( points.shoulder, .35 )
+    snippets.dartTip = new Snippet('notch', points.dartTip)
+    
+    points.titleAnchor = points.dartBottomRight.shiftFractionTowards( points.waistSide, .1 ).shiftFractionTowards( points.shoulder, .3 )
       macro('title', {
         at: points.titleAnchor,
         nr: 4,
@@ -49,7 +52,92 @@ export default function (part) {
       })
   
       if (sa) paths.sa = paths.outsideSeam.offset(sa).attr('class', 'fabric sa')
-
+      if( paperless ) {
+        let pLeft = paths.dart.edge('left')
+        // macro('hd', {
+        //   from: points.dartBottomRight,
+        //   to: points.shoulderDart,
+        //   y: points.waistCenter.y +sa + 15,
+        // })
+        macro('hd', {
+          from: pLeft,
+          to: points.waistSide,
+          y: points.waistCenter.y +sa + 15,
+        })
+        macro('hd', {
+          from: points.dartBottomRight,
+          to: points.armhole,
+          y: points.waistCenter.y +sa + 25,
+        })
+        macro('hd', {
+          from: points.dartTip,
+          to: points.waistSide,
+          y: points.waistCenter.y +sa + 35,
+        })
+        macro('hd', {
+          from: points.dartBottomRight,
+          to: points.waistSide,
+          y: points.waistCenter.y +sa + 45,
+        })
+        // macro('hd', {
+        //   from: points.cbNeck,
+        //   to: points.waistSide,
+        //   y: points.waistCenter.y +sa + 45,
+        // })
+        macro('hd', {
+          from: pLeft,
+          to: points.shoulder,
+          y: points.shoulderDart.y -sa - 15,
+        })
+        macro('hd', {
+          from: points.shoulderDart,
+          to: points.shoulder,
+          y: points.shoulderDart.y -sa - 25,
+        })
+        macro('hd', {
+          from: points.shoulderDart,
+          to: points.armhole,
+          y: points.shoulderDart.y -sa - 35,
+        })
+  
+        macro('vd', {
+          from: points.shoulder,
+          to: points.dartTip,
+          x: points.armhole.x +sa + 15,
+        })
+        macro('vd', {
+          from: points.armhole,
+          to: points.waistSide,
+          x: points.armhole.x +sa + 15,
+        })
+        macro('vd', {
+          from: points.shoulder,
+          to: points.waistSide,
+          x: points.armhole.x +sa + 25,
+        })
+        macro('vd', {
+          from: points.shoulder,
+          to: points.dartBottomRight,
+          x: points.armhole.x +sa + 35,
+        })
+        macro('vd', {
+          from: points.shoulderDart,
+          to: points.dartBottomRight,
+          x: points.armhole.x +sa + 45,
+        })
+        // macro('vd', {
+        //   from: points.waistCenter,
+        //   to: points.cbNeck,
+        //   x: points.cbNeck.x -sa - 15,
+        // })
+        // macro('vd', {
+        //   from: points.waistCenter,
+        //   to: points.hps,
+        //   x: points.cbNeck.x -sa - 25,
+        // })
+  
+      }
+  
     }
     
     return part
