3d863ba0a14f9e0905d7a37a00f580d8a373e399
Joost De Cock <joost@joost.at>
2022-06-21 19:47:42 +0200

    chore(noble): Added tests

---
+++ designs/noble/tests/shared.test.mjs
@@ -0,0 +1,41 @@
+// This file is auto-generated.
+// Changes you make will be overwritten.
+import chai from 'chai'
+import models from '@freesewing/models'
+import patterns from '@freesewing/pattern-info'
+import Noble from './dist/index.mjs'
+
+// Shared tests
+import { testPatternConfig } from '../../../tests/patterns/config.mjs'
+import { testPatternDrafting } from '../../../tests/patterns/drafting.mjs'
+import { testPatternSampling } from '../../../tests/patterns/sampling.mjs'
+
+const expect = chai.expect
+
+
+// Test config
+testPatternConfig(
+  'noble',
+  new Noble(),
+  expect,
+  models,
+  patterns
+)
+
+// Test drafting
+testPatternDrafting(
+  'noble',
+  Noble,
+  expect,
+  models,
+  patterns
+)
+
+// Test sampling
+testPatternSampling(
+  'noble',
+  Noble,
+  expect,
+  models,
+  patterns
+)
