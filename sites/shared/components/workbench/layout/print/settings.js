f0d3f21070df22f50b556129bcddc4afeaede7d3
Joost De Cock <joost@joost.at>
2022-06-29 13:07:44 +0200

    chore(shared): Style reset button consistent with other buttons

--- sites/shared/components/workbench/layout/print/settings.js
+++ sites/shared/components/workbench/layout/print/settings.js
@@ -2,6 +2,7 @@ import PageSizePicker from './pagesize-picker'
 import OrientationPicker from './orientation-picker'
 import PrintIcon from 'shared/components/icons/print'
 import RightIcon from 'shared/components/icons/right'
+import ClearIcon from 'shared/components/icons/clear'
 import { useTranslation } from 'next-i18next'
 
 const PrintLayoutSettings = props => {
@@ -13,15 +14,6 @@ const PrintLayoutSettings = props => {
     <div className="flex flex-row gap-8 justify-center">
       <PageSizePicker {...props} />
       <OrientationPicker {...props} />
-      <button
-        key="reset"
-        onClick={() => props.unsetGist('layout')}
-        className="btn btn-primary"
-      >
-        <span className="capitalize">
-          {t('reset')}
-        </span>
-      </button>
       <div className="flex flex-row font-bold items-center px-0 text-xl">
         <PrintIcon />
         <span className="ml-2">{count}</span>
@@ -32,6 +24,14 @@ const PrintLayoutSettings = props => {
         <div className="rotate-90"><RightIcon /></div>
         <span className="text-xl ml-2">{rows}</span>
       </div>
+      <button
+        key="reset"
+        onClick={() => props.unsetGist('layout')}
+        className="btn btn-primary btn-outline"
+      >
+        <ClearIcon className="h-6 w-6 mr-2"/>
+        {t('reset')}
+      </button>
     </div>
   )
 }
