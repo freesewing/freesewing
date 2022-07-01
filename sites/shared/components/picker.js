c1b53465cfbb72b5e55d10583ec4181aa343e302
Enoch Riese <enoch.riese@gmail.com>
2022-07-01 15:58:39 -0500

    local and theme pickers working with headless ui

---
+++ sites/shared/components/picker.js
@@ -0,0 +1,56 @@
+import {useRef, forwardRef} from 'react'
+import { Menu } from '@headlessui/react'
+import Link from 'next/link'
+
+const Picker = ({Icon, className, selectedItem, iconOnly=false, children}) => {
+
+	return (<Menu as="div" className={`dropdown dropdown-end ${className} w-auto`}>
+		<Menu.Button className={iconOnly
+			? `btn btn-sm`
+			: `m-0 btn btn-neutral flex flex-row gap-2 btn-outline
+			md:btn-ghost
+			hover:bg-neutral hover:border-neutral-content
+			`}
+			aria-label="Choose Theme">
+			<Icon />
+			{!iconOnly && <span>{selectedItem}</span>}
+			</Menu.Button>
+		<Menu.Items as="ul" className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+		 {children}
+		</Menu.Items>
+	</Menu>)
+}
+
+export default Picker
+
+const itemClass = (active) => "btn btn-ghost " + (active ? 'bg-base-200' : '')
+
+export const PickerLink = (props) => {
+	return (<li role="menuitem">
+		<Menu.Item>
+			{({active}) => (<ForwardLink active={active} {...props}></ForwardLink>)}
+		</Menu.Item>
+	</li>)
+}
+
+const ForwardLink = forwardRef(({href, locale, active, children, ...rest}, ref) => (
+	<Link href={href} locale={locale}>
+  	<a className={itemClass(active)} {...rest} role={undefined} ref={ref}>
+  		<span className="text-base-content">
+    		{children}
+  		</span>
+  	</a>
+	</Link>
+))
+
+export const PickerButton = ({onClick, children}) => {
+	return (<Menu.Item as="li" onClick={onClick}>
+    {({ active }) => (
+      <button className={itemClass(active)}>
+      	<span className="text-base-content">
+      		{children}
+      	</span>
+      </button>
+    )}
+  </Menu.Item>)
+}
--- sites/shared/components/theme-picker.js
+++ sites/shared/components/theme-picker.js
@@ -1,41 +1,67 @@
 import themes from 'shared/themes/index.js'
 import ThemeIcon from 'shared/components/icons/theme.js'
+import {useRef} from 'react'
 import { useTranslation } from 'next-i18next'
+import Picker, {PickerButton} from './picker';
+import { Menu } from '@headlessui/react'
 
 const ThemePicker = ({ app, className, iconOnly=false }) => {
   const { t } = useTranslation(['themes'])
 
-  return (
-      <div className={`dropdown dropdown-end ${className} w-auto`}>
-        <div tabIndex="0" className={iconOnly
-         ? `btn btn-sm`
-         : `m-0 btn btn-neutral flex flex-row gap-2 btn-outline
-            md:btn-ghost
-            hover:bg-neutral hover:border-neutral-content
-          `}
-        >
-          <ThemeIcon />
-          {!iconOnly && <span>{t(`${app.theme}Theme`)}</span>}
-        </div>
-        <ul
-          tabIndex="0"
-          className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
-        >
-          {Object.keys(themes).map(theme => (
-            <li key={theme}>
-              <button
-                onClick={() => app.setTheme(theme)}
-                className="btn btn-ghost hover:bg-base-200"
-              >
-                <span className="text-base-content">
-                  {t(`${theme}Theme`)}
-                </span>
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-  )
+  const items = {};
+  for (var k in themes){
+    items[k] = {...themes[k], key: k}
+  }
+
+  const pickerProps = {
+    className,
+    iconOnly,
+    Icon: ThemeIcon,
+    selectedItem: t(`${app.theme}Theme`)
+  }
+  return (<Picker {...pickerProps}>
+    {Object.keys(themes).map(theme => (
+      <PickerButton onClick={() =>
+      app.setTheme(theme)} key={theme}>
+        {t(`${theme}Theme`)}
+      </PickerButton>
+    ))}
+  </Picker>)
+
+
+
+  // return (
+  //     <div className={`dropdown dropdown-end ${className} w-auto`}>
+  //       <div tabIndex="0" className={iconOnly
+  //        ? `btn btn-sm`
+  //        : `m-0 btn btn-neutral flex flex-row gap-2 btn-outline
+  //           md:btn-ghost
+  //           hover:bg-neutral hover:border-neutral-content
+  //         `}
+  //       >
+  //         <ThemeIcon />
+  //         {!iconOnly && <span>{t(`${app.theme}Theme`)}</span>}
+  //       </div>
+  //       <ul
+  //         tabIndex="0"
+  //         className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+  //       >
+  //         {Object.keys(themes).map(theme => (
+  //           <li key={theme}>
+  //             <button
+  //               onClick={() => app.setTheme(theme)}
+  //               className="btn btn-ghost hover:bg-base-200"
+  //             >
+  //               <span className="text-base-content">
+  //                 {t(`${theme}Theme`)}
+  //               </span>
+  //             </button>
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  // )
+  // return null;
 }
 
 export default ThemePicker
