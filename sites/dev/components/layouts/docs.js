9131f736b27b8fa97d58b9fb1a3cb10b4e5c6903
Joost De Cock <joost@joost.at>
2022-07-02 22:55:41 +0200

    fix(dev): Remove theme picker from sidebar

--- sites/dev/components/layouts/docs.js
+++ sites/dev/components/layouts/docs.js
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 // Shared components
 import Aside from 'shared/components/navigation/aside'
-import ThemePicker from 'shared/components/theme-picker'
 import Breadcrumbs from 'shared/components/breadcrumbs.js'
 import { getCrumbs } from 'shared/utils'
 
@@ -15,7 +14,7 @@ const DefaultLayout = ({ app, title=false, crumbs=false, children=[] }) => {
 
   return (
     <div className="m-auto flex flex-row justify-center">
-      <Aside app={app} slug={slug} before={<ThemePicker app={app} className="block sm:hidden"/>}/>
+      <Aside app={app} slug={slug}/>
       <section className="py-28 md:py-36 max-w-7xl px-6 xl:pl-8 2xl:pl-16">
         <div>
           {title && (
