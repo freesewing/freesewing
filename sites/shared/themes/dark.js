8875b82878f1d3c21b6cb5a993bd28aac3d7837a
Enoch Riese <enoch.riese@gmail.com>
2022-07-01 21:30:39 -0500

    increase contrast with neutral focus color. undo hacky workaround

--- sites/shared/themes/dark.js
+++ sites/shared/themes/dark.js
@@ -16,7 +16,7 @@ module.exports = {
   'accent-content': colors.pink['50'],
 
   'neutral': '#000000', // Dark as my soul
-  'neutral-focus': colors.neutral['800'],
+  'neutral-focus': colors.neutral['300'],
   'neutral-content': colors.neutral['50'],
 
   'base-100': colors.neutral['900'],
