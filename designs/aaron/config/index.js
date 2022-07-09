757c1a0d92e59593f81761656a4a69ad9976c06e
Joost De Cock <joost@joost.at>
2022-07-09 17:24:28 +0200

    feat(aaron): Add support for high-bust draft. See #2386

--- designs/aaron/config/index.js
+++ designs/aaron/config/index.js
@@ -11,7 +11,7 @@ export default {
   type: 'pattern',
   difficulty: 2,
   optionGroups: {
-    fit: ['chestEase', 'hipsEase', 'stretchFactor'],
+    fit: ['chestEase', 'hipsEase', 'stretchFactor', 'draftForHighBust'],
     style: [
       'armholeDrop',
       'backlineBend',
@@ -65,5 +65,8 @@ export default {
     stretchFactor: { pct: 5, min: 0, max: 15 },
     shoulderStrapWidth: { pct: 15, min: 10, max: 40 },
     shoulderStrapPlacement: { pct: 40, min: 20, max: 80 },
+
+    // draft for high bust
+    draftForHighBust: { bool: false },
   },
 }
--- designs/aaron/src/index.js
+++ designs/aaron/src/index.js
@@ -1,14 +1,27 @@
 import freesewing from '@freesewing/core'
 import Brian from '@freesewing/brian'
 import plugins from '@freesewing/plugin-bundle'
+import plugin from '@freesewing/plugin-bust' // Note: conditional plugin
 import config from '../config'
 
 // Parts
 import draftBack from './back'
 import draftFront from './front'
 
+/* Check to see whether we should load the bust plugin
+ * Only of the `draftForHighBust` options is set
+ * AND the highBust measurement is available
+ */
+const condition = (settings = false) =>
+  settings &&
+  settings.options &&
+  settings.options.draftForHighBust &&
+  settings.measurements.highBust
+    ? true
+    : false
+
 // Create design
-const Aaron = new freesewing.Design(config, plugins)
+const Aaron = new freesewing.Design(config, plugins, { plugin, condition })
 
 // Attach draft methods to prototype
 Aaron.prototype.draftBase = function (part) {
