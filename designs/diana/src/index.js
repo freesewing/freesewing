eb83ff81dd3cd177ed6d4111bf9bc0dd467a0151
Joost De Cock <joost@joost.at>
2022-07-09 17:36:29 +0200

    feat(diana): Add support for high-bust draft. See #2386

--- designs/diana/src/index.js
+++ designs/diana/src/index.js
@@ -1,13 +1,26 @@
 import freesewing from '@freesewing/core'
 import Brian from '@freesewing/brian'
 import plugins from '@freesewing/plugin-bundle'
+import plugin from '@freesewing/plugin-bust' // Note: conditional plugin
 import config from '../config'
 // Parts
 import draftFront from './front'
 import draftSleeve from './sleeve'
 
+/* Check to see whether we should load the bust plugin
+ * Only of the `draftForHighBust` options is set
+ * AND the highBust measurement is available
+ */
+const condition = (settings = false) =>
+  settings &&
+  settings.options &&
+  settings.options.draftForHighBust &&
+  settings.measurements.highBust
+    ? true
+    : false
+
 // Create design
-const Diana = new freesewing.Design(config, plugins)
+const Diana = new freesewing.Design(config, plugins, { plugin, condition })
 
 // Attach draft methods to prototype
 Diana.prototype.draftBase = function (part) {
