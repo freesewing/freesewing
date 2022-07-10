08ce5fd8020b9c80ca58008c2812e82844e572e9
Joost De Cock <joost@joost.at>
2022-07-10 12:32:52 +0200

    fix: Add trailing newline to options images insert

--- scripts/optiondocsimages.js
+++ scripts/optiondocsimages.js
@@ -25,7 +25,8 @@ theme = theme.default
 const image = (pattern, option) => `
 
 ## Effect of this option on the pattern
-![This image shows the effect of this option by superimposing several variants that have a different value for this option](${pattern}_${option.toLowerCase()}_sample.svg "Effect of this option on the pattern")`
+![This image shows the effect of this option by superimposing several variants that have a different value for this option](${pattern}_${option.toLowerCase()}_sample.svg "Effect of this option on the pattern")
+`
 
 
 const insertImage = (file, pattern, option) => {
