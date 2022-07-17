0b18a3e1c50a649d1935561f909f8ba0cf47a295
Enoch Riese <enoch.riese@gmail.com>
2022-07-17 15:03:27 -0400

    add stuff that was mysteriously missing from error-boundary

--- sites/shared/components/error/error-boundary.js
+++ sites/shared/components/error/error-boundary.js
@@ -26,7 +26,7 @@ class ErrorBoundary extends React.Component {
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, error };
   }
 
   componentDidCatch(error, errorInfo) {
