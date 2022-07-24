e7391db9803ff368393d4157df7508c19b417803
joostdecock <joost@joost.at>
2022-07-24 21:46:04 +0200

    fix(simon): Forgot about notch. See #2400

--- designs/simon/src/yoke.js
+++ designs/simon/src/yoke.js
@@ -29,6 +29,8 @@ export default (part) => {
     delete snippets.armholePitchNotch
     delete snippets.collarNotch
     delete snippets.shoulderNotch
+    delete snippets.shoulderNotch
+    snippets.sleevecapNotch = new Snippet('notch', points.armholeYokeSplitPreBoxpleat)
     points.title = new Point(points.neck.x, points.cbYoke.y / 3)
     macro('title', { at: points.title, nr: 4, title: 'yoke', scale: 0.8 })
     points.logo = points.title.shift(-90, 50)
