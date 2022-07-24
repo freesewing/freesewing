e11489349bf9ea935d68c4682f271e0c91cf30e8
Enoch Riese <enoch.riese@gmail.com>
2022-07-23 23:16:25 -0500

    copy locale files to the sites in prebuild

--- sites/shared/prebuild/i18n.mjs
+++ sites/shared/prebuild/i18n.mjs
@@ -1,5 +1,40 @@
 import {build} from '../../../packages/i18n/src/prebuild.mjs'
+import {denyList} from '../../../packages/i18n/scripts/prebuilder.mjs'
+import fs from 'fs'
+import path from 'path'
+import {mkdir} from 'fs/promises'
 
 export const prebuildI18n = async(site, only=false) => {
-  build((loc) => site !== 'dev' || loc  === 'en', only)
+  const writeJson = async (locale, namespace, content) => fs.writeFileSync(
+      path.resolve(
+        '..',
+        site,
+        'public',
+        'locales',
+        locale,
+        `${namespace}.json`
+      ),
+      JSON.stringify(content)
+    )
+
+  const filter = site === 'dev' ? (loc => lock === 'en') : (loc => denyList.indexOf(loc) === -1)
+  const locales = await build(filter, only)
+
+  console.log (`copying them to ${site}`, Object.keys(locales))
+
+  const languages = {}
+  Object.keys(locales).forEach(l => languages[l] = locales[l].i18n[l])
+  for (const locale in locales) {
+    // Only English for dev site
+    const loc = locales[locale]
+    // Fan out into namespaces
+    for (const namespace in loc) {
+      writeJson(
+        locale, namespace,
+        loc[namespace]
+      )
+    }
+
+    writeJson(locale, 'locales', languages)
+  }
 }
