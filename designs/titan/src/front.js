1cdac0731bccc3479e43d80f04e6b40702ad0697
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2022-08-10 19:08:41 +0100

    change logo anchors y

--- designs/titan/src/front.js
+++ designs/titan/src/front.js
@@ -295,7 +295,7 @@ export default (part) => {
       from: points.grainlineTop,
       to: points.grainlineBottom,
     })
-    points.logoAnchor = new Point(points.crotchSeamCurveStart.x / 2, points.crotchSeamCurveStart.y)
+    points.logoAnchor = new Point(points.crotchSeamCurveStart.x / 2, points.fork.y)
     snippets.logo = new Snippet('logo', points.logoAnchor)
     points.titleAnchor = points.logoAnchor.shift(-90, 60)
     macro('title', {
