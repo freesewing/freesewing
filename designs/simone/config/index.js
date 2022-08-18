bea7b36b0e59cc2dfda4cecc3065df900ac78f80
Benjamin F <example@example.com>
2022-08-18 14:32:43 -0700

    Move Bust-aligned Buttons option into Style->Closure submenu.

--- designs/simone/config/index.js
+++ designs/simone/config/index.js
@@ -1,18 +1,31 @@
 import { version } from '../package.json'
 import { config as simonConfig } from '@freesewing/simon'
 
+// Add the Bust-aligned Buttons option into the "Style->Closure" submenu.
+// - Because the closure submenu is an object in the style array,
+//   we cannot simply replace/overwrite it by adding a new closure
+//   submenu. Doing so would just create a duplicate "Closure"
+//   submenu.
+// - Instead, we would need to remove the existing closure submenu
+//   object from simonConfig.optionGroups.style first, before adding
+//   the new closure submenu. (Doing it this way would also have the
+//   side effect of changing the position of the "Closure" submenu
+//   within the main"Style" menu.)
+// - Alternately, we can simply modify the existing closure submenu
+//   object in place, and that is what we are doing here: injecting the
+//   new 'bustAlignedButtons' option directly into the closure submenu
+//   array. This seems to be the least disruptive method.
+const closureindex = simonConfig.optionGroups.style.findIndex((item) =>
+    typeof item === 'object' && "closure" in item)
+simonConfig.optionGroups.style[closureindex].closure.push('bustAlignedButtons')
+
 const config = {
   version,
   ...simonConfig,
   name: 'simone',
   optionGroups: {
     ...simonConfig.optionGroups,
-    style: [
-        ...simonConfig.optionGroups.style,
-        'frontDarts',
-        'contour',
-        'bustAlignedButtons',
-    ],
+    style: [...simonConfig.optionGroups.style, 'frontDarts', 'contour'],
     advanced: [
       ...simonConfig.optionGroups.advanced,
       'bustDartAngle',
