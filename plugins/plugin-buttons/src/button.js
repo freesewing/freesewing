a93488572391a3a05be903b51960f93a71677a87
Enoch Riese <enoch.riese@gmail.com>
2022-08-23 11:10:15 -0500

    mix inline and class styling for buttons and buttonholes

--- plugins/plugin-buttons/src/button.js
+++ plugins/plugin-buttons/src/button.js
@@ -2,10 +2,11 @@ export default `
 <g id="button">
   <circle
     cx="0" cy="0" r="3.4"
+    style="stroke:var(--pattern-mark);fill:none;stroke-width:var(--pattern-stroke);"
     class="mark"
   />
-  <circle cx="-1" cy="-1" r="0.5" class="no-stroke fill-mark"/>
-  <circle cx="1"  cy="-1" r="0.5" class="no-stroke fill-mark" />
-  <circle cx="1"  cy="1"  r="0.5" class="no-stroke fill-mark" />
-  <circle cx="-1" cy="1"  r="0.5" class="no-stroke fill-mark" />
+  <circle cx="-1" cy="-1" r="0.5" class="no-stroke fill-mark" style="stroke:none;fill:var(--pattern-mark)"/>
+  <circle cx="1"  cy="-1" r="0.5" class="no-stroke fill-mark" style="stroke:none;fill:var(--pattern-mark)" />
+  <circle cx="1"  cy="1"  r="0.5" class="no-stroke fill-mark" style="stroke:none;fill:var(--pattern-mark)" />
+  <circle cx="-1" cy="1"  r="0.5" class="no-stroke fill-mark" style="stroke:none;fill:var(--pattern-mark)" />
 </g>`
--- plugins/plugin-buttons/src/buttonhole.js
+++ plugins/plugin-buttons/src/buttonhole.js
@@ -3,17 +3,20 @@ export default `
   <path
     class="mark"
     d="M -1,-5 L 1,-5 L 1,5 L -1,5 z"
+    style="fill:none;stroke:var(--pattern-mark);stroke-width:var(--pattern-stroke);"
   />
 </g>
 <g id="buttonhole-start">
   <path
     class="mark"
     d="M -1,-10 L 1,-10 L 1,0 L -1,0 z"
+    style="fill:none;stroke:var(--pattern-mark);stroke-width:var(--pattern-stroke);"
   />
 </g>
 <g id="buttonhole-end">
   <path
     class="mark"
     d="M -1,0 L 1,0 L 1,10 L -1,10 z"
+    style="fill:none;stroke:var(--pattern-mark);stroke-width:var(--pattern-stroke);"
   />
 </g>`
