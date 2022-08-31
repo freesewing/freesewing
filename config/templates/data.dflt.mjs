764d1f4de9e964b2f7053f5b7fab109629937133
Joost De Cock <joost@joost.at>
2022-08-31 20:22:20 +0200

    chore: Updated data template to have indivudual named exports

--- config/templates/data.dflt.mjs
+++ config/templates/data.dflt.mjs
@@ -1,6 +1,4 @@
 // This file is auto-generated | All changes you make will be overwritten.
-export const data = {
-  name: "{{{ name }}}",
-  version: "{{ version }}",
-}
-
+export const name = "{{{ name }}}"
+export const version = "{{ version }}"
+export const data = { name, version }
