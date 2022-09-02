4d7315a33ea48636c96e3906da98e4f974b01c91
Benjamin F <example@example.com>
2022-09-02 07:12:34 -0700

    Port Titan to v3.

--- designs/titan/src/index.mjs
+++ designs/titan/src/index.mjs
@@ -1,19 +1,13 @@
-import freesewing from '@freesewing/core'
-import plugins from '@freesewing/plugin-bundle'
-import config from '../config'
-// Parts
-import draftBack from './back'
-import draftFront from './front'
+import { Design } from '@freesewing/core'
+import { data } from '../data.mjs'
+import { back } from './back.mjs'
+import { front } from './front.mjs'
 
 // Create design
-const Titan = new freesewing.Design(config, plugins)
-
-// Attach draft methods to prototype
-Titan.prototype.draftBack = (part) => draftBack(part)
-Titan.prototype.draftFront = (part) => draftFront(part)
+const Titan = new Design({
+  data,
+  parts: [ back, front ],
+})
 
 // Named exports
-export { config, Titan }
-
-// Default export
-export default Titan
+export { back, front, Titan }
