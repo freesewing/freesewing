2f7865d4d75464c1bdf43b0b8c5ef825a7e6591f
Benjamin F <example@example.com>
2022-09-05 11:47:05 -0700

    Port Diana to v3.

--- designs/diana/src/index.mjs
+++ designs/diana/src/index.mjs
@@ -1,47 +1,25 @@
-import freesewing from '@freesewing/core'
-import Brian from '@freesewing/brian'
-import plugins from '@freesewing/plugin-bundle'
-import plugin from '@freesewing/plugin-bust' // Note: conditional plugin
-import config from '../config'
+import { Design } from '@freesewing/core'
+import { data } from '../data.mjs'
+// Inherited Parts
+import { front as frontBase } from '@freesewing/brian'
+import { back as backBase } from '@freesewing/brian'
+import { sleeve as sleeveBase } from '@freesewing/brian'
 // Parts
-import draftFront from './front'
-import draftSleeve from './sleeve'
-
-/* Check to see whether we should load the bust plugin
- * Only of the `draftForHighBust` options is set
- * AND the highBust measurement is available
- */
-const condition = (settings = false) =>
-  settings &&
-  settings.options &&
-  settings.options.draftForHighBust &&
-  settings.measurements.highBust
-    ? true
-    : false
+import { front } from './front.mjs'
+import { back } from './front.mjs'
+import { sleeve } from './sleeve.mjs'
 
 // Create design
-const Diana = new freesewing.Design(config, plugins, { plugin, condition })
-
-// Attach draft methods to prototype
-Diana.prototype.draftBase = function (part) {
-  return new Brian(this.settings).draftBase(part)
-}
-Diana.prototype.draftFrontBase = function (part) {
-  return new Brian(this.settings).draftFront(part)
-}
-Diana.prototype.draftBackBase = function (part) {
-  return new Brian(this.settings).draftBack(part)
-}
-Diana.prototype.draftSleeveBase = function (part) {
-  let brian = new Brian(this.settings)
-  return brian.draftSleeve(brian.draftSleevecap(part))
-}
-Diana.prototype.draftFront = draftFront
-Diana.prototype.draftBack = draftFront
-Diana.prototype.draftSleeve = draftSleeve
+const Diana = new Design({
+  data,
+  parts: [
+    front, back, sleeve,
+  ],
+})
 
 // Named exports
-export { config, Diana }
-
-// Default export
-export default Diana
+export {
+  front, back, sleeve,
+  frontBase, backBase, sleeveBase,
+  Diana,
+}
