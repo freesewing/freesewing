8db899587dcfb86d1cddfb8f8a85a34e20165301
Benjamin F <example@example.com>
2022-09-10 09:58:51 -0700

    Add box around elastic text, and remove dimensions.

--- designs/ursula/src/elastic.mjs
+++ designs/ursula/src/elastic.mjs
@@ -1,7 +1,8 @@
 import { gusset } from './gusset.mjs'
 
 function ursulaElastic(part) {
-  const { options, Point, points, store, utils, units, sa, paperless, macro } = part.shorthand()
+  const { options, Point, points, Path, paths, store, utils, units,
+    sa, paperless, macro } = part.shorthand()
 
   // Stretch utility method
   store.set('elasticScale', utils.stretchToScale(options.elasticStretch))
@@ -22,14 +23,13 @@ function ursulaElastic(part) {
     .attr('data-text', ':')
     .attr('data-text', units(waistBandLength * store.get('elasticScale') + 2 * sa))
 
-  // Paperless?
-  if (paperless) {
-    macro('hd', {
-      from: points.elasticInfo,
-      to: points.elasticInfo,
-      y: points.elasticInfo.y + sa + 15,
-    })
-  }
+  // Draw a box around the text, so the part shows up correctly.
+  paths.box = new Path()
+    .move(new Point(-10,-10))
+    .line(new Point(-10,15))
+    .line(new Point(200,15))
+    .line(new Point(200,-10))
+    .close()
 
   return part
 }
