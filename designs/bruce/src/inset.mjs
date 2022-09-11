e70b80725c528fe2b9e5e5f2af30cdea6394e565
joostdecock <joost@joost.at>
2022-09-11 12:42:23 +0200

    chore(bruce): Port to v3 stage 2

--- designs/bruce/src/inset.mjs
+++ designs/bruce/src/inset.mjs
@@ -1,10 +1,20 @@
 import { init } from './init.mjs'
 import { back } from './back.mjs'
 
-function draftBruceInset (part) {
-  let { store, sa, Point, points, Path, paths, complete, paperless, macro, Snippet, snippets } =
-    part.shorthand()
-
+function draftBruceInset({
+  store,
+  sa,
+  Point,
+  points,
+  Path,
+  paths,
+  complete,
+  paperless,
+  macro,
+  Snippet,
+  snippets,
+  part,
+}) {
   // Initialize
   init(part)
 
