34cd9ae1aacab8200b5cc71b157b3c1a3ac68a6c
joostdecock <joost@joost.at>
2022-09-25 15:03:09 +0200

    chore(plugin-bust): Changes for v3. See #2856

--- plugins/plugin-bust/tests/plugin.test.mjs
+++ plugins/plugin-bust/tests/plugin.test.mjs
@@ -15,21 +15,20 @@ pattern.draft()
 
 describe('Bust plugin Tests', () => {
   it('Should set swap the chest measurements', () => {
-    expect(pattern.settings.measurements.bust).to.equal(100)
-    expect(pattern.settings.measurements.chest).to.equal(90)
+    expect(pattern.settings[0].measurements.bust).to.equal(100)
+    expect(pattern.settings[0].measurements.chest).to.equal(90)
   })
 
   it('Should copy measurement from chest to bust and from highBust to chest', function () {
     const testPattern = new Design({
       measurements: {},
-      plugins: [plugin],
     })
-    const pattern = new testPattern()
+    const pattern = new testPattern().use(plugin)
     const userMeasurements = { chest: 50, highBust: 60 }
-    pattern.settings.measurements = userMeasurements
+    pattern.settings[0].measurements = userMeasurements
     pattern.draft()
-    expect(pattern.settings.measurements.bust).to.equal(50)
-    expect(pattern.settings.measurements.chest).to.equal(60)
+    expect(pattern.settings[0].measurements.bust).to.equal(50)
+    expect(pattern.settings[0].measurements.chest).to.equal(60)
   })
 
   it('Should not overwrite existing bust measurements', function () {
@@ -37,9 +36,9 @@ describe('Bust plugin Tests', () => {
     const testPattern = new Design(config, plugin)
     let pattern = new testPattern()
     let userMeasurements = { chest: 50, highBust: 60, bust: 55 }
-    pattern.settings.measurements = userMeasurements
+    pattern.settings[0].measurements = userMeasurements
     pattern.draft()
-    expect(pattern.settings.measurements.bust).to.equal(55)
-    expect(pattern.settings.measurements.chest).to.equal(50)
+    expect(pattern.settings[0].measurements.bust).to.equal(55)
+    expect(pattern.settings[0].measurements.chest).to.equal(50)
   })
 })
