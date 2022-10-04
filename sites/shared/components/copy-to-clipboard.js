a2bf3926c4aeb82a4a2767c65617ecd7cf025296
Joost De Cock <joost@joost.at>
2022-10-04 23:44:06 +0200

    chore(shared): Updated copy-to-clipboard to revert

--- sites/shared/components/copy-to-clipboard.js
+++ sites/shared/components/copy-to-clipboard.js
@@ -1,29 +1,31 @@
 import ReactDOMServer from 'react-dom/server'
 import { useState } from 'react'
 import CopyIcon from 'shared/components/icons/copy.js'
-import {CopyToClipboard} from 'react-copy-to-clipboard'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const strip = html => (typeof DOMParser === 'undefined')
-  ? html
-  : new DOMParser()
-    .parseFromString(html, 'text/html')
-    .body.textContent || ""
+const strip = (html) =>
+  typeof DOMParser === 'undefined'
+    ? html
+    : new DOMParser().parseFromString(html, 'text/html').body.textContent || ''
 
+const handleCopied = (setCopied) => {
+  setCopied(true)
+  setTimeout(() => setCopied(false), 1000)
+}
 
 const CopyToClipboardIcon = ({ content }) => {
-
   const [copied, setCopied] = useState(false)
 
-  const text = (typeof content === 'string')
-    ? content
-    : strip(ReactDOMServer.renderToStaticMarkup(content))
+  const text =
+    typeof content === 'string' ? content : strip(ReactDOMServer.renderToStaticMarkup(content))
 
   return (
-    <CopyToClipboard text={text} onCopy={() => setCopied(true)}>
-      <button className={copied ? 'text-success' : ''}><CopyIcon /></button>
+    <CopyToClipboard text={text} onCopy={() => handleCopied(setCopied)}>
+      <button className={copied ? 'text-success' : ''}>
+        <CopyIcon className="w-5 h-5" />
+      </button>
     </CopyToClipboard>
   )
 }
 
 export default CopyToClipboardIcon
-
