aac5156c9d1d92d351184a98c7f49ee48a0ddf0a
Joost De Cock <joost@joost.at>
2022-10-12 18:09:22 +0200

    fix(rehype-highlight-lines): Add code linebreak

--- packages/rehype-highlight-lines/src/index.mjs
+++ packages/rehype-highlight-lines/src/index.mjs
@@ -80,7 +80,7 @@ export default (userOptions = {}) => {
         // Deal with extra linebreaks in the element before the start
         const prev = parent.children[i - 1]
         if (prev.type === 'text' && prev.value.includes('\n'))
-          prev.value = prev.value.replace(/\n/g, '')
+          prev.value = prev.value.replace(/\n/g, '') + '\n'
 
         if (options.swallow) node.__remove_dupes = true
       } else if (type === 'closing') {
