6bd84f42018a5ba35b870d1f8c4fe52671de7d8b
Joost De Cock <joost@joost.at>
2022-10-15 17:18:23 +0200

    feat(dev): Added 404 page

--- sites/shared/prebuild/index.mjs
+++ sites/shared/prebuild/index.mjs
@@ -16,12 +16,21 @@ const run = async () => {
   } else if (SITE === 'dev') {
     const mdxPages = await prebuildMdx(SITE)
     if (process.env.GENERATE_OG_IMAGES) {
-      // Create og image for home page
-      const img = await generateOgImage({
+      // Create og image for the home page
+      await generateOgImage({
         lang: 'en',
         site: SITE,
         slug: '',
         title: 'FreeSewing.dev',
+      })
+      // Create og image for the 404 page
+      await generateOgImage({
+        lang: 'en',
+        site: SITE,
+        slug: '/404',
+        intro: "There's nothing here. Only this message to say there's nothing here.",
+        title: 'Page not found',
+        lead: '404',
       })
     }
     prebuildNavigation(mdxPages, false, SITE)
