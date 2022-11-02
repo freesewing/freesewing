1839145c97c253f2dcd21088f8612a551061ae91
joostdecock <joost@joost.at>
2022-11-02 14:12:38 +0100

    chore(sanity): Updated schema for user avatars

---
+++ sites/sanity/schemas/avatar.js
@@ -0,0 +1,17 @@
+export const avatar = {
+  name: `avatar`,
+  type: 'document',
+  title: `User Avatar`,
+  fields: [
+    {
+      name: 'userid',
+      type: 'number',
+      title: 'User ID',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+  ],
+}
--- sites/sanity/schemas/schema.js
+++ sites/sanity/schemas/schema.js
@@ -3,14 +3,16 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import { blogSchemaBuilder } from './blog.js'
 import { showcaseSchemaBuilder } from './showcase.js'
 import { newsletter } from './newsletter.js'
+import { avatar } from './avatar.js'
 
 const languages = ['en', 'nl', 'de', 'es', 'fr']
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    ...languages.map(lang => blogSchemaBuilder(lang)),
-    ...languages.map(lang => showcaseSchemaBuilder(lang)),
+    ...languages.map((lang) => blogSchemaBuilder(lang)),
+    ...languages.map((lang) => showcaseSchemaBuilder(lang)),
     newsletter,
+    avatar,
   ]),
 })
