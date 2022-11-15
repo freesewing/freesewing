e09b25f2683029c70079ac06df14eeb79abfefb7
Enoch Riese <enoch.riese@gmail.com>
2022-11-15 10:39:03 -0600

    increase timeout even more. allow warnings

--- scripts/testall.js
+++ scripts/testall.js
@@ -20,13 +20,13 @@ spawn(
     'testci',
     '--stream',
     '--parallel',
-    '--loglevel',
-    'error',
+    // '--loglevel',
+    // 'error',
     '--',
     '--file',
     `${collectorScript}`,
-    '--no-warnings',
-    '--timeout=5000',
+    // '--no-warnings',
+    '--timeout=10000',
   ],
   { stdio: 'inherit' }
 ).on('exit', function (code) {
