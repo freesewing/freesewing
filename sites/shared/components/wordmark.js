1e379d68ff73a7706b41a61097b1c67c87d0298d
joostdecock <joost@joost.at>
2022-11-23 21:42:22 +0100

    chore(sites): Updates to headers

--- sites/shared/components/wordmark.js
+++ sites/shared/components/wordmark.js
@@ -19,7 +19,7 @@ export const WordMark = () => (
   <Link href="/">
     <a
       role="button"
-      className="btn btn-ghost btn-sm normal-case text-2xl hover:bg-transparent font-bold"
+      className="btn btn-ghost btn-sm normal-case text-2xl hover:bg-transparent font-bold px-0 -mt-1"
     >
       <InnerWordMark />
     </a>
