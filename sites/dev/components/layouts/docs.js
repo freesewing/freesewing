de89826a07e9b388782a023c7b5ef7c3f0fd6ead
joostdecock <joost@joost.at>
2022-11-25 20:22:54 +0100

    fix(dev): Add theme picker to mobile menu

--- sites/dev/components/layouts/docs.js
+++ sites/dev/components/layouts/docs.js
@@ -1,8 +1,11 @@
 import { useRouter } from 'next/router'
 // Shared components
 import Aside from 'site/components/navigation/aside.js'
+import ThemePicker from 'shared/components/theme-picker.js'
 import Breadcrumbs from 'shared/components/breadcrumbs.js'
 import { getCrumbs } from 'shared/utils'
+import HomeIcon from 'shared/components/icons/home.js'
+import Link from 'next/link'
 
 const DefaultLayout = ({ app, title = false, crumbs = false, children = [] }) => {
   const router = useRouter()
@@ -11,7 +14,20 @@ const DefaultLayout = ({ app, title = false, crumbs = false, children = [] }) =>
 
   return (
     <div className="grid grid-cols-4 m-auto justify-center place-items-stretch">
-      <Aside app={app} slug={slug} />
+      <Aside
+        app={app}
+        slug={slug}
+        before={[
+          <div className="flex flex-row items-center justify-between border-b mb-4">
+            <Link href="/">
+              <a>
+                <HomeIcon />
+              </a>
+            </Link>
+            <ThemePicker app={app} />
+          </div>,
+        ]}
+      />
       <section className="col-span-4 lg:col-span-3 py-24 px-4 lg:pl-8 bg-base-50">
         {title && (
           <div className="xl:pl-4">
