fc329f8a1a792ca1d39e5d6f70feff6ac495f07d
Joost De Cock <joost@joost.at>
2022-12-24 22:51:03 +0100

    fix(org): Typo in design component

--- sites/org/components/design.js
+++ sites/org/components/design.js
@@ -69,9 +69,12 @@ const Design = ({ design }) => {
       `}
     >
       {/* Link over the entire card */}
-      <Link href={`/designs/${design}`}>
+      <Link
+        href={`/designs/${design}`}
+        className="absolute top-0 right-0 w-full h-full"
+        title={t(`${design}.t`)}
+      >
         {' '}
-        className="absolute top-0 right-0 w-full h-full" title={t(`${design}.t`)} >
       </Link>
       {/* Slanted corner ribbon with the difficulty */}
       <div className="absolute top-0 right-0">
