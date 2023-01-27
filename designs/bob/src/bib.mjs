ae2b752788ce08e1b8e03a18a0c711ebc5975afb
Benjamin F <example@example.com>
2023-01-26 20:32:00 -0800

    Draft of new notches and symbols

--- designs/bob/src/bib.mjs
+++ designs/bob/src/bib.mjs
@@ -1,248 +1,162 @@
 import { pluginBundle } from '@freesewing/plugin-bundle'
 
+const markers = `
+<marker id="pointer" markerWidth="10" markerHeight="8" refX="9.5" refY="5.1" orient="-45" markerUnits="userSpaceOnUse"><polyline points="1 1, 9 5, 1 7" />
+</marker>
+`
+const notches = `
+<g id="snotch">
+  <path d="M -10 -6 L -0.5 0 L -10 6 Z" class="note" />
+  <path d="M -10 -6 L -20 0 L -10 6 Z" class="note" />
+  <!-- path d="M 10 6 L -0.5 0 L 10 -6 Z" class="note" /-->
+</g>
+<g id="sfnotch">
+  <path d="M -10 -6 L -0.5 0 L -10 6 Z" class="note fill-note" />
+  <path d="M -10 -6 L -20 0 L -10 6 Z" class="note fill-note" />
+</g>
+<g id="dnotch">
+  <path d="M -10 -12 L -0.5 -6 L -10 0 Z" class="note" />
+  <path d="M -10 0 L -0.5 8 L -10 12 Z" class="note" />
+  <path d="M -10 -12 L -20 -6 L -20 6 L -10 12 Z" class="note" />
+</g>
+<g id="dfnotch">
+  <path d="M -10 -12 L -0.5 -6 L -10 0 Z" class="note fill-note" />
+  <path d="M -10 0 L -0.5 8 L -10 12 Z" class="note fill-note" />
+  <path d="M -10 -12 L -20 -6 L -20 6 L -10 12 Z" class="note fill-note" />
+</g>
+<g id="circle">
+  <circle cx="0" cy="0" r="2.5" class="note" />
+</g>
+<g id="fcircle">
+  <circle cx="0" cy="0" r="2.5" class="note fill-note" />
+</g>
+<g id="square">
+  <path d="M -2.5 -2.5 H 2.5 V 2.5 H -2.5 Z" class="note" />
+</g>
+<g id="fsquare">
+  <path d="M -2.5 -2.5 H 2.5 V 2.5 H -2.5 Z" class="note fill-note" />
+</g>
+<g id="triangle">
+  <path d="M 0 -2.165 L -2.5 2.165 L 2.5 2.165 Z Z" class="note" />
+</g>
+<g id="ftriangle">
+  <path d="M 0 -2.165 L -2.5 2.165 L 2.5 2.165 Z Z" class="note fill-note" />
+</g>
+<g id="star">
+<path d="m 0,-2.5 L .667,-.611 h2 l-1.556,1.22 .556,1.889 -1.667-1.111
+-1.667,1.111 .556,-1.889 -1.556-1.22 h2 z" class="note" />
+</g>
+<g id="fstar">
+<path d="m 0,-2.5 L .667,-.611 h2 l-1.556,1.22 .556,1.889 -1.667-1.111
+-1.667,1.111 .556,-1.889 -1.556-1.22 h2 z" class="note fill-note" />
+</g>
+<g id="pentagon">
+<path d="m 1.62,2.50 H -1.63 L -2.63,-0.59 L 0,-2.5 2.63,-0.59
+Z" class="note" />
+</g>
+<g id="fpentagon">
+<path d="m 1.62,2.50 H -1.63 L -2.63,-0.59 L 0,-2.5 2.63,-0.59
+Z" class="note fill-note" />
+</g>
+<g id="circleplus">
+  <circle cx="0" cy="0" r="2.5" class="note" />
+  <path d="M 0,-2.5 V 2.5" class="note" />
+  <path d="M -2.5,0 H 2.5" class="note" />
+</g>
+<g id="circlex">
+  <circle cx="0" cy="0" r="2.5" class="note" />
+  <path d="M 0,-2.5 V 2.5" transform="rotate(45)" class="note" />
+  <path d="M -2.5,0 H 2.5" transform="rotate(45)" class="note" />
+</g>
+<g id="diamond">
+  <path d="M 0,-2.5 L 1.8,0 L 0,2.5 L -1.8,0 Z" class="note" />
+</g>
+<g id="fdiamond">
+  <path d="M 0,-2.6 L 1.8,0 L 0,2.6 L -1.8,0 Z" class="note fill-note" />
+</g>
+`
+
+export const benplugin = {
+  name: 'benplugin',
+  version: 'test1',
+  hooks: {
+    preRender: (svg) => {
+      if (svg.defs.indexOf(markers) === -1) svg.defs += markers
+      if (svg.defs.indexOf(notches) === -1) svg.defs += notches
+    },
+  },
+}
+
 export const bib = {
   name: 'bob.bib',
-  measurements: [],
-  optionalMeasurements: ['head'],
-  options: {
-    neckRatio: { pct: 80, min: 70, max: 90, menu: 'fit' },
-    widthRatio: { pct: 45, min: 35, max: 55, menu: 'fit' },
-    lengthRatio: { pct: 75, min: 55, max: 85, menu: 'fit' },
-    headSize: { pct: 100, min: 10, max: 200, snap: 5, menu: 'size' },
-  },
-  plugins: [pluginBundle],
-  draft: ({
-    Point,
-    points,
-    Path,
-    paths,
-    optionalMeasurements,
-    options,
-    macro,
-    log,
-    complete,
-    snippets,
-    Snippet,
-    paperless,
-    part,
-  }) => {
-    // Head size
-    const head = (optionalMeasurements?.head || 360) * options.headSize
+  plugins: [pluginBundle, benplugin],
+  draft: ({ Point, points, Path, paths, Snippet, snippets, part, sa }) => {
+    points.start = new Point(10, 10)
+    points.end = new Point(90, 90)
 
-    // Construct the neck opening
-    let tweak = 1
-    let target = (head * options.neckRatio) / 4
-    let delta
-    do {
-      points.right = new Point((tweak * head) / 10, 0)
-      points.bottom = new Point(0, (tweak * head) / 12)
-
-      points.rightCp1 = points.right.shift(90, points.bottom.dy(points.right) / 2)
-      points.bottomCp2 = points.bottom.shift(0, points.bottom.dx(points.right) / 2)
-
-      paths.neck = new Path()
-        .move(points.right)
-        .curve(points.rightCp1, points.bottomCp2, points.bottom)
-
-      delta = paths.neck.length() - target
-      if (delta > 0) tweak = tweak * 0.99
-      else tweak = tweak * 1.02
-    } while (Math.abs(delta) > 1)
-
-    points.rightCp2 = points.rightCp1.flipY()
-    points.bottomCp1 = points.bottomCp2.flipX()
-
-    points.left = points.right.flipX()
-    points.leftCp1 = points.rightCp2.flipX()
-    points.leftCp2 = points.rightCp1.flipX()
-
-    points.top = points.bottom.flipY()
-    points.topCp1 = points.bottomCp2.flipY()
-    points.topCp2 = points.bottomCp1.flipY()
-
-    // Construct the outline
-    let width = head * options.widthRatio
-    let length = head * options.lengthRatio
-
-    points.topLeft = new Point(width / -2, points.top.y - (width / 2 - points.right.x))
-    points.topRight = points.topLeft.shift(0, width)
-    points.bottomLeft = points.topLeft.shift(-90, length)
-    points.bottomRight = points.topRight.shift(-90, length)
-
-    points.edgeLeft = new Point(points.topLeft.x, points.left.y)
-    points.edgeRight = new Point(points.topRight.x, points.right.y)
-    points.edgeTop = new Point(0, points.topLeft.y)
-
-    points.edgeLeftCp = points.edgeLeft.shiftFractionTowards(points.topLeft, 0.5)
-    points.edgeRightCp = points.edgeLeftCp.flipX()
-    points.edgeTopLeftCp = points.edgeTop.shiftFractionTowards(points.topLeft, 0.5)
-    points.edgeTopRightCp = points.edgeTopLeftCp.flipX()
-
-    // Round the end of the straps
-    let strap = points.edgeTop.dy(points.top)
-
-    points.tipRight = points.edgeTop.translate(strap / 2, strap / 2)
-    points.tipRightTop = new Point(points.tipRight.x, points.edgeTop.y)
-    points.tipRightBottom = new Point(points.tipRight.x, points.top.y)
-
-    macro('round', {
-      from: points.edgeTop,
-      to: points.tipRight,
-      via: points.tipRightTop,
-      prefix: 'tipRightTop',
-    })
-    macro('round', {
-      from: points.tipRight,
-      to: points.top,
-      via: points.tipRightBottom,
-      prefix: 'tipRightBottom',
-    })
-
-    // Rotate straps so they don't overlap
-    let rotateThese = [
-      'edgeTopLeftCp',
-      'edgeTop',
-      'tipRight',
-      'tipRightTop',
-      'tipRightTopStart',
-      'tipRightTopCp1',
-      'tipRightTopCp2',
-      'tipRightTopEnd',
-      'tipRightBottomStart',
-      'tipRightBottomCp1',
-      'tipRightBottomCp2',
-      'tipRightBottomEnd',
-      'tipRightBottom',
-      'top',
-      'topCp2',
-    ]
-
-    while (points.tipRightBottomStart.x > -1) {
-      for (let p of rotateThese) points[p] = points[p].rotate(1, points.edgeLeft)
-    }
-
-    // Add points to anchor snaps on
-    points.snapLeft = points.top.shiftFractionTowards(points.edgeTop, 0.5)
-
-    // Mirror points to the other side
-    points.edgeTopRightCp = points.edgeTopLeftCp.flipX()
-    points.topCp1 = points.topCp2.flipX()
-    points.tipLeftTopStart = points.tipRightTopStart.flipX()
-    points.tipLeftTopCp1 = points.tipRightTopCp1.flipX()
-    points.tipLeftTopCp2 = points.tipRightTopCp2.flipX()
-    points.tipLeftTopEnd = points.tipRightTopEnd.flipX()
-    points.tipLeftBottomStart = points.tipRightBottomStart.flipX()
-    points.tipLeftBottomCp1 = points.tipRightBottomCp1.flipX()
-    points.tipLeftBottomCp2 = points.tipRightBottomCp2.flipX()
-    points.tipLeftBottomEnd = points.tipRightBottomEnd.flipX()
-    points.snapRight = points.snapLeft.flipX()
-
-    // Round the bottom of the bib
-    // Radius is fixed, but you could use an option for it)
-    macro('round', {
-      from: points.topLeft,
-      to: points.bottomRight,
-      via: points.bottomLeft,
-      radius: points.bottomRight.x / 4,
-      prefix: 'bottomLeft',
-    })
-    macro('round', {
-      from: points.bottomLeft,
-      to: points.topRight,
-      via: points.bottomRight,
-      radius: points.bottomRight.x / 4,
-      prefix: 'bottomRight',
-    })
-
-    // Construct the path
-    paths.seam = new Path()
-      .move(points.edgeLeft)
-      .line(points.bottomLeftStart)
-      .curve(points.bottomLeftCp1, points.bottomLeftCp2, points.bottomLeftEnd)
-      .line(points.bottomRightStart)
-      .curve(points.bottomRightCp1, points.bottomRightCp2, points.bottomRightEnd)
-      .line(points.edgeRight)
-      .curve(points.edgeRightCp, points.edgeTopRightCp, points.tipLeftTopStart)
-      .curve(points.tipLeftTopCp1, points.tipLeftTopCp2, points.tipLeftTopEnd)
-      .curve(points.tipLeftBottomCp1, points.tipLeftBottomCp2, points.tipLeftBottomEnd)
-      .curve(points.topCp1, points.rightCp2, points.right)
-      .curve(points.rightCp1, points.bottomCp2, points.bottom)
-      .curve(points.bottomCp1, points.leftCp2, points.left)
-      .curve(points.leftCp1, points.topCp2, points.tipRightBottomEnd)
-      .curve(points.tipRightBottomCp2, points.tipRightBottomCp1, points.tipRightBottomStart)
-      .curve(points.tipRightTopCp2, points.tipRightTopCp1, points.tipRightTopStart)
-      .curve(points.edgeTopLeftCp, points.edgeLeftCp, points.edgeLeft)
-      .close()
+    paths.test = new Path()
+      .move(points.start)
+      .line(points.end)
+      //.attr('marker-end', 'url(#pointer)')
       .attr('class', 'fabric')
 
-    log.info(['biasTapeLength', { mm: paths.seam.length() }])
-    log.info(['fabricHeight', { mm: points.tipRightTopStart.dy(points.bottomLeftEnd) }])
-    log.info(['fabricWidth', { mm: points.bottomLeftStart.dx(points.bottomRightEnd) }])
-
-    // Complete?
-    if (complete) {
-      // Add the snaps
-      snippets.snapStud = new Snippet('snap-stud', points.snapLeft)
-      snippets.snapSocket = new Snippet('snap-socket', points.snapRight).attr('opacity', 0.5)
-
-      // Add the bias tape
-      paths.bias = paths.seam
-        .offset(-5)
-        .attr('class', 'various dashed')
-        .attr('data-text', 'finishWithBiasTape')
-        .attr('data-text-class', 'center fill-various')
-
-      // Add the title
-      points.title = points.bottom.shift(-90, 45)
-      macro('title', {
-        at: points.title,
-        nr: 1,
-        title: 'bib',
-      })
-
-      // Add the scalebox
-      points.scalebox = points.title.shift(-90, 55)
-      macro('scalebox', { at: points.scalebox })
-
-      // Add the logo
-      points.logo = new Point(0, 0)
-      snippets.logo = new Snippet('logo', points.logo)
-
-      // Paperless?
-      if (paperless) {
-        // Add dimensions
-        macro('hd', {
-          from: points.bottomLeftStart,
-          to: points.bottomRightEnd,
-          y: points.bottomLeft.y + 15,
-        })
-        macro('vd', {
-          from: points.bottomRightStart,
-          to: points.bottom,
-          x: points.bottomRight.x + 15,
-        })
-        macro('vd', {
-          from: points.bottomRightStart,
-          to: points.right,
-          x: points.bottomRight.x + 30,
-        })
-        macro('vd', {
-          from: points.bottomRightStart,
-          to: points.tipLeftTopStart,
-          x: points.bottomRight.x + 45,
-        })
-        macro('hd', {
-          from: points.left,
-          to: points.right,
-          y: points.left.y + 25,
-        })
-        macro('ld', {
-          from: points.tipLeftBottomEnd,
-          to: points.tipLeftTopStart,
-          d: -15,
-        })
-      }
+    const length = paths.test.length()
+    for (let p = 0; p <= 5; p++) {
+      points['p' + p] = paths.test.shiftAlong((length * p) / 5)
     }
+    points.pa = new Point(-10, 20)
+    points.pb = points.pa.shift(-90, 10)
+    points.pc = points.pa.shift(-90, 20)
+    points.pd = points.pa.shift(-90, 30)
+    points.pe = points.pa.shift(-90, 40)
+    points.pf = points.pa.shift(-90, 50)
+    points.pg = points.pa.shift(-90, 60)
+    points.ph = points.pa.shift(-90, 70)
+    points.pi = points.pa.shift(-90, 80)
+    points.pj = points.pa.shift(-90, 90)
+    points.pk = points.pj.shift(0, 10)
+    points.pl = points.pj.shift(0, 20)
+    points.pm = points.pj.shift(0, 30)
+    points.pn = points.pj.shift(0, 40)
+    points.po = points.pj.shift(0, 50)
+    points.pp = points.pj.shift(0, 60)
+
+    const angle = paths.test.start().angle(paths.test.end())
+    let notchScale = 1
+    if (sa) notchScale = sa / 10
+    let notchAngle = angle
+
+    snippets.s1 = new Snippet('snotch', points.p1)
+      .attr('data-rotate', notchAngle)
+      .attr('data-scale', notchScale)
+    snippets.s2 = new Snippet('sfnotch', points.p2)
+      .attr('data-rotate', notchAngle)
+      .attr('data-scale', notchScale)
+    snippets.s3 = new Snippet('dnotch', points.p3)
+      .attr('data-rotate', notchAngle)
+      .attr('data-scale', notchScale)
+    snippets.s4 = new Snippet('dfnotch', points.p4)
+      .attr('data-rotate', notchAngle)
+      .attr('data-scale', notchScale)
+
+    snippets.s5 = new Snippet('circle', points.pa)
+    snippets.s6 = new Snippet('fcircle', points.pb)
+    snippets.s7 = new Snippet('square', points.pc)
+    snippets.s8 = new Snippet('fsquare', points.pd)
+    snippets.s9 = new Snippet('triangle', points.pe)
+    snippets.s10 = new Snippet('ftriangle', points.pf)
+    snippets.s11 = new Snippet('star', points.pg)
+    snippets.s12 = new Snippet('fstar', points.ph)
+    snippets.s13 = new Snippet('pentagon', points.pi)
+    snippets.s14 = new Snippet('fpentagon', points.pj)
+    snippets.s15 = new Snippet('circleplus', points.pk)
+    snippets.s16 = new Snippet('circlex', points.pl)
+    snippets.s17 = new Snippet('diamond', points.pm)
+    snippets.s18 = new Snippet('fdiamond', points.pn)
+
+    paths.sa = paths.test.offset(sa).setClass('fabric sa')
+
+    paths.noclip = new Path().move(new Point(-40, -40)).move(new Point(150, 150))
 
     return part
   },
