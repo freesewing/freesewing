b80409dddf948b1251a95ab5dfd04e9575d760ae
joostdecock <joost@joost.at>
2023-02-07 19:56:20 +0100

    chore: Fix extra comma

--- sites/shared/components/mdx/highlight.mjs
+++ sites/shared/components/mdx/highlight.mjs
@@ -16,7 +16,7 @@ export const Highlight = (props) => {
   if (props.children?.props?.className) {
     language = props.children.props.className.split('-').pop()
     if (language.indexOf('.') !== -1) {
-      ;[status, language] = language.split('.')
+      [status, language] = language.split('.')
     }
   }
 
