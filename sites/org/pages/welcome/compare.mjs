34499bd23ed28664cea99e6cf3e09937f89cb077
joostdecock <joost@joost.at>
2023-04-16 19:41:12 +0200

    fix(org): page props in welcome/compare

--- sites/org/pages/welcome/compare.mjs
+++ sites/org/pages/welcome/compare.mjs
@@ -33,7 +33,7 @@ const ComparePage = (props) => {
   return (
     <PageWrapper app={app} title={t('title')} layout={BareLayout} footer={false}>
       <DynamicAuthWrapper app={app}>
-        <div className="m-auto max-w-lg text-center lg:mt-12 p-8">
+        <div className="m-auto max-w-lg text-center lg:mt-24 p-8">
           <DynamicCompare app={app} title welcome />
         </div>
       </DynamicAuthWrapper>
@@ -47,6 +47,9 @@ export async function getStaticProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, namespaces)),
+      page: {
+        path: ['welcome', 'compare'],
+      },
     },
   }
 }
