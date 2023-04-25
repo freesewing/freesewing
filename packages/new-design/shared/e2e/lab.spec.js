e8b0752eb50da36feeef292ca5dcfa26e003fd11
Enoch Riese <enoch.riese@gmail.com>
2023-04-25 00:45:58 -0400

    stronger test for new-design

--- packages/new-design/shared/e2e/lab.spec.js
+++ packages/new-design/shared/e2e/lab.spec.js
@@ -1,20 +1,19 @@
 import { test, expect } from '@playwright/test'
 
 test('Compiles and drafts', async ({ page }) => {
-  await page.goto('http://localhost:8000/')
-  await page.getByRole('link', { name: '👉 To your design 👈' }).click()
+  await page.goto('http://localhost:8000/design')
 
-  const sizeButton = page
-    .getByRole('list')
-    .filter({ hasText: 'Size 28Size 30Size 32Size 34Size 36Size 38Size 40Size 42Size 44Size 46' })
-    .getByRole('button', { name: 'Size 36' })
-
-  if (await sizeButton.isVisible()) {
-    await sizeButton.click()
+  await page.waitForSelector('main')
+  if (await page.getByRole('heading', { name: 'Preload a set of measurements' }).isVisible()) {
+    await page
+      .getByRole('list')
+      .filter({ hasText: 'Size 28Size 30Size 32Size 34Size 36Size 38Size 40Size 42Size 44Size 46' })
+      .getByRole('button', { name: 'Size 36' })
+      .click()
     await page.getByTitle('draftDesign').click()
   }
 
   await expect(page.getByText('Something went wrong')).toHaveCount(0)
   await expect(page.getByText('Unhandled Runtime Error')).toHaveCount(0)
-  await expect(page.getByRole('button', { name: '° Measurements' })).toBeVisible()
+  await expect(page.getByTitle('Measurements')).toBeVisible()
 })
