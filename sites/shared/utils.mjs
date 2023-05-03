e551846530d1c25a9ff741578940321f47fae8c7
Benjamin F <example@example.com>
2023-05-03 14:22:34 -0700

    fix(shared): Revert initial constant options fix

--- sites/shared/utils.mjs
+++ sites/shared/utils.mjs
@@ -187,9 +187,8 @@ export const optionsMenuStructure = (options) => {
   // Fixme: One day we should sort this based on the translation
   for (const option of orderBy(sorted, ['menu', 'name'], ['asc'])) {
     if (typeof option === 'object') {
-      const isAConstant = Object.keys(option).length === 1
       if (option.menu) set(menu, `${option.menu}.${option.name}`, optionType(option))
-      else if (typeof option.menu === 'undefined' && !isAConstant) {
+      else if (typeof option.menu === 'undefined') {
         console.log(
           `Warning: Option ${option.name} does not have a menu config. ` +
             'Either configure it, or set it to false to hide this option.'
