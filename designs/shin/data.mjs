9f352b5107f48987639370792754cec62029ced8
joostdecock <joost@joost.at>
2023-05-19 17:17:44 +0200

    release: v3.0.0-alpha.12
    This fixes an issue with @freesewing/new-design

--- designs/shin/data.mjs
+++ designs/shin/data.mjs
@@ -1,4 +1,4 @@
 // This file is auto-generated | All changes you make will be overwritten.
 export const name = '@freesewing/shin'
-export const version = '3.0.0-alpha.10'
+export const version = '3.0.0-alpha.12'
 export const data = { name, version }
