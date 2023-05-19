31efe377895bc695d2fc99b3bea4e21b27eff4bd
joostdecock <joost@joost.at>
2023-05-19 10:38:30 +0200

    chore(i18n): Suspend prebuild script in anticipation of v3 changes

--- packages/i18n/scripts/prebuilder.mjs
+++ packages/i18n/scripts/prebuilder.mjs
@@ -1,4 +1,4 @@
-import { build } from '../src/prebuild.mjs'
+//import { build } from '../src/prebuild.mjs'
 
 // use a deny-list to keep locales that aren't ready out of the build
 export const denyList = ['uk']
