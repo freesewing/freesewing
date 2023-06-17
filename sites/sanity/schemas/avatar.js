61abedd1b348e6bf8215f344ba3472247f09f220
joostdecock <joost@joost.at>
2023-06-17 10:51:25 +0200

    chore: Migrate sanity datasets

--- sites/sanity/schemas/avatar.js
+++ sites/sanity/schemas/avatar.js
@@ -5,25 +5,8 @@ export const userimg = {
   fields: [
     {
       name: 'recordid',
-      type: 'number',
-      title: 'User ID',
-    },
-    {
-      name: 'img',
-      type: 'image',
-      title: 'Image',
-    },
-  ],
-}
-export const personimg = {
-  name: `personimg`,
-  type: 'document',
-  title: `Person Image`,
-  fields: [
-    {
-      name: 'recordid',
-      type: 'number',
-      title: 'Person ID',
+      type: 'string',
+      title: 'Image ID',
     },
     {
       name: 'img',
