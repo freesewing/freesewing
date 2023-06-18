bb07616a3b671120f39b16f11fd4239672758dac
joostdecock <joost@joost.at>
2023-06-18 10:57:31 +0200

    feat(shared): Add draft header to inspect view

--- sites/shared/components/workbench/views/inspect/index.mjs
+++ sites/shared/components/workbench/views/inspect/index.mjs
@@ -3,6 +3,7 @@ import { PanZoomPattern as ShowPattern } from 'shared/components/workbench/pan-z
 import { InspectorPattern } from './inspector/pattern.mjs'
 import { DraftMenu, ns as menuNs } from './menu.mjs'
 import { objUpdate } from 'shared/utils.mjs'
+import { DraftViewHeader } from '../draft/index.mjs'
 
 export const ns = menuNs
 
@@ -66,26 +67,36 @@ export const InspectView = ({
   }
 
   return (
-    <div className="flex flex-row">
-      <div className="w-2/3 shrink-0 grow lg:p-4 sticky top-0">{output}</div>
-      <div className="w-1/3 shrink grow-0 lg:p-4 max-w-2xl h-screen overflow-scroll">
-        <DraftMenu
-          {...{
-            design,
-            pattern,
-            patternConfig,
-            settings,
-            ui,
-            update,
-            language,
-            account,
-            DynamicDocs,
-            inspector,
-            renderProps,
-            view,
-            setView,
-          }}
-        />
+    <div className="flex flex-col">
+      <DraftViewHeader
+        {...{
+          settings,
+          ui,
+          update,
+          control: account.control,
+        }}
+      />
+      <div className="flex flex-row">
+        <div className="w-2/3 shrink-0 grow lg:p-4 sticky top-0">{output}</div>
+        <div className="w-1/3 shrink grow-0 lg:p-4 max-w-2xl h-screen overflow-scroll">
+          <DraftMenu
+            {...{
+              design,
+              pattern,
+              patternConfig,
+              settings,
+              ui,
+              update,
+              language,
+              account,
+              DynamicDocs,
+              inspector,
+              renderProps,
+              view,
+              setView,
+            }}
+          />
+        </div>
       </div>
     </div>
   )
