77178b790b3653918bc2f7b366bf485fba2e1487
joostdecock <joost@joost.at>
2023-06-25 19:30:18 +0200

    fix(plugin-annotations): Fix issues in defs formatting

--- plugins/plugin-annotations/src/notches.mjs
+++ plugins/plugin-annotations/src/notches.mjs
@@ -4,16 +4,16 @@ export const notchesDefs = [
     name: 'notch',
     def: `
 <g id="notch">
-  <circle cy="0" cx="0" r="1.4" class="fill-note" />
-  <circle cy="0" cx="0" r="2.8" class="note" />
+  <circle cy="0" cx="0" r="1.4" class="fill-note"></circle>
+  <circle cy="0" cx="0" r="2.8" class="note"></circle>
 </g>`,
   },
   {
     name: 'bnotch',
     def: `
 <g id="bnotch">
-  <path d="M -1.1 -1.1 L 1.1 1.1 M 1.1 -1.1 L -1.1 1.1" class="note" />
-  <circle cy="0" cx="0" r="2.8" class="note" />
+  <path d="M -1.3 -1.3 L 1.3 1.3 M 1.3 -1.3 L -1.3 1.3" class="note"></path>
+  <circle cy="0" cx="0" r="2.8" class="note"></circle>
 </g>`,
   },
 ]
