8aab23aa565057d403c9efe9b75d6dfcb05e1767
joostdecock <joost@joost.at>
2023-07-02 13:03:46 +0200

    feat(bella): Add i18n named export

--- designs/bella/src/index.mjs
+++ designs/bella/src/index.mjs
@@ -2,10 +2,11 @@ import { Design } from '@freesewing/core'
 import { data } from '../data.mjs'
 import { back } from './back.mjs'
 import { frontSideDart } from './front-side-dart.mjs'
+import { i18n } from '../i18n/index.mjs'
 
 const Bella = new Design({
   data,
   parts: [back, frontSideDart],
 })
 
-export { back, frontSideDart, Bella }
+export { back, frontSideDart, Bella, i18n }
