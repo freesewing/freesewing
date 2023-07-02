7992b99f8d69411c7c50cea29e45fd5cb4c3d297
joostdecock <joost@joost.at>
2023-07-02 16:58:40 +0200

    feat(teagan): Add i18n named export

--- designs/teagan/src/index.mjs
+++ designs/teagan/src/index.mjs
@@ -1,5 +1,7 @@
-import { Design } from '@freesewing/core'
+import { Design, mergeI18n } from '@freesewing/core'
 import { data } from '../data.mjs'
+import { i18n as brianI18n } from '@freesewing/brian'
+import { i18n as teaganI18n } from '../i18n/index.mjs'
 import { back } from './back.mjs'
 import { front } from './front.mjs'
 import { sleeve } from './sleeve.mjs'
@@ -10,5 +12,10 @@ const Teagan = new Design({
   parts: [back, front, sleeve],
 })
 
+// Merge translations
+const i18n = mergeI18n([brianI18n, teaganI18n], {
+  o: { drop: ['sleeveLengthBonus'] },
+})
+
 // Named exports
-export { back, front, sleeve, Teagan }
+export { back, front, sleeve, Teagan, i18n }
