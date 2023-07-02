444f945cc4c858dcc2587d90c097a834052baa6a
joostdecock <joost@joost.at>
2023-07-02 14:43:29 +0200

    fix(diana): Add missing options in translation config

--- designs/diana/src/index.mjs
+++ designs/diana/src/index.mjs
@@ -1,6 +1,7 @@
-import { Design } from '@freesewing/core'
+import { Design, mergeI18n } from '@freesewing/core'
 import { data } from '../data.mjs'
-import { i18n } from '../i18n/index.mjs'
+import { i18n as brianI18n } from '@freesewing/brian'
+import { i18n as dianaI18n } from '../i18n/index.mjs'
 // Parts
 import { front } from './front.mjs'
 import { back } from './front.mjs'
@@ -11,6 +12,9 @@ const Diana = new Design({
   data,
   parts: [front, back, sleeve],
 })
+
+// Merge translations
+const i18n = mergeI18n([brianI18n, dianaI18n])
 
 // Named exports
 export { front, back, sleeve, Diana, i18n }
