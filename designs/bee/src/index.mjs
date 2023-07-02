da6106c9a4f62f5ee12a886f92e9ff5dff696ed4
joostdecock <joost@joost.at>
2023-07-02 13:04:36 +0200

    feat(bee): Add i18n named export

--- designs/bee/src/index.mjs
+++ designs/bee/src/index.mjs
@@ -3,10 +3,11 @@ import { data } from '../data.mjs'
 import { cup } from './cup.mjs'
 import { neckTie } from './neck-tie.mjs'
 import { bandTie } from './band-tie.mjs'
+import { i18n } from '../i18n/index.mjs'
 
 const Bee = new Design({
   data,
   parts: [cup, neckTie, bandTie],
 })
 
-export { cup, neckTie, bandTie, Bee }
+export { cup, neckTie, bandTie, Bee, i18n }
