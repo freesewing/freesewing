52b5a8a6a2b0df81c396b0382b828e0bad8a36b1
joostdecock <joost@joost.at>
2023-07-02 13:50:17 +0200

    fix(bee): Add missing options in translation config

--- designs/bee/src/index.mjs
+++ designs/bee/src/index.mjs
@@ -1,13 +1,44 @@
-import { Design } from '@freesewing/core'
+import { Design, mergeI18n } from '@freesewing/core'
 import { data } from '../data.mjs'
 import { cup } from './cup.mjs'
 import { neckTie } from './neck-tie.mjs'
 import { bandTie } from './band-tie.mjs'
-import { i18n } from '../i18n/index.mjs'
+import { i18n as beeI18n } from '../i18n/index.mjs'
+import { i18n as bellaI18n } from '@freesewing/bella'
 
 const Bee = new Design({
   data,
   parts: [cup, neckTie, bandTie],
+})
+
+// Merge translations
+const i18n = mergeI18n([bellaI18n, beeI18n], {
+  o: {
+    keep: [
+      ...Object.keys(cup.options),
+      ...Object.keys(neckTie.options),
+      ...Object.keys(bandTie.options),
+      'bustDartCurve',
+      'bustSpanEase',
+      'chestEase',
+      'fullChestEaseReduction',
+      'shoulderToShoulderEase',
+      'waistEase',
+      'backDartHeight',
+      'bustDartLength',
+      'waistDartLength',
+      'armholeDepth',
+      'backArmholeCurvature',
+      'backArmholePitchDepth',
+      'backArmholeSlant',
+      'frontArmholeCurvature',
+      'frontArmholePitchDepth',
+      'backHemSlope',
+      'backNeckCutout',
+      'frontShoulderWidth',
+      'highBustWidth',
+    ],
+  },
 })
 
 export { cup, neckTie, bandTie, Bee, i18n }
