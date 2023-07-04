63f05ee2ad3b138c6bbaab287da4e3fab9a7eb4f
Joost De Cock <joost@joost.at>
2023-07-04 20:50:26 +0200

    fix(hugo): Add missing translations and inherited options

--- designs/hugo/src/index.mjs
+++ designs/hugo/src/index.mjs
@@ -1,6 +1,7 @@
-import { Design } from '@freesewing/core'
+import { Design, mergeI18n } from '@freesewing/core'
 import { data } from '../data.mjs'
-import { i18n } from '../i18n/index.mjs'
+import { i18n as brianI18n } from '@freesewing/brian'
+import { i18n as hugoI18n } from '../i18n/index.mjs'
 import { back } from './back.mjs'
 import { front } from './front.mjs'
 import { sleeve } from './sleeve.mjs'
@@ -16,6 +17,9 @@ const Hugo = new Design({
   data,
   parts: [back, front, sleeve, pocket, pocketFacing, hoodSide, hoodCenter, waistband, cuff],
 })
+
+// Merge translations
+const i18n = mergeI18n([brianI18n, hugoI18n])
 
 // Named exports
 export {
