54abc42e73d356cf84468a19003402e38f1c83f4
joostdecock <joost@joost.at>
2023-07-15 12:16:21 +0200

    feat(dev): Added footer links via use-navigation

--- sites/dev/hooks/use-navigation.mjs
+++ sites/dev/hooks/use-navigation.mjs
@@ -8,14 +8,15 @@ import { orderedSlugLut } from 'shared/hooks/use-navigation-helpers.mjs'
  * - home page => no navvigation shown
  * - /contact => Added below
  *
- * Remember Mc Toshi:
+ * Remember Mc Shifto:
  * Note: Set 'm' to truthy to show this as a main section in the side-navigation (optional)
  * Note: Set 'c' to set the control level to hide things from users (optional)
- * Note: Set 't' to the title
- * Note: Set 'o' to set the order (optional)
  * Note: Set 's' to the slug (optional insofar as it's not a real page (a spacer for the header))
  * Note: Set 'h' to indicate this is a top-level page that should be hidden from the side-nav (like search)
  * Note: Set 'i' when something should be included as top-level in the collapse side-navigation (optional)
+ * Note: Set 'f' to add the page to the footer
+ * Note: Set 't' to the title
+ * Note: Set 'o' to set the order (optional)
  */
 
 export const ns = ['account', 'sections', 'design', 'tags']
@@ -25,57 +26,61 @@ const sitePages = () => {
     // Top-level pages that are the sections menu
     api: {
       m: 1,
+      s: 'api',
       t: 'API Documentation',
       o: 10,
-      s: 'api',
     },
     design: {
       m: 1,
+      s: 'design',
       t: 'Design Sewing Patterns',
       o: 10,
-      s: 'design',
     },
     contribute: {
       m: 1,
+      s: 'contribute',
       t: 'Contribute to FreeSewing',
       o: 20,
-      s: 'contribute',
     },
     i18n: {
       m: 1,
+      s: 'i18n',
       t: 'Help Translate FreeSewing',
       o: 40,
-      s: 'i18n',
     },
     infra: {
       m: 1,
+      s: 'infra',
       t: 'FreeSewing Infrastructure',
       o: 50,
-      s: 'infra',
     },
     about: {
       m: 1,
+      s: 'about',
+      f: 1,
       t: 'About FreeSewing',
       o: 60,
-      s: 'about',
     },
     support: {
       m: 1,
+      s: 'support',
+      f: 1,
       t: 'Support FreeSewing',
       o: 70,
-      s: 'support',
     },
     search: {
-      t: 'Search',
-      o: 270,
       s: 'search',
       h: 1,
+      f: 1,
+      t: 'Search',
+      o: 270,
     },
     sitemap: {
-      t: 'Sitemap',
-      o: 270,
       s: 'sitemap',
       h: 1,
+      f: 1,
+      t: 'Sitemap',
+      o: 270,
     },
   }
   return pages
--- sites/shared/components/footer/index.mjs
+++ sites/shared/components/footer/index.mjs
@@ -1,3 +1,9 @@
+// Dependencies
+import orderBy from 'lodash.orderby'
+// Hooks
+import { useNavigation } from 'site/hooks/use-navigation.mjs'
+// Components
+import Link from 'next/link'
 import { Ribbon } from 'shared/components/ribbon.mjs'
 import { WordMark } from 'shared/components/wordmark.mjs'
 import { SocialIcons } from 'shared/components/social/icons.mjs'
@@ -5,27 +11,45 @@ import { Sponsors, ns as sponsorsNs } from 'shared/components/sponsors/index.mjs
 import { FreeSewingIcon } from 'shared/components/icons.mjs'
 export const ns = ['footer', ...sponsorsNs]
 
-export const Footer = ({ app }) => (
-  <footer className="bg-neutral">
-    <Ribbon />
-    <div className="w-full sm:w-auto flex flex-col gap-2 items-center justify-center pt-12">
-      <FreeSewingIcon className="w-24 lg:w-40 m-auto m-auto text-neutral-content" />
-      <div className="mt-4">
-        <WordMark />
+const onlyFooterLinks = (tree) => orderBy(tree, ['t'], ['asc']).filter((entry) => entry.f)
+
+export const Footer = () => {
+  const { siteNav } = useNavigation({ ignoreControl: true })
+
+  return (
+    <footer className="bg-neutral">
+      <Ribbon />
+      <div className="w-full sm:w-auto flex flex-col gap-2 items-center justify-center pt-12">
+        <FreeSewingIcon className="w-24 lg:w-40 m-auto m-auto text-neutral-content" />
+        <div className="mt-4">
+          <WordMark />
+        </div>
+        <p className="text-neutral-content text-normal leading-5 text-center -mt-2 opacity-70">
+          Come for the sewing patterns
+          <br />
+          Stay for the community
+        </p>
       </div>
-      <p className="text-neutral-content text-normal leading-5 text-center -mt-2 opacity-70">
-        Come for the sewing patterns
-        <br />
-        Stay for the community
-      </p>
-    </div>
 
-    <div className="w-full sm:w-auto flex flex-row flex-wrap gap-6 lg:gap-8 items-center justify-center px-8 py-14">
-      <SocialIcons />
-    </div>
+      <div className="w-full max-w-xl text-center py-8 m-auto">
+        <ul className="text-neutral-content list inline font-medium text-center">
+          {onlyFooterLinks(siteNav).map((page) => (
+            <li key={page.s} className="block lg:inline">
+              <Link href={page.s} className="p-3 hover:text-secondary-focus hover:text-underline">
+                {page.t}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-    <div className="mt-8 py-8 px-8 flex flex-row gap-8 flex-wrap 2xl:flex-nowrap justify-around text-neutral-content py-10 border border-solid border-l-0 border-r-0 border-b-0 border-base-300">
-      <Sponsors />
-    </div>
-  </footer>
-)
+      <div className="w-full sm:w-auto flex flex-row flex-wrap gap-6 lg:gap-8 items-center justify-center px-8 py-14">
+        <SocialIcons />
+      </div>
+
+      <div className="mt-8 py-8 px-8 flex flex-row gap-8 flex-wrap 2xl:flex-nowrap justify-around text-neutral-content py-10 border border-solid border-l-0 border-r-0 border-b-0 border-base-300">
+        <Sponsors />
+      </div>
+    </footer>
+  )
+}
