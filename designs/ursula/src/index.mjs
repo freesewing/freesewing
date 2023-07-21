c9c35308e45f87293bd49de535a135bdaac6d8d5
joostdecock <joost@joost.at>
2023-07-21 20:20:44 +0200

    DO NOT MERGE: Diff unice/ursula

--- designs/ursula/src/index.mjs
+++ designs/ursula/src/index.mjs
@@ -1,16 +1,16 @@
 import { Design } from '@freesewing/core'
 import { data } from '../data.mjs'
 import { i18n } from '../i18n/index.mjs'
-import { back } from './back.mjs'
 import { front } from './front.mjs'
+import { back } from './back.mjs'
 import { gusset } from './gusset.mjs'
 import { elastic } from './elastic.mjs'
 
 // Setup our new design
-const Ursula = new Design({
+const Unice = new Design({
   data,
-  parts: [back, front, gusset, elastic],
+  parts: [front, back, gusset, elastic],
 })
 
 // Named exports
-export { back, front, gusset, elastic, Ursula, i18n }
+export { front, back, gusset, elastic, Unice, i18n }
