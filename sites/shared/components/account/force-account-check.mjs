185dc80cbfe92481fecf124c9dc3e761d0315815
joostdecock <joost@joost.at>
2023-07-26 20:17:33 +0200

    chore: Fix linter warnings

--- sites/shared/components/account/force-account-check.mjs
+++ sites/shared/components/account/force-account-check.mjs
@@ -1,19 +1,13 @@
 // Dependencies
 import { useState, useEffect } from 'react'
-import { useTranslation } from 'next-i18next'
 // Hooks
 import { useAccount } from 'shared/hooks/use-account.mjs'
 import { useBackend } from 'shared/hooks/use-backend.mjs'
-import { useToast } from 'shared/hooks/use-toast.mjs'
-
-export const ns = ['account', 'toast']
 
 export const ForceAccountCheck = ({ showWelcome = false, trigger = null }) => {
   // Hooks
   const { account, setAccount, token, logout } = useAccount()
   const backend = useBackend(token)
-  const { t } = useTranslation(ns)
-  const toast = useToast()
 
   // State
   const [lastCheck, setLastCheck] = useState(Date.now())
