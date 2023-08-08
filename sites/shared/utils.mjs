8faa014973f262f7a46639bad3627773d8c61dfd
Joost De Cock <joost@joost.at>
2023-08-08 09:39:07 +0200

    chore: Remove sanity

--- sites/shared/utils.mjs
+++ sites/shared/utils.mjs
@@ -101,41 +101,6 @@ export const optionType = (option) => {
 export const capitalize = (string) =>
   typeof string === 'string' ? string.charAt(0).toUpperCase() + string.slice(1) : ''
 
-export const strapiImage = (
-  img,
-  sizes = ['thumbnail', 'xlarge', 'large', 'medium', 'small', 'xsmall']
-) => {
-  const image = {
-    caption: img.caption || '',
-    w: img.width,
-    h: img.height,
-    url: img.url,
-    sizes: {},
-  }
-  for (const size of sizes) {
-    if (img.formats[size])
-      image.sizes[size] = {
-        w: img.formats[size].width,
-        h: img.formats[size].height,
-        url: img.formats[size].url,
-      }
-  }
-
-  // Some images only have a small original, and thus no (resized) sizes
-  // In that case, return the original for the requested size
-  if (Object.keys(image.sizes).length < 1) {
-    for (const size of sizes) {
-      image.sizes[size] = {
-        w: img.width,
-        h: img.height,
-        url: img.url,
-      }
-    }
-  }
-
-  return image
-}
-
 export const getCrumbs = (app, slug = false) => {
   if (!slug) return null
   const crumbs = []
