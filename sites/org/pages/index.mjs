b8ba1991962a10f3c71a597ff760fbe9224c3f61
joostdecock <joost@joost.at>
2023-08-25 11:25:33 +0200

    chore(org): Reduce colors on the homepage

--- sites/org/pages/index.mjs
+++ sites/org/pages/index.mjs
@@ -12,11 +12,13 @@ import { BareLayout } from 'site/components/layouts/bare.mjs'
 import { ForceAccountCheck } from 'shared/components/account/force-account-check.mjs'
 import {
   OkIcon,
+  NoIcon,
   DesignIcon,
   ShowcaseIcon,
   DocsIcon,
   HelpIcon,
   ChatIcon,
+  FreeSewingIcon,
 } from 'shared/components/icons.mjs'
 import { FreeSewingAnimation } from 'shared/components/animations/freesewing.mjs'
 import { HowDoesItWorkAnimation } from 'shared/components/animations/how-does-it-work.mjs'
@@ -26,18 +28,35 @@ import Link from 'next/link'
 
 const ns = nsMerge(pageNs, 'patrons', 'common', 'homepage', 'signup', 'errors')
 
-const CardLink = ({ bg, textColor, href, title, text, icon }) => (
+const Card = ({ bg = 'bg-base-200', textColor = 'text-base-content', title, children, icon }) => (
+  <div className={`px-8 ${bg} py-10 rounded-lg block ${textColor} shadow-lg grow`}>
+    <h2 className="mb-4 text-inherit flex flex-row gap-4 justify-between items-center font-medium">
+      {title}
+      {icon}
+    </h2>
+    {children}
+  </div>
+)
+
+const CardLink = ({
+  bg = 'bg-base-200',
+  textColor = 'text-base-content',
+  href,
+  title,
+  text,
+  icon,
+}) => (
   <Link
     href={href}
     className={`px-8 ${bg} py-10 rounded-lg block ${textColor}
     hover:bg-secondary hover:text-secondary-content shadow-lg
     transition-color duration-300 grow`}
   >
-    <h2 className="mb-4 text-inherit flex flex-row gap-4 items-center font-light">
-      {icon}
+    <h2 className="mb-4 text-inherit flex flex-row gap-4 justify-between items-center font-medium">
       {title}
+      {icon}
     </h2>
-    <p className="font-medium text-inherit italic">{text}</p>
+    <p className="font-medium text-inherit italic text-lg">{text}</p>
   </Link>
 )
 
@@ -90,21 +109,20 @@ const HomePage = ({ page }) => {
         </div>
 
         <div className="flex flex-col gap-8 md:grid md:grid-cols-2 md:gap-4 mt-12 md:mt-20 md:px-4">
-          <div className="p-1 bg-gradient-to-tr from-accent to-primary rounded-none md:rounded-xl md:shadow -mx-2 px-2 md:mx-auto md:px-1 flex flex-col">
-            <div className="bg-base-100 px-4 md:px-8 py-10 rounded-none md:rounded-lg grow">
-              <h2 className="mb-4">{t('whatIsFreeSewing')}</h2>
-              <p className="font-medium">{t('homepage:what1')}</p>
-              <p className="font-medium">{t('homepage:what3')}</p>
-            </div>
-          </div>
-
-          <div className="p-1 bg-gradient-to-tr from-info to-neutral rounded-none md:rounded-xl md:shadow -mx-2 px-2 md:mx-auto md:px-1 flex flex-col">
-            <div className="bg-base-100 px-4 md:px-8 py-10 rounded-none md:rounded-lg grow">
-              <h2 className="mb-4">{t('whatIsFreeSewingNot')}</h2>
-              <p className="font-medium">{t('homepage:whatNot1')}</p>
-              <p className="font-medium">{t('homepage:whatNot2')}</p>
-            </div>
-          </div>
+          <Card
+            title={t('whatIsFreeSewing')}
+            icon={<OkIcon className="w-12 h-12 text-success" stroke={4} />}
+          >
+            <p className="font-medium text-lg">{t('homepage:what1')}</p>
+            <p className="font-medium text-lg">{t('homepage:what3')}</p>
+          </Card>
+          <Card
+            title={t('whatIsFreeSewingNot')}
+            icon={<NoIcon className="w-12 h-12 text-error" stroke={3} />}
+          >
+            <p className="font-medium text-lg">{t('homepage:whatNot1')}</p>
+            <p className="font-medium text-lg">{t('homepage:whatNot2')}</p>
+          </Card>
         </div>
 
         <div className="text-center mt-20 md:mt-20">
@@ -119,32 +137,24 @@ const HomePage = ({ page }) => {
 
       <div className="flex flex-col md:grid md:grid-cols-2 gap-4 max-w-7xl m-auto mb-24 px-4">
         <CardLink
-          bg="bg-primary"
-          textColor="text-primary-content"
           href="/designs"
           title="Designs"
           icon={<DesignIcon className="w-10 h-10 shrink-0" />}
           text="Browse our collection of designs, and turn them into sewing patterns that are made-to-measure just for you."
         />
         <CardLink
-          bg="bg-primary bg-opacity-30"
-          textColor="text-base-content"
           href="/showcase"
           title="Showcase"
           icon={<ShowcaseIcon className="w-10 h-10 shrink-0" />}
           text="Get inspiration from the FreeSewing community, and see how others have applied their creativity to our designs."
         />
         <CardLink
-          bg="bg-neutral bg-opacity-30"
-          textColor="text-base-content"
           href="/docs/guide"
           title="Getting Started"
           icon={<DocsIcon className="w-10 h-10 shrink-0" />}
           text="FreeSewing.org is unlike any sewing pattern website you know. Read this short guide to get the most our of our platform."
         />
         <CardLink
-          bg="bg-neutral"
-          textColor="text-neutral-content"
           href="/docs/faq"
           title="Frequently Asked Questions"
           icon={<HelpIcon className="w-10 h-10 shrink-0" />}
@@ -155,8 +165,6 @@ const HomePage = ({ page }) => {
       <div className="max-w-7xl m-auto mb-24 px-4">
         <div className="w-full lg:w-1/2 m-auto">
           <CardLink
-            bg="bg-accent bg-opacity-30"
-            textColor="text-base-content"
             href="/support"
             title="Need Help?"
             icon={<ChatIcon className="w-10 h-10 shrink-0" />}
