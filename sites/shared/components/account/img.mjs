034477cc633f4f7c81edabf0dea17d1e142cf781
joostdecock <joost@joost.at>
2023-08-25 18:10:14 +0200

    feat(shared): Ported account/img to new inputs

--- sites/shared/components/account/img.mjs
+++ sites/shared/components/account/img.mjs
@@ -11,6 +11,8 @@ import { useLoadingStatus } from 'shared/hooks/use-loading-status.mjs'
 import { Icons, welcomeSteps, BackToAccountButton } from './shared.mjs'
 import { ContinueButton } from 'shared/components/buttons/continue-button.mjs'
 import { SaveSettingsButton } from 'shared/components/buttons/save-settings-button.mjs'
+import { PassiveImageInput } from 'shared/components/inputs.mjs'
+import { DynamicOrgDocs } from 'shared/components/dynamic-docs/org.mjs'
 
 export const ns = ['account', 'status']
 
@@ -18,24 +20,13 @@ export const ImgSettings = ({ title = false, welcome = false }) => {
   const { account, setAccount } = useAccount()
   const backend = useBackend()
   const { setLoadingStatus, LoadingStatus } = useLoadingStatus()
-  const { t } = useTranslation(ns)
+  const { t, i18n } = useTranslation(ns)
 
-  const [img, setImg] = useState(false)
-  const [url, setUrl] = useState('')
-
-  const onDrop = useCallback((acceptedFiles) => {
-    const reader = new FileReader()
-    reader.onload = () => {
-      setImg(reader.result)
-    }
-    acceptedFiles.forEach((file) => reader.readAsDataURL(file))
-  }, [])
-
-  const { getRootProps, getInputProps } = useDropzone({ onDrop })
+  const [img, setImg] = useState('')
 
   const save = async () => {
     setLoadingStatus([true, 'processingUpdate'])
-    const result = await backend.updateAccount({ img: url ? url : img })
+    const result = await backend.updateAccount({ img })
     if (result.success) {
       setAccount(result.data.account)
       setLoadingStatus([true, 'settingsSaved', true, true])
@@ -47,41 +38,21 @@ export const ImgSettings = ({ title = false, welcome = false }) => {
   return (
     <div className="max-w-xl">
       <LoadingStatus />
-      {title ? <h2 className="text-4xl">{t('imgTitle')}</h2> : null}
-      <div>
-        {!welcome || img !== false ? (
-          <img
-            alt="img"
-            src={img || cloudflareImageUrl({ id: `user-${account.ihash}`, variant: 'public' })}
-            className="shadow mb-4"
-          />
-        ) : null}
-        <div
-          {...getRootProps()}
-          className={`
-          flex rounded-lg w-full flex-col items-center justify-center
-          lg:h-64 lg:border-4 lg:border-secondary lg:border-dashed
-        `}
-        >
-          <input {...getInputProps()} />
-          <p className="hidden lg:block p-0 m-0">{t('imgDragAndDropImageHere')}</p>
-          <p className="hidden lg:block p-0 my-2">{t('or')}</p>
-          <button className={`btn btn-secondary btn-outline mt-4 px-8`}>
-            {t('imgSelectImage')}
-          </button>
-        </div>
-        <p className="hidden lg:block p-0 my-2 text-center">{t('or')}</p>
-        <div className="flex flex-row items-center">
-          <input
-            type="url"
-            className="input input-secondary w-full input-bordered"
-            placeholder="Paste an image URL here"
-            value={url}
-            onChange={(evt) => setUrl(evt.target.value)}
-          />
-        </div>
-      </div>
-
+      {!welcome || img !== false ? (
+        <img
+          alt="img"
+          src={img || cloudflareImageUrl({ id: `user-${account.ihash}`, variant: 'public' })}
+          className="shadow mb-4"
+        />
+      ) : null}
+      <PassiveImageInput
+        label={t('image')}
+        placeholder={'image'}
+        update={setImg}
+        current={img}
+        valid={(val) => val.length > 0}
+        docs={<DynamicOrgDocs language={i18n.language} path={`site/account/img`} />}
+      />
       {welcome ? (
         <>
           <button className={`btn btn-secondary mt-4 px-8`} onClick={save} disabled={!img}>
