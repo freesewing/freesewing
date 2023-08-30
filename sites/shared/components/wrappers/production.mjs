61d743a5a50cd6ad55828dfa71ad07590b000f30
Joost De Cock <joost@joost.at>
2023-08-30 17:51:35 +0200

    chore(shared): Better prodcutionWrapper

--- sites/shared/components/wrappers/production.mjs
+++ sites/shared/components/wrappers/production.mjs
@@ -1,4 +1,4 @@
 import { isProduction } from 'shared/freesewing.config.mjs'
 
-export const ProductionWrapper = ({ only = false, not = true, children }) =>
-  !not || only ? children : null
+export const NotInProduction = ({ children }) => (isProduction ? null : children)
+export const OnlyInProduction = ({ children }) => (isProduction ? children : null)
