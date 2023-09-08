c94ab04be115090070da69dbbf581284ab728929
joostdecock <joost@joost.at>
2023-09-08 11:59:18 +0200

    fix(bee): Typo in translation key

--- plugins/plugin-round/src/index.mjs
+++ plugins/plugin-round/src/index.mjs
@@ -13,8 +13,8 @@ export const plugin = {
       let via = so.via
       let radius = so.radius
       let prefix = so.prefix || 'round'
-      let angle1 = from.angle(via)
-      let angle2 = via.angle(to)
+      //let angle1 = from.angle(via)
+      //let angle2 = via.angle(to)
       //if ((Math.round(angle1) - Math.round(angle2)) % 90 !== 0)
       //  console.log('Warning: The round macro only handles 90 degree angles correctly.')
       let fd = from.dist(via)
