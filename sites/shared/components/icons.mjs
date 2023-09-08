c24b8ab3508a72679c66ede27e9dfe3123c359bf
joostdecock <joost@joost.at>
2023-09-08 13:36:13 +0200

    fix(shared): Missing icons

--- sites/shared/components/icons.mjs
+++ sites/shared/components/icons.mjs
@@ -282,6 +282,12 @@ export const EmailIcon = (props) => (
   </IconWrapper>
 )
 
+export const ErrorIcon = (props) => (
+  <IconWrapper {...props}>
+    <path d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+  </IconWrapper>
+)
+
 export const ExpandIcon = (props) => (
   <IconWrapper {...props}>
     <path d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
