3362afac04a0ba61390ee828df02880aca6d6eab
Joost De Cock <joost@joost.at>
2023-09-10 10:56:41 +0200

    feat(aaron): Make knit binding adjustable

--- designs/aaron/src/arm-binding.mjs
+++ designs/aaron/src/arm-binding.mjs
@@ -13,6 +13,7 @@ export const armBinding = {
     Snippet,
     snippets,
     macro,
+    complete,
     expand,
     units,
     part,
@@ -72,6 +73,24 @@ export const armBinding = {
       align: 'center',
       scale: 0.5,
     })
+
+    // Fold lines
+    if (complete) {
+      for (const i of [1, 2, 3]) {
+        paths[`fold${i}`] = new Path()
+          .move(points.topLeft.shiftFractionTowards(points.topRight, 0.25 * i))
+          .line(points.bottomLeft.shiftFractionTowards(points.bottomRight, 0.25 * i))
+          .addClass('note help')
+        macro('banner', {
+          id: `foldHere${i}`,
+          path: paths[`fold${i}`],
+          text: 'foldHere',
+          classes: 'fill-note text-sm center',
+          repeat: 30,
+          spaces: 60,
+        })
+      }
+    }
 
     // Logo
     points.logo = points.title.shift(-90, 75)
