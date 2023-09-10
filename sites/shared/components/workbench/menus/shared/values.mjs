b7c89bdd2e6133d0c1ce29aa4f56edcad2680b7a
Joost De Cock <joost@joost.at>
2023-09-10 16:23:46 +0200

    fix(shared): Typo in icon name

--- sites/shared/components/workbench/menus/shared/values.mjs
+++ sites/shared/components/workbench/menus/shared/values.mjs
@@ -40,7 +40,7 @@ export const ListValue = ({ current, t, config, changed }) => {
   else if (typeof val === 'string') key = val
   // otherwise stringify booleans
   else if (val) key = <BoolYesIcon />
-  else key = <dBoolNoIcon />
+  else key = <BoolNoIcon />
 
   const translated = config.doNotTranslate || typeof key !== 'string' ? key : t(key)
 
