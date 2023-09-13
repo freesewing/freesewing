5c9979172883e58b75f3a97d36d8c4b941d7f478
Joost De Cock <joost@joost.at>
2023-09-13 11:56:18 +0200

    chore(benjamin): Completed migration for v3 beta

--- designs/benjamin/src/bow2.mjs
+++ designs/benjamin/src/bow2.mjs
@@ -34,28 +34,33 @@ function draftBenjaminBow2({
     .attr('class', 'fabric')
     .unhide()
 
-  store.cutlist.addCut()
-  store.cutlist.addCut({ material: 'interfacing' })
+  if (sa) paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa').unhide()
 
-  if (complete) {
-    // Paperless?
-    if (paperless) {
-      macro('hd', {
-        from: points.bandBottomLeft,
-        to: points.tip2Bottom,
-        y: store.get('baseY'),
-      })
-    }
-    if (sa) {
-      paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa').unhide()
-    }
-    macro('title', {
-      at: points.titleAnchor,
-      nr: 2,
-      title: 'Medium Bow',
-      scale: store.get('tipWidth') / 75,
-    })
-  }
+  /*
+   * Annotations
+   */
+
+  // Cut list
+  store.cutlist.addCut([
+    { cut: 2, from: 'fabric' },
+    { cut: 2, from: 'interfacing' },
+  ])
+
+  // Title
+  macro('title', {
+    at: points.titleAnchor,
+    nr: 2,
+    title: 'Medium Bow',
+    scale: store.get('tipWidth') / 75,
+  })
+
+  // Dimensions
+  macro('hd', {
+    id: 'wFull',
+    from: points.bandBottomLeft,
+    to: points.tip2Bottom,
+    y: store.get('baseY'),
+  })
 
   return part
 }
--- designs/benjamin/src/bow3.mjs
+++ designs/benjamin/src/bow3.mjs
@@ -20,11 +20,6 @@ function draftBenjaminBow3({
 
   points.grainlineStart = new Point(-290, 0)
 
-  macro('grainline', {
-    from: points.grainlineStart,
-    to: points.tip.shift(180, 20),
-  })
-
   paths.seam = new Path()
     .move(points.bandTopLeft)
     .line(points.bandBottomLeft)
@@ -34,28 +29,38 @@ function draftBenjaminBow3({
     .attr('class', 'fabric')
     .unhide()
 
-  store.cutlist.addCut({ cut: 1 })
-  store.cutlist.addCut({ cut: 1, material: 'interfacing' })
+  if (sa) paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa').unhide()
 
-  if (complete) {
-    // Paperless?
-    if (paperless) {
-      macro('hd', {
-        from: points.bandBottomLeft,
-        to: points.tip2Bottom,
-        y: store.get('baseY'),
-      })
-    }
-    if (sa) {
-      paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa').unhide()
-    }
-    macro('title', {
-      at: points.titleAnchor,
-      nr: 3,
-      title: 'Long Bow',
-      scale: store.get('tipWidth') / 75,
-    })
-  }
+  /*
+   * Annotations
+   */
+  // Cutlist
+  store.cutlist.addCut([
+    { cut: 1, from: 'fabric' },
+    { cut: 1, material: 'interfacing' },
+  ])
+
+  // Grainline
+  macro('grainline', {
+    from: points.grainlineStart,
+    to: points.tip.shift(180, 20),
+  })
+
+  //Title
+  macro('title', {
+    at: points.titleAnchor,
+    nr: 3,
+    title: 'Long Bow',
+    scale: store.get('tipWidth') / 75,
+  })
+
+  // Dimensions
+  macro('hd', {
+    id: 'wFull',
+    from: points.bandBottomLeft,
+    to: points.tip2Bottom,
+    y: store.get('baseY'),
+  })
 
   return part
 }
