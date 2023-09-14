8dab0cdb4eb37c7c7646fdcbd4e759fe5c0f6c44
Joost De Cock <joost@joost.at>
2023-09-14 16:54:32 +0200

    fix(plugin-annotations): Fix bnotch syntax

--- plugins/plugin-annotations/src/notches.mjs
+++ plugins/plugin-annotations/src/notches.mjs
@@ -12,7 +12,7 @@ export const notchesDefs = [
     name: 'bnotch',
     def: `
 <g id="bnotch">
-  <path d="M -1.1 -1.1 L 1.1 1.1 M 1.1 -1.1 L -1.1 1.1" class="note" />
+  <path d="M -1.1 -1.1 L 1.1 1.1 M 1.1 -1.1 L -1.1 1.1" class="note"></path>
   <circle cy="0" cx="0" r="2.8" class="note" />
 </g>`,
   },
