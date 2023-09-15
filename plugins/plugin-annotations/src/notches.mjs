9955784078ea947d414e67c677e27cbdcec23e62
Joost De Cock <joost@joost.at>
2023-09-15 08:43:57 +0200

    fix(plugin-annotations): Circle syntax in defs

--- plugins/plugin-annotations/src/notches.mjs
+++ plugins/plugin-annotations/src/notches.mjs
@@ -4,8 +4,8 @@ export const notchesDefs = [
     name: 'notch',
     def: `
 <g id="notch">
-  <circle cy="0" cx="0" r="1.4" class="fill-note" />
-  <circle cy="0" cx="0" r="2.8" class="note" />
+  <circle cy="0" cx="0" r="1.4" class="fill-note"></circle>
+  <circle cy="0" cx="0" r="2.8" class="note"></circle>
 </g>`,
   },
   {
@@ -13,7 +13,7 @@ export const notchesDefs = [
     def: `
 <g id="bnotch">
   <path d="M -1.1 -1.1 L 1.1 1.1 M 1.1 -1.1 L -1.1 1.1" class="note"></path>
-  <circle cy="0" cx="0" r="2.8" class="note" />
+  <circle cy="0" cx="0" r="2.8" class="note"></circle>
 </g>`,
   },
 ]
