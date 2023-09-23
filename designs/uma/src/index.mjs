45e76dd95827be35db1b7400a134786a79fdc73e
joostdecock <joost@joost.at>
2023-09-23 07:55:25 +0200

    breaking: Out with ursula & unice, in with uma
    This is the least-worst solution of a conflict that I didn't see coming
    until it was too late. So here we are, and this is how I'm handling it:
    
    - After discussion with the Ursula designer I am reincarnating Ursula as
      Uma under my own name as per their request.
    - After discussion with the Uma designer, I am removing Uma.
    
    Let the message be clear:
    
    **If you can't play nice with others, I will take away your toys.**

---
+++ designs/uma/src/index.mjs
@@ -0,0 +1,16 @@
+import { Design } from '@freesewing/core'
+import { data } from '../data.mjs'
+import { i18n } from '../i18n/index.mjs'
+import { back } from './back.mjs'
+import { front } from './front.mjs'
+import { gusset } from './gusset.mjs'
+import { elastic } from './elastic.mjs'
+
+// Setup our new design
+const Uma = new Design({
+  data,
+  parts: [back, front, gusset, elastic],
+})
+
+// Named exports
+export { back, front, gusset, elastic, Uma, i18n }
--- markdown/org/docs/designs/ursula/options/en.md
+++
@@ -1,5 +0,0 @@
----
-title: "Ursula undies: Design Options"
----
-
-<PatternOptions pattern='ursula' />
