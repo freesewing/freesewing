05e66de1d6305e6bd2a77f147f4370ba684b24de
woutervdub <wouter.vdub@yahoo.com>
2023-09-28 03:56:24 +0000

    Implements #4952

--- designs/noble/src/options.mjs
+++ designs/noble/src/options.mjs
@@ -1,9 +1,11 @@
+import { pctBasedOn } from '@freesewing/core'
+
 // Constants
 export const shoulderToShoulderCorrection = 0.995
 export const bustDartCurve = 1
 export const bustDartLength = 0.9
 // Percentages
-export const bustSpanEase = { pct: 0, min: -5, max: 20, menu: 'fit' }
+export const bustSpanEase = { pct: 0, min: -5, max: 20, ...pctBasedOn('bustSpan'), menu: 'fit' }
 export const backHemSlope = { deg: 2.5, min: 0, max: 5, menu: 'advanced' }
 export const upperDartLength = { pct: 90, min: 80, max: 95, menu: 'darts' }
 export const dartPosition = { dflt: 'shoulder', list: ['shoulder', 'armhole'], menu: 'darts' }
@@ -11,13 +13,27 @@ export const shoulderDartPosition = {
   pct: 50,
   min: 10,
   max: 90,
-  // eslint-disable-next-line no-unused-vars
-  menu: (settings, mergedOptions) => (mergedOptions?.dartPosition != 'shoulder' ? 'darts' : false),
+  menu: (settings, mergedOptions) => {
+    return settings.options.dartPosition === undefined
+      ? mergedOptions.dartPosition == 'shoulder'
+        ? 'darts'
+        : false
+      : settings.options.dartPosition == 'shoulder'
+      ? 'darts'
+      : false
+  },
 }
 export const armholeDartPosition = {
   pct: 50,
   min: 10,
   max: 90,
-  // eslint-disable-next-line no-unused-vars
-  menu: (settings, mergedOptions) => (mergedOptions?.dartPosition == 'shoulder' ? 'darts' : false),
+  menu: (settings, mergedOptions) => {
+    return settings.options.dartPosition === undefined
+      ? mergedOptions.dartPosition == 'armhole'
+        ? 'darts'
+        : false
+      : settings.options.dartPosition == 'armhole'
+      ? 'darts'
+      : false
+  },
 }
