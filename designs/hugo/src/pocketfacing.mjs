87311a541a40434a78e9d27ef419867b62efd158
joostdecock <joost@joost.at>
2023-10-01 17:52:41 +0200

    fix(hugo): Avoid flag/sa confusion. See #5057

--- designs/hugo/src/pocketfacing.mjs
+++ designs/hugo/src/pocketfacing.mjs
@@ -1,25 +1,6 @@
 import { pocket } from './pocket.mjs'
 
 function hugoPocketFacing({ sa, points, Path, paths, macro, store, expand, part }) {
-  if (expand) store.flag.preset('expandIsOn')
-  else {
-    // Expand is on, do not draw the part but flag this to the user
-    store.flag.note({
-      msg: `hugo:cutPocketFacing`,
-      suggest: {
-        text: 'flag:show',
-        icon: 'expand',
-        update: {
-          settings: ['expand', 1],
-        },
-      },
-    })
-    // Also hint about expand
-    store.flag.preset('expandIsOff')
-
-    return part.hide()
-  }
-
   // Remove clutter
   for (const key in paths) {
     if (key !== 'facing') delete paths[key]
@@ -27,6 +8,7 @@ function hugoPocketFacing({ sa, points, Path, paths, macro, store, expand, part 
 
   paths.seam = paths.facing
     .clone()
+    .unhide()
     .line(points.pocketTop)
     .curve(points.pocketTopCp, points.pocketTip, points.pocketTip)
     .line(points.facingEnd)
