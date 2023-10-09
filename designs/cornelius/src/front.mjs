b522ce91bb3a44e33dd5c0b294fc80626399dcd3
woutervdub <wouter.vdub@yahoo.com>
2023-10-09 18:04:39 +0000

    Implements #4949

--- designs/cornelius/src/front.mjs
+++ designs/cornelius/src/front.mjs
@@ -1,116 +1,138 @@
+import { pctBasedOn } from '@freesewing/core'
 import { frontpoints } from './frontpoints.mjs'
 
-function draftCorneliusFront({
-  options,
-  Path,
-  points,
-  paths,
-  Snippet,
-  snippets,
-  complete,
-  sa,
-  store,
-  paperless,
-  macro,
-  part,
-}) {
-  const cc = 0.551915024494 // circle constant
+export const front = {
+  name: 'cornelius.front',
+  from: frontpoints,
+  options: {
+    pctZtoR: 0.35,
+    pctRtoZin: 0.75,
+    pctRtoZup: 0.25,
+    pctRtoKin: 0.75,
+    pctRtoKdown: 0.25,
+    pctKtoRout: 0.15,
+    pctKtoRup: 0.25,
+    pctKtoH: 0.7,
+    flyWidth: { pct: 0.38, min: 0.2, max: 0.6, ...pctBasedOn('waist'), menu: 'style' },
+  },
+  draft: ({
+    options,
+    Path,
+    points,
+    paths,
+    Snippet,
+    snippets,
+    sa,
+    store,
+    measurements,
+    macro,
+    part,
+  }) => {
+    const cc = 0.551915024494 // circle constant
 
-  let halfInch = store.get('halfInch')
-  let ventLength = store.get('ventLength')
-  let waist = store.get('waist')
-  let keystone = options.cuffStyle == 'keystone'
+    const halfInch = store.get('halfInch')
+    const ventLength = store.get('ventLength')
+    const waist = store.get('waist')
+    const keystone = options.cuffStyle == 'keystone'
 
-  let flyWidth = 3.5
-  store.set('flyWidth', flyWidth)
+    const flyWidth = options.flyWidth * measurements.waist
+    store.set('flyWidth', flyWidth)
 
-  store.set('sideSeam', paths.sideSeam.length())
+    store.set('sideSeam', paths.sideSeam.length())
 
-  points.pZcpR = points.pZ.shiftFractionTowards(points.pX, options.pctZtoR)
-  points.pRcpZ = points.pR
-    .shiftFractionTowards(points.pF, options.pctRtoZin)
-    .shiftFractionTowards(points.pZ, options.pctRtoZup)
+    points.pZcpR = points.pZ.shiftFractionTowards(points.pX, options.pctZtoR)
+    points.pRcpZ = points.pR
+      .shiftFractionTowards(points.pF, options.pctRtoZin)
+      .shiftFractionTowards(points.pZ, options.pctRtoZup)
 
-  let waistAngle = Math.abs(points.pW.angle(points.pD) - points.pW.angle(points.pZ))
-  if (waistAngle > 180) {
-    waistAngle -= 180
-  }
+    let waistAngle = Math.abs(points.pW.angle(points.pD) - points.pW.angle(points.pZ))
+    if (waistAngle > 180) {
+      waistAngle -= 180
+    }
 
-  points.flyTop = points.pW.shift(
-    points.pW.angle(points.pZ) - 180 + waistAngle,
-    halfInch * flyWidth
-  )
-  points.flyBottom = points.flyTop.shift(
-    points.pW.angle(points.pZ),
-    points.pW.dist(points.pZ) - halfInch * flyWidth
-  )
+    points.flyTop = points.pW.shift(
+      points.pW.angle(points.pZ) - 180 + waistAngle,
+      halfInch * flyWidth
+    )
+    points.flyBottom = points.flyTop.shift(
+      points.pW.angle(points.pZ),
+      points.pW.dist(points.pZ) - halfInch * flyWidth
+    )
 
-  points.pZcpFB = points.pZ.shift(points.pW.angle(points.pZ) - waistAngle, halfInch * flyWidth * cc)
-  points.pFBcpZ = points.flyBottom.shift(points.pW.angle(points.pZ), halfInch * flyWidth * cc)
+    points.pZcpFB = points.pZ.shift(
+      points.pW.angle(points.pZ) - waistAngle,
+      halfInch * flyWidth * cc
+    )
+    points.pFBcpZ = points.flyBottom.shift(points.pW.angle(points.pZ), halfInch * flyWidth * cc)
 
-  paths.fly = new Path()
-    .move(points.pW)
-    .line(points.flyTop)
-    .line(points.flyBottom)
-    .curve(points.pFBcpZ, points.pZcpFB, points.pZ)
-    .hide()
+    paths.fly = new Path()
+      .move(points.pW)
+      .line(points.flyTop)
+      .line(points.flyBottom)
+      .curve(points.pFBcpZ, points.pZcpFB, points.pZ)
+      .hide()
 
-  paths.flyFold = new Path().move(points.pW).line(points.pZ).attr('class', 'fabric dashed')
-  store.set('flyLength', paths.flyFold.length())
+    paths.flyFold = new Path().move(points.pW).line(points.pZ).attr('class', 'fabric dashed')
+    store.set('flyLength', paths.flyFold.length())
 
-  store.set('frontWaistLength', paths.waistSeam.line(points.flyTop).length())
+    store.set('frontWaistLength', paths.waistSeam.line(points.flyTop).length())
 
-  paths.crotchSeam = new Path().move(points.pZ).curve(points.pZcpR, points.pRcpZ, points.pR).hide()
+    paths.crotchSeam = new Path()
+      .move(points.pZ)
+      .curve(points.pZcpR, points.pRcpZ, points.pR)
+      .hide()
 
-  points.pRcpK = points.pR
-    .shiftFractionTowards(points.pF, options.pctRtoKin)
-    .shiftFractionTowards(points.pK, options.pctRtoKdown)
-  points.pKcpR = points.pK
-    .shiftFractionTowards(points.pH, -1 * (options.pctKtoRout + options.fullness))
-    .shiftFractionTowards(points.pR, options.pctKtoRup)
+    points.pRcpK = points.pR
+      .shiftFractionTowards(points.pF, options.pctRtoKin)
+      .shiftFractionTowards(points.pK, options.pctRtoKdown)
+    points.pKcpR = points.pK
+      .shiftFractionTowards(points.pH, -1 * (options.pctKtoRout + options.fullness))
+      .shiftFractionTowards(points.pR, options.pctKtoRup)
 
-  paths.insideSeam = new Path().move(points.pR).curve(points.pRcpK, points.pKcpR, points.pK).hide()
-  store.set('insideSeam', paths.insideSeam.length())
+    paths.insideSeam = new Path()
+      .move(points.pR)
+      .curve(points.pRcpK, points.pKcpR, points.pK)
+      .hide()
+    store.set('insideSeam', paths.insideSeam.length())
 
-  let tempP = points.pH.shift(270, halfInch * 1.5)
-  points.pKcpH = points.pK.shiftFractionTowards(tempP, options.pctKtoH)
-  points.pJcpH = points.pJ.shiftFractionTowards(tempP, options.pctKtoH)
+    let tempP = points.pH.shift(270, halfInch * 1.5)
+    points.pKcpH = points.pK.shiftFractionTowards(tempP, options.pctKtoH)
+    points.pJcpH = points.pJ.shiftFractionTowards(tempP, options.pctKtoH)
 
-  paths.legSeam = new Path().move(points.pK).curve(points.pKcpH, points.pJcpH, points.pJ).hide()
+    paths.legSeam = new Path().move(points.pK).curve(points.pKcpH, points.pJcpH, points.pJ).hide()
 
-  store.set('frontLegSeam', paths.legSeam.length())
+    store.set('frontLegSeam', paths.legSeam.length())
 
-  // Keystone original:
-  // The keystone design has the slit in the cuff to the front. I deviated from
-  // this for comfort and ease of construction preferences. The 'slit' is now
-  // part of the side seam.
-  if (keystone) {
-    points.pSlitBottom = paths.legSeam.shiftAlong(paths.legSeam.length() - halfInch * 4)
-    points.pSlitTop = points.pSlitBottom.shift(90, halfInch * 5 * options.ventLength)
-    store.set('slitDistance', paths.legSeam.length() - halfInch * 4)
-  }
+    // Keystone original:
+    // The keystone design has the slit in the cuff to the front. I deviated from
+    // this for comfort and ease of construction preferences. The 'slit' is now
+    // part of the side seam.
+    if (keystone) {
+      points.pSlitBottom = paths.legSeam.shiftAlong(paths.legSeam.length() - halfInch * 4)
+      points.pSlitTop = points.pSlitBottom.shift(90, halfInch * 5 * options.ventLength)
+      store.set('slitDistance', paths.legSeam.length() - halfInch * 4)
+    }
 
-  points.pocketWaist = paths.waistSeam.shiftAlong(waist / 2 / 4.5)
-  points.pocketSide = paths.sideSeam.shiftAlong(paths.sideSeam.length() - (waist / 2 / 4.5) * 3.5)
+    points.pocketWaist = paths.waistSeam.shiftAlong(waist / 2 / 4.5)
+    points.pocketSide = paths.sideSeam.shiftAlong(paths.sideSeam.length() - (waist / 2 / 4.5) * 3.5)
 
-  paths.pocketSeam = new Path().move(points.pocketSide).line(points.pocketWaist).hide()
+    paths.pocketSeam = new Path().move(points.pocketSide).line(points.pocketWaist).hide()
 
-  paths.waistSeam = paths.waistSeam.split(points.pocketWaist)[1]
-  paths.sideSeam = paths.sideSeam.split(points.pocketSide)[0]
+    paths.waistSeam = paths.waistSeam.split(points.pocketWaist)[1]
+    paths.sideSeam = paths.sideSeam.split(points.pocketSide)[0]
 
-  paths.seam = paths.waistSeam
-    .join(paths.fly)
-    .join(paths.crotchSeam)
-    .join(paths.insideSeam)
-    .join(paths.legSeam)
-    .join(paths.sideSeam)
-    .join(paths.pocketSeam)
-    .close()
-    .attr('class', 'fabric')
+    paths.seam = paths.waistSeam
+      .join(paths.fly)
+      .join(paths.crotchSeam)
+      .join(paths.insideSeam)
+      .join(paths.legSeam)
+      .join(paths.sideSeam)
+      .join(paths.pocketSeam)
+      .close()
+      .attr('class', 'fabric')
 
-  points.topOfVent = paths.sideSeam.shiftAlong(ventLength)
+    points.topOfVent = paths.sideSeam.shiftAlong(ventLength)
 
-  if (complete) {
     snippets.n1 = new Snippet('notch', points.pK)
     snippets.n2 = new Snippet('notch', points.pJ)
     snippets.n3 = new Snippet('notch', points.pocketWaist)
@@ -135,11 +157,15 @@ function draftCorneliusFront({
     }
     snippets.n5 = new Snippet('notch', points.topOfVent)
 
+    points.gridAnchor = points.pO.clone()
+
+    store.cutlist.addCut({ cut: 2, from: 'fabric' })
+
     points.logo = points.pE.clone()
     snippets.logo = new Snippet('logo', points.logo)
     points.title = points.logo.shift(270, 50)
     macro('title', {
-      nr: 76,
+      nr: 5,
       at: points.title,
       title: 'Front',
       align: 'center',
@@ -156,37 +182,35 @@ function draftCorneliusFront({
     if (sa) {
       paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa')
     }
-  }
 
-  // Paperless?
-  if (paperless) {
     macro('ld', {
       id: 1,
       from: points.topOfVent,
       to: points.pJ,
+      d: sa + 15,
     })
     macro('hd', {
       id: 2,
-      from: points.pocketSide,
-      to: points.pocketWaist,
-      y: points.pocketWaist.y + 15,
+      from: points.pocketWaist,
+      to: points.pocketSide,
+      y: points.pocketWaist.y - sa - 15,
     })
     macro('hd', {
       id: 3,
       from: points.pW,
       to: points.pocketWaist,
-      y: points.pocketWaist.y + 15,
+      y: points.pocketWaist.y - sa - 15,
     })
     macro('hd', {
       id: 4,
-      from: points.pocketWaist,
-      to: points.flyTop,
-      y: points.pocketWaist.y + 15,
+      from: points.flyTop,
+      to: points.pW,
+      y: points.pocketWaist.y - sa - 15,
     })
     macro('hd', {
       id: 5,
-      from: points.pAextra,
-      to: points.pR,
+      from: points.pR,
+      to: points.pAextra,
     })
     macro('hd', {
       id: 6,
@@ -204,9 +228,9 @@ function draftCorneliusFront({
       })
       macro('vd', {
         id: 8,
-        from: points.pSlitTop,
-        to: points.pSlitBottom,
-        x: points.pSlitTop.x + 15,
+        from: points.pSlitBottom,
+        to: points.pSlitTop,
+        x: points.pSlitTop.x - 15,
       })
     }
     macro('vd', {
@@ -217,20 +241,20 @@ function draftCorneliusFront({
     })
     macro('vd', {
       id: 10,
-      from: points.pW,
-      to: points.pR,
-      x: points.pR.x,
+      from: points.pR,
+      to: points.pW,
+      x: points.pR.x - sa - 15,
     })
     macro('vd', {
       id: 11,
-      from: points.pR,
-      to: points.pK,
-      x: points.pR.x,
+      from: points.pK,
+      to: points.pR,
+      x: points.pR.x - sa - 15,
     })
     macro('vd', {
       id: 12,
-      from: points.pW,
-      to: points.pZ,
+      from: points.pZ,
+      to: points.pW,
       x: points.pW.x + 15,
     })
     macro('vd', {
@@ -239,23 +263,7 @@ function draftCorneliusFront({
       to: points.pocketWaist,
       x: points.pocketWaist.x,
     })
-  }
 
-  return part
-}
-
-export const front = {
-  name: 'cornelius.front',
-  from: frontpoints,
-  options: {
-    pctZtoR: 0.35,
-    pctRtoZin: 0.75,
-    pctRtoZup: 0.25,
-    pctRtoKin: 0.75,
-    pctRtoKdown: 0.25,
-    pctKtoRout: 0.15,
-    pctKtoRup: 0.25,
-    pctKtoH: 0.7,
+    return part
   },
-  draft: draftCorneliusFront,
 }
--- designs/cornelius/src/pocketfacing.mjs
+++ designs/cornelius/src/pocketfacing.mjs
@@ -1,68 +1,60 @@
 import { frontpoints } from './frontpoints.mjs'
 
-function draftCorneliusPocketFacing({
-  Path,
-  Point,
-  points,
-  paths,
-  Snippet,
-  snippets,
-  complete,
-  sa,
-  store,
-  paperless,
-  macro,
-  part,
-}) {
-  let halfInch = store.get('halfInch')
-  let waist = store.get('waist')
+export const pocketFacing = {
+  name: 'cornelius.pocketFacing',
+  from: frontpoints,
+  draft: ({ Path, Point, points, paths, Snippet, snippets, sa, store, macro, part }) => {
+    const halfInch = store.get('halfInch')
+    const waist = store.get('waist')
 
-  paths.waistSeam = paths.waistSeam.split(points.pocketFacingTL)[0].hide()
+    paths.waistSeam = paths.waistSeam.split(points.pocketFacingTL)[0].hide()
 
-  // Adding sa to this point so it allows for the pocket seam to be finished without the facing being included
-  points.pocketFacingBR = paths.sideSeam.shiftAlong(
-    paths.sideSeam.length() - (waist / 2 / 4.5) * 3.5 - halfInch * 3 + sa
-  )
-  // If we split at the exact point where the curves join, things fall apart
-  // So let's work around that until this is handled in core
-  if (points.pocketFacingBR.sitsOn(points.pAextra))
-    paths.sideSeam = new Path()
-      .move(points.pAextra)
-      .curve(points.pAextraCPu, points.pUcpA, points.pU)
+    // Adding sa to this point so it allows for the pocket seam to be finished without the facing being included
+    points.pocketFacingBR = paths.sideSeam.shiftAlong(
+      paths.sideSeam.length() - (waist / 2 / 4.5) * 3.5 - halfInch * 3 + sa
+    )
+    // If we split at the exact point where the curves join, things fall apart
+    // So let's work around that until this is handled in core
+    if (points.pocketFacingBR.sitsOn(points.pAextra))
+      paths.sideSeam = new Path()
+        .move(points.pAextra)
+        .curve(points.pAextraCPu, points.pUcpA, points.pU)
+        .hide()
+    else paths.sideSeam = paths.sideSeam.split(points.pocketFacingBR)[1].hide()
+
+    points.brCPtl = points.pocketFacingBR.shift(
+      points.pocketFacingBR.angle(points.pocketSide) + 90,
+      halfInch * 3
+    )
+    points.tlCPbr = points.pocketFacingTL.shift(
+      points.pocketFacingTL.angle(points.pocketWaist) - 90,
+      halfInch * 6
+    )
+
+    paths.facingInside = new Path()
+      .move(points.pocketFacingTL)
+      .curve(points.tlCPbr, points.brCPtl, points.pocketFacingBR)
       .hide()
-  else paths.sideSeam = paths.sideSeam.split(points.pocketFacingBR)[1].hide()
 
-  points.brCPtl = points.pocketFacingBR.shift(
-    points.pocketFacingBR.angle(points.pocketSide) + 90,
-    halfInch * 3
-  )
-  points.tlCPbr = points.pocketFacingTL.shift(
-    points.pocketFacingTL.angle(points.pocketWaist) - 90,
-    halfInch * 6
-  )
+    paths.seam = paths.waistSeam
+      .join(paths.facingInside)
+      .join(paths.sideSeam)
+      .close()
+      .attr('class', 'fabric')
 
-  paths.facingInside = new Path()
-    .move(points.pocketFacingTL)
-    .curve(points.tlCPbr, points.brCPtl, points.pocketFacingBR)
-    .hide()
-
-  paths.seam = paths.waistSeam
-    .join(paths.facingInside)
-    .join(paths.sideSeam)
-    .close()
-    .attr('class', 'fabric')
-
-  if (complete) {
     snippets.n1 = new Snippet('notch', points.pocketWaist)
     snippets.n2 = new Snippet('notch', points.pocketSide)
 
+    points.gridAnchor = points.pU.clone()
+
     points.logo = points.pUcpA.shiftFractionTowards(points.pocketFacingTL, 0.5).shift(270, 30)
     snippets.logo = new Snippet('logo', points.logo)
-    points.title = points.logo.shift(270, 50)
+    points.title = points.logo.shiftFractionTowards(points.pocketFacingBR, 0.35)
     macro('title', {
       nr: 3,
       at: points.title,
       title: 'PocketFacing',
+      align: 'center',
     })
 
     macro('grainline', {
@@ -70,13 +62,20 @@ function draftCorneliusPocketFacing({
       to: new Point(points.pU.x, points.title.y),
     })
 
+    store.cutlist.addCut({ cut: 2, from: 'fabric' })
+
     if (sa) {
       paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa')
     }
-  }
 
-  // Paperless?
-  if (paperless) {
+    macro('hd', {
+      id: 0,
+      from: points.pocketSide,
+      to: points.pocketFacingBR,
+      y: points.pU.y - sa - 15,
+      noStartMarker: true,
+      noEndMarker: true,
+    })
     macro('hd', {
       id: 1,
       from: points.pU,
@@ -85,14 +84,14 @@ function draftCorneliusPocketFacing({
     })
     macro('hd', {
       id: 2,
-      from: points.pU,
-      to: points.pocketWaist,
+      from: points.pocketWaist,
+      to: points.pU,
       y: points.pU.y - sa - 15,
     })
     macro('hd', {
       id: 3,
-      from: points.pocketWaist,
-      to: points.pocketFacingTL,
+      from: points.pocketFacingTL,
+      to: points.pocketWaist,
       y: points.pU.y - sa - 15,
     })
     macro('vd', {
@@ -107,13 +106,7 @@ function draftCorneliusPocketFacing({
       to: points.pocketFacingBR,
       x: points.pocketSide.x + sa + 15,
     })
-  }
 
-  return part
-}
-
-export const pocketFacing = {
-  name: 'cornelius.pocketFacing',
-  from: frontpoints,
-  draft: draftCorneliusPocketFacing,
+    return part
+  },
 }
