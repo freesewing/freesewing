dd13c15195c4ab16638551e8aeb1bee968619372
Joost De Cock <joost@joost.at>
2023-10-12 11:30:52 +0200

    chore(shared): Use ReadMore for navlinks

--- sites/shared/components/navigation/sitenav.mjs
+++ sites/shared/components/navigation/sitenav.mjs
@@ -9,23 +9,10 @@ import { Link, PageLink } from 'shared/components/link.mjs'
 import orderBy from 'lodash.orderby'
 import { icons } from 'shared/components/navigation/primary.mjs'
 import { useTranslation } from 'next-i18next'
+import { BaseAccordion } from 'shared/components/accordion.mjs'
+import { ReadMore } from 'shared/components/mdx/read-more.mjs'
 
 export const ns = ['sections']
-/*
- * This returns only those children that are expected to show up
- * in the side navigation. Specifically:
- *  - Key length needs to be longer than 1
- *  - Child pages cannot have m or h set (main section or hidden)
- *  - Title may not be 'spacer' (header spacer)
- *
- *  It also takes care of ordering them, and returns an array
- *
- * @params tree {object} - A navigation object as returned by useNavigation => siteNav
- */
-const onlyValidChildren = (tree) =>
-  orderBy(tree, ['o', 't'], ['asc', 'asc']).filter(
-    (entry) => typeof entry === 'object' && entry.t !== 'spacer' && !entry.m && !entry._ && !entry.h
-  )
 
 /*
  * This returns only those children that are main sections. Specifically:
@@ -39,96 +26,6 @@ const onlyValidChildren = (tree) =>
  */
 const onlyMainSections = (tree) =>
   orderBy(tree, ['o', 't'], ['asc', 'asc']).filter((entry) => entry.m)
-
-const SectionLink = ({ skey, tree, slug }) =>
-  tree[skey]._ ? null : tree[skey].s === slug ? ( // Underscore means always hide
-    <>
-      <span className="pl-2 border-l-2 py-2 block w-full border-secondary bg-opacity-10">
-        {tree[skey].t}
-      </span>
-      {pageHasChildren(tree[skey]) && <Section tree={tree[skey]} slug={slug} />}
-    </>
-  ) : isSlugPart(tree[skey].s, slug) ? (
-    <>
-      <Link
-        href={`/${tree[skey].s}`}
-        className="pl-2 border-l-2 py-2 block w-full hover:border-secondary hover:bg-secondary hover:bg-opacity-10"
-      >
-        {tree[skey].t}
-      </Link>
-      {pageHasChildren(tree[skey]) && <Section tree={tree[skey]} slug={slug} />}
-    </>
-  ) : (
-    <Link
-      href={`/${tree[skey].s}`}
-      className="pl-2 border-l-2 py-2 block w-full hover:border-secondary hover:bg-secondary hover:bg-opacity-10"
-    >
-      {tree[skey].t}
-    </Link>
-  )
-
-/*
- * A React component to render a section of the navigation
- *
- * @param t {string}    - The section title
- * @param s {string}    - The section slug
- * @param tree {object} - The object describing any futher child pages
- * @param slug {string} - The slug of the currently active/viewed page
- */
-const Section = ({
-  tree, // Object with the navigation
-  slug, // Slug of the current page (used to make links active)
-}) => (
-  <ul className="ml-4">
-    {onlyValidChildren(tree).map((page, i) => (
-      <li key={i}>
-        {slug === page.s ? (
-          <>
-            <span
-              className={
-                'pl-2 font-medium border-l-2 py-2 block w-full border-secondary ' +
-                'bg-secondary bg-opacity-10'
-              }
-            >
-              {page.t}
-            </span>
-            {pageHasChildren(page) && <Section tree={page} slug={slug} />}
-          </>
-        ) : (
-          <SectionLink {...{ skey: page.s.split('/').pop(), tree, slug }} />
-        )}
-      </li>
-    ))}
-  </ul>
-)
-
-/*
- * A React component to render a main link navigation
- *
- * @param t {string}    - The section title
- * @param s {string}    - The section slug
- */
-const MainLink = ({
-  t, // The link title/text
-  s, // The link slug
-  slug, // The current page slug
-}) => {
-  const classes =
-    '' +
-    'break-all py-2 px-2 block w-full font-bold text-lg ' +
-    'flex flex-row items-start gap-0.5 lg:gap-1 border-l-2'
-
-  return s === slug ? (
-    <span className={`${classes} border-secondary bg-secondary bg-opacity-10`}>{t}</span>
-  ) : (
-    <Link
-      href={`/${s}`}
-      className={`${classes} border-transparent hover:border-secondary hover:bg-secondary hover:bg-opacity-10`}
-    >
-      {t}
-    </Link>
-  )
-}
 
 /*
  * A React component to render breadcrumbs to the current page
@@ -197,44 +94,13 @@ export const Breadcrumbs = ({ lead = false }) => {
 
 /*
  * A React component to render sidebar navigation based on the siteNav object and current slug
- *
- * The main sections are determined in the navigation prebuild code.
- * We always display the navigation as:
- *   - Always show all top-level entries
- *   - Always show all direct children of all top-level entries (this allows for better discoverability)
- *   - If we're deeper down, only expand the active page
+ * It just re-uses the ReadMore component as we provide to MDX content
  */
-export const NavLinks = () => {
-  // Grab siteNav and slug from the navigation context
-  const { siteNav, slug } = useContext(NavigationContext)
-
-  let subtree = siteNav
-  /*
-   * FreeSewing.org has a lot of content, especially the design docs get nested rather deep
-   * So we trim the navigation tree so that the designs content is not overwhelming
-   */
-  if (siteConfig.tld === 'org') {
-    const chunks = slug.split('/')
-    if (chunks[0] === 'docs') {
-      if (chunks.length > 3) {
-        if (chunks[1] === 'designs') subtree = siteNav.docs.designs[chunks[2]]
-      }
-      // If nothing matched, restrict it to the docs root
-      if (subtree.blog) subtree = siteNav.docs
-    }
-  }
-
-  return (
-    <ul className="w-full list mb-8 mt-3">
-      {onlyValidChildren(subtree).map((page, i) => (
-        <li key={i} className="w-full">
-          <MainLink s={page.s} t={page.t} slug={slug} />
-          {pageHasChildren(page) && !page.n && <Section {...{ tree: page, slug }} />}
-        </li>
-      ))}
-    </ul>
-  )
-}
+export const NavLinks = () => (
+  <div className="mdx my-4 bg">
+    <ReadMore recurse />
+  </div>
+)
 
 /*
  * A React component to render sidebar navigation for the main sections
