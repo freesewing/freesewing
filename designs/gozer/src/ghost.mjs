927e6d91652965b6852762d04e35b2e788c51113
woutervdub <wouter.vdub@yahoo.com>
2023-10-17 18:45:29 +0000

    Cleanup

--- designs/gozer/src/ghost.mjs
+++ designs/gozer/src/ghost.mjs
@@ -1,5 +1,3 @@
-import { cbqc } from '@freesewing/core'
-
 export const ghost = {
   name: 'gozer.ghost',
   measurements: ['hpsToWaistBack', 'waistToFloor', 'head'],
@@ -8,47 +6,29 @@ export const ghost = {
     const size =
       measurements.hpsToWaistBack + measurements.waistToFloor + measurements.head / Math.PI
 
-    points.middle = new Point(0, 0)
+    points.middle = new Point(0, 0).addCircle(size, 'fabric')
+
     points.eyeLine = points.middle.shift(270, measurements.head / Math.PI / 2)
     points.eyeLeft = points.eyeLine.shift(180, measurements.head * 0.13)
     points.eyeRight = points.eyeLine.shift(0, measurements.head * 0.13)
-    points.top = new Point(0, -1 * size)
     points.left = new Point(-1 * size, 0)
     points.right = new Point(size, 0)
-    points.bottom = new Point(0, size)
-    points.topCp1 = points.top.shift(180, size * cbqc)
-    points.topCp2 = points.top.shift(0, size * cbqc)
-    points.leftCp1 = points.left.shift(270, size * cbqc)
-    points.leftCp2 = points.left.shift(90, size * cbqc)
-    points.bottomCp1 = points.bottom.shift(0, size * cbqc)
-    points.bottomCp2 = points.bottom.shift(180, size * cbqc)
-    points.rightCp1 = points.right.shift(90, size * cbqc)
-    points.rightCp2 = points.right.shift(270, size * cbqc)
 
     points.eyeLeft.addCircle(eyeSize)
     points.eyeRight.addCircle(eyeSize)
 
-    paths.seam = new Path()
-      .move(points.top)
-      .curve(points.topCp1, points.leftCp2, points.left)
-      .curve(points.leftCp1, points.bottomCp2, points.bottom)
-      .curve(points.bottomCp1, points.rightCp2, points.right)
-      .curve(points.rightCp1, points.topCp2, points.top)
-      .close()
-      .addClass('fabric')
-
-    points.title = points.middle.shiftFractionTowards(points.bottom, 0.65)
+    points.title = points.middle.shift(270, size * 0.65)
     macro('title', {
       nr: 1,
       at: points.title,
       title: 'Gozer',
       align: 'center',
     })
-    points.logo = points.middle.shiftFractionTowards(points.bottom, 0.5)
+    points.logo = points.middle.shift(270, size * 0.5)
     snippets.logo = new Snippet('logo', points.logo)
 
     if (sa) {
-      paths.sa = paths.seam.offset(sa).addClass('fabric sa')
+      points.sa = new Point(0, 0).addCircle(size + sa, 'fabric sa')
     }
 
     macro('hd', {
