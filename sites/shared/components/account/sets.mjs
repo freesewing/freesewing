82c119e6171ac1262173cab0486cdd71792046f8
joostdecock <joost@joost.at>
2023-10-22 19:13:00 +0200

    fix(shared): Old URLs used in router.push

--- sites/shared/components/account/sets.mjs
+++ sites/shared/components/account/sets.mjs
@@ -73,7 +73,7 @@ export const NewSet = () => {
     const result = await backend.createSet({ name })
     if (result.success) {
       setLoadingStatus([true, t('nailedIt'), true, true])
-      router.push(`/account/sets/${result.data.set.id}`)
+      router.push(`/account/set?id=${result.data.set.id}`)
     } else setLoadingStatus([true, 'backendError', true, false])
   }
 
