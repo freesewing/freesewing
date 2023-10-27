a25df39508d42aebbbe90eeec48ec7a6968673fd
Soham Tembhurne <82658685+sohamtembhurne@users.noreply.github.com>
2023-10-28 00:52:36 +0530

    fix line spacing

--- plugins/plugin-annotations/src/logo.mjs
+++ plugins/plugin-annotations/src/logo.mjs
@@ -5,8 +5,6 @@ export const logoDefs = [
   {
     name: 'logo',
     def: (scale) =>
-      `<g id="logo" transform="scale(${
-        2 * scale
-      }) translate(-23 -36)"><path class="logo" stroke="none" fill="currentColor" d="${logoPath}" /></g>`,
+      `<g id="logo" transform="scale(${2*scale}) translate(-23 -36)"><path class="logo" stroke="none" fill="currentColor" d="${logoPath}"/></g>`,
   },
 ]
