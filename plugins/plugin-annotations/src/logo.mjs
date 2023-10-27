84f1befd83ce1d59eb3c8523dae683a71fbd67df
sohamtembhurne <sohamatembhurne@outlook.com>
2023-10-28 00:38:11 +0530

    logo.mjs: fix transform issues

--- plugins/plugin-annotations/src/logo.mjs
+++ plugins/plugin-annotations/src/logo.mjs
@@ -5,6 +5,8 @@ export const logoDefs = [
   {
     name: 'logo',
     def: (scale) =>
-      `<g id="logo" transform="scale(${scale}) translate(-23 -36)"><path class="logo" stroke="none" fill="currentColor" d=${logoPath} /></g>`,
+      `<g id="logo" transform="scale(${
+        2 * scale
+      }) translate(-23 -36)"><path class="logo" stroke="none" fill="currentColor" d="${logoPath}" /></g>`,
   },
 ]
