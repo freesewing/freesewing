cde93b4e615b717101fc3ba697996216155ad244
Natalia Sayang <nareva@gmail.com>
2023-11-17 22:24:14 +0000

    Adding Bee line drawing as react component

--- sites/shared/components/designs/linedrawings/index.mjs
+++ sites/shared/components/designs/linedrawings/index.mjs
@@ -1,5 +1,6 @@
 import { Aaron, AaronFront, AaronBack } from 'shared/components/designs/linedrawings/aaron.mjs'
 import { Albert, AlbertFront } from 'shared/components/designs/linedrawings/albert.mjs'
+import { Bee, BeeFront } from 'shared/components/designs/linedrawings/bee.mjs'
 import { Bruce, BruceFront, BruceBack } from 'shared/components/designs/linedrawings/bruce.mjs'
 import { Simon, SimonFront, SimonBack } from 'shared/components/designs/linedrawings/simon.mjs'
 import { Wahid, WahidFront, WahidBack } from 'shared/components/designs/linedrawings/wahid.mjs'
@@ -7,6 +8,7 @@ import { Wahid, WahidFront, WahidBack } from 'shared/components/designs/linedraw
 export const lineDrawingsFront = {
   aaron: AaronFront,
   albert: AlbertFront,
+  bee: BeeFront,
   bruce: BruceFront,
   simon: SimonFront,
   wahid: WahidFront,
@@ -22,6 +24,7 @@ export const lineDrawingsBack = {
 export const lineDrawings = {
   aaron: Aaron,
   albert: Albert,
+  bee: Bee,
   bruce: Bruce,
   simon: Simon,
   wahid: Wahid,
