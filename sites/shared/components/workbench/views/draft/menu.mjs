ae9637e50dc0c877cb01372c4d37de9f6cf0b1cf
joostdecock <joost@joost.at>
2023-11-18 18:53:39 +0100

    chore: Remove lint

--- sites/shared/components/workbench/views/draft/menu.mjs
+++ sites/shared/components/workbench/views/draft/menu.mjs
@@ -1,5 +1,4 @@
 //  __SDEFILE__ - This file is a dependency for the stand-alone environment
-import { loadSettingsConfig as loadUiSettingsConfig } from 'shared/components/workbench/menus/ui-settings/config.mjs'
 import {
   DesignOptions,
   ns as designMenuNs,
@@ -45,7 +44,6 @@ export const DraftMenu = ({
     account,
     control,
   }
-  const uiSettingsConfig = loadUiSettingsConfig()
 
   const sections = [
     {
