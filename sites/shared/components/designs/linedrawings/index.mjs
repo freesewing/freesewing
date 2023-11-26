7973d7e26dcf52c76f4795ec3ba0ddd2e8a7eff8
Natalia Sayang <nareva@gmail.com>
2023-11-26 18:54:03 +0000

    Add Benjamin, Bent, and Bob line drawings

--- sites/shared/components/designs/linedrawings/index.mjs
+++ sites/shared/components/designs/linedrawings/index.mjs
@@ -1,6 +1,9 @@
 import { Aaron, AaronFront, AaronBack } from 'shared/components/designs/linedrawings/aaron.mjs'
 import { Albert, AlbertFront } from 'shared/components/designs/linedrawings/albert.mjs'
 import { Bella, BellaFront, BellaBack } from 'shared/components/designs/linedrawings/bella.mjs'
+import { Benjamin, BenjaminFront } from 'shared/components/designs/linedrawings/benjamin.mjs'
+import { Bent, BentFront, BentBack } from 'shared/components/designs/linedrawings/bent.mjs'
+import { Bob, BobFront, BobBack } from 'shared/components/designs/linedrawings/bob.mjs'
 import { Bruce, BruceFront, BruceBack } from 'shared/components/designs/linedrawings/bruce.mjs'
 import { Simon, SimonFront, SimonBack } from 'shared/components/designs/linedrawings/simon.mjs'
 import { Wahid, WahidFront, WahidBack } from 'shared/components/designs/linedrawings/wahid.mjs'
@@ -9,6 +12,9 @@ export const lineDrawingsFront = {
   aaron: AaronFront,
   albert: AlbertFront,
   bella: BellaFront,
+  benjamin: BenjaminFront,
+  bent: BentFront,
+  bob: BobFront,
   bruce: BruceFront,
   simon: SimonFront,
   wahid: WahidFront,
@@ -17,6 +23,8 @@ export const lineDrawingsFront = {
 export const lineDrawingsBack = {
   aaron: AaronBack,
   bella: BellaBack,
+  bent: BentBack,
+  bob: BobBack,
   bruce: BruceBack,
   simon: SimonBack,
   wahid: WahidBack,
@@ -26,6 +34,9 @@ export const lineDrawings = {
   aaron: Aaron,
   albert: Albert,
   bella: Bella,
+  benjamin: Benjamin,
+  bent: Bent,
+  bob: Bob,
   bruce: Bruce,
   simon: Simon,
   wahid: Wahid,
