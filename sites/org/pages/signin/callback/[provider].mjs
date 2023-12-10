210aa93d5c4401f8b1a762717d79dc4188a9a1ac
joostdecock <joost@joost.at>
2023-12-10 12:32:49 +0100

    chore: Reconfigure & prebuild

--- sites/org/pages/signin/callback/[provider].mjs
+++ sites/org/pages/signin/callback/[provider].mjs
@@ -36,6 +36,7 @@ const OauthCallbackPage = ({ page, provider }) => {
       const state = urlParams.get('state')
       const code = urlParams.get('code')
       const result = await backend.oauthSignIn({ state, code, provider })
+      console.log({ state, code, result })
       if (result.data?.account && result.data?.token) {
         setAccount(result.data.account)
         setToken(result.data.token)
