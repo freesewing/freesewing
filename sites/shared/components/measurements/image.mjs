3bca940c268c5e1258440fba45b4327b2f1d78d6
joostdecock <joost@joost.at>
2024-01-14 14:29:48 +0100

    fix(shared): Keep in valid props out of image tag
    This removes the `blurHeight` and `blurWidth` props from the image tag
    in the MeasieImage component.
    
    These were added by the webpack SVG loader, and then spread into the
    image.
    
    For background, see: https://github.com/vercel/next.js/issues/56511#issuecomment-1890953467
    
    This closes #5804

--- sites/shared/components/measurements/image.mjs
+++ sites/shared/components/measurements/image.mjs
@@ -27,21 +27,25 @@ export const MeasieImage = (props) => {
   if (!m) return null
 
   const pose = seated.includes(m) ? 'seated' : 'standing'
-
+  console.log({ img: sarahImages[m] })
   return (
     <Tabs tabs="Sarah, Tim">
-      <Tab>
+      <Tab tabId="Sarah">
         <img
           className="shadow aspect-[4/3]"
-          {...sarahImages[m]}
+          height={sarahImages[m].height}
+          width={sarahImages[m].height}
+          src={sarahImages[m].height}
           alt={t('measurements:' + m)}
           style={{ ...style, backgroundImage: `url(/img/sarah-${pose}.jpg)` }}
         />
       </Tab>
-      <Tab>
+      <Tab tabId="Tim">
         <img
           className="shadow"
-          {...timImages[m]}
+          height={timImages[m].height}
+          width={timImages[m].height}
+          src={timImages[m].height}
           alt={t('measurements:' + m)}
           style={{ ...style, backgroundImage: `url(/img/tim-${pose}.jpg)` }}
         />
