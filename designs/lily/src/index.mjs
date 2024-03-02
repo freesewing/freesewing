b5f91435d8d5c33289e996328eb625189b7e17ef
anna-puk <anna_puk@tux-orbital.org>
2024-03-02 20:49:15 +0000

    lily contents

--- designs/lily/src/index.mjs
+++ designs/lily/src/index.mjs
@@ -1,16 +1,16 @@
-//
-
 import { Design } from '@freesewing/core'
 import { i18n } from '../i18n/index.mjs'
 import { data } from '../data.mjs'
 // Parts
-import { box } from './box.mjs'
+import { front } from './front.mjs'
+import { back } from './back.mjs'
+import { waistband } from './waistband.mjs'
 
 // Create new design
 const Lily = new Design({
   data,
-  parts: [box],
+  parts: [front, back, waistband],
 })
 
 // Named exports
-export { box, i18n, Lily }
+export { front, back, waistband, i18n, Lily }
