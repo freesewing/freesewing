feb32b6cab82db091a4a561fe215d7fd240754ea
joostdecock <joost@joost.at>
2024-03-03 10:12:21 +0100

    fix(shared): Migrate to execa 5 named export

--- sites/shared/prebuild/git.mjs
+++ sites/shared/prebuild/git.mjs
@@ -1,4 +1,4 @@
-import execa from 'execa'
+import { execa } from 'execa'
 import { exec } from 'node:child_process'
 import { gitToAuthor, authors as authorInfo } from '../../../config/authors.mjs'
 import path from 'path'
