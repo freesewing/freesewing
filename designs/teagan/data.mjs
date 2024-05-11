21f386f56e1cfddfa3acdcc1ecb9d876b3f39679
joostdecock <joost@joost.at>
2024-05-11 17:30:26 +0200

    prerelease: v3.3.0-rc.1

--- designs/teagan/data.mjs
+++ designs/teagan/data.mjs
@@ -1,4 +1,4 @@
 // This file is auto-generated | All changes you make will be overwritten.
 export const name = '@freesewing/teagan'
-export const version = '3.2.0'
+export const version = '3.3.0-rc.1'
 export const data = { name, version }
