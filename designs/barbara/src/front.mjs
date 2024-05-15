444c04f9f791d3559a2251f355e15c21ac0f160d
lou <25495562+Futur3r@users.noreply.github.com>
2024-05-15 23:28:26 +0200

    added neckline curve to front

--- designs/barbara/src/front.mjs
+++ designs/barbara/src/front.mjs
@@ -4,6 +4,7 @@ export const front = {
     'underbust',
     'hpsToBust',
     'bustPointToUnderbust',
+    'highBustFront',
     'shoulderToShoulder',
     'shoulderSlope',
     'neck',
@@ -13,29 +14,37 @@ export const front = {
   options: {
     // Static
     underbustFactor: 0.25,
+    highBustFrontFactor: 0.5,
     // Fit
     underbustEase: { pct: 10, min: 0, max: 20, menu: 'fit' },
-    cleavageDepth: { pct: 25, min: 0, max: 75, menu: 'fit' },
     wingHeight: { pct: 80, min: 10, max: 100, menu: 'fit' },
     // Style
-    shoulderStrapPlacement: { pct: 30, min: 0, max: 90, menu: 'style' },
-    shoulderStrapWidth: { pct: 18, min: 10, max: 20, menu: 'style' },
+    necklineDrop: { pct: 25, min: 0, max: 75, menu: 'style' },
+    necklineBend: { pct: 100, min: 0, max: 100, menu: 'style' },
+    strapNecklineAngle: { pct: 0, min: 0, max: 100, menu: 'style' },
+    shoulderStrapPlacement: { pct: 30, min: 5, max: 90, menu: 'style' },
+    shoulderStrapWidth: { pct: 14, min: 10, max: 20, menu: 'style' },
   },
-  draft: ({ part, Path, Point, paths, points, options, measurements, macro }) => {
+  draft: ({ part, Path, Point, paths, points, options, measurements, macro, utils }) => {
     // Construct the bottom of the front
     points.wingBottom = new Point(0, 0)
-    points.wingTop = points.wingBottom.shift(
-      90,
-      (measurements.waistToArmpit - measurements.waistToUnderbust) * options.wingHeight
-    )
+    //points.wingTop = points.wingBottom.shift(
+    //  90,
+    //  (measurements.waistToArmpit - measurements.waistToUnderbust) * options.wingHeight
+    //)
     points.middleBottom = points.wingBottom.shift(
       0,
       measurements.underbust * options.underbustFactor
     )
     points.middleTop = points.middleBottom.shift(
       90,
-      measurements.bustPointToUnderbust + measurements.hpsToBust * options.cleavageDepth
+      measurements.bustPointToUnderbust + measurements.hpsToBust * options.necklineDrop
     )
+    points.armpit = new Point(
+      points.middleBottom.x - measurements.highBustFront * options.highBustFrontFactor,
+      points.wingBottom.y - (measurements.waistToArmpit - measurements.waistToUnderbust) //* options.wingHeight
+    )
+    points.wingTop = points.wingBottom.shiftFractionTowards(points.armpit, options.wingHeight)
     // Construct the shoudler strap from the edge of the neck
     points.neckLeft = points.middleBottom.translate(
       -(measurements.neck / (2 * 3.14)),
@@ -51,12 +60,30 @@ export const front = {
       -(measurements.shoulderToShoulder / 2 - points.neckLeft.dx(points.middleBottom)) *
         options.shoulderStrapWidth
     )
+    // Construct the chest curve
+    points.necklineCorner = utils.beamsIntersect(
+      points.strapRight,
+      points.strapLeft.rotate(
+        90 - measurements.shoulderSlope * options.strapNecklineAngle,
+        points.strapRight
+      ),
+      points.middleTop,
+      points.middleBottom.rotate(-90, points.middleTop)
+    )
+    points.middleTopCp1 = points.middleTop.shiftFractionTowards(
+      points.necklineCorner,
+      options.necklineBend
+    )
+    points.strapRightCp2 = points.strapRight.shiftFractionTowards(
+      points.necklineCorner,
+      options.necklineBend
+    )
 
     paths.test = new Path()
       .move(points.wingBottom)
       .line(points.middleBottom)
       .line(points.middleTop)
-      .line(points.strapRight)
+      .curve(points.middleTopCp1, points.strapRightCp2, points.strapRight)
       .line(points.strapLeft)
       .line(points.wingTop)
       .close()
