787cea54538152bc641e2f9fdeedb8111ff0b244
Joost De Cock <joost@joost.at>
2024-09-28 14:51:30 +0200

    fix(dev): Use Link component for local links

--- sites/dev/src/pages/index.js
+++ sites/dev/src/pages/index.js
@@ -1,8 +1,8 @@
-import Link from '@docusaurus/Link';
-import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link'
+import Layout from '@theme/Layout'
 import NewDesign from './_new-design.md'
 import SetupMonorepo from './_setup-monorepo.md'
-import MDXContent from '@theme/MDXContent';
+import MDXContent from '@theme/MDXContent'
 
 const styles = {
   top: {
@@ -11,7 +11,7 @@ const styles = {
     paddingTop: '2rem',
   },
   logo: {
-    maxWidth: '140px'
+    maxWidth: '140px',
   },
   cards: {
     display: 'flex',
@@ -47,7 +47,6 @@ const styles = {
   },
 }
 
-
 const Card = ({ title, icon, children }) => (
   <div style={styles.card}>
     <h3 style={styles.cardheading}>
@@ -65,71 +64,76 @@ export default function Home() {
       description="FreeSewing is an open source Javascript library for parametric sewing patterns"
     >
       <MDXContent>
-      <div style={styles.top}>
-        <img style={styles.logo} src="/img/logo.svg" />
-        <h1>FreeSewing</h1>
-        <h2>An open source Javascript library for parametric sewing patterns</h2>
-      </div>
-      <div style={styles.cards}>
-        <Card title="Custom-Fit Fashion" icon="👕">
-          <p style={styles.cardp}>FreeSewing is the leading open source library for on-demand garment manufacturing.</p>
-          <p style={styles.cardp}>
-            Loved by home sewers and fashion entrepreneurs alike,
-            we provide the tech stack for your creative endeavours.
-          </p>
-        </Card>
-        <Card title="Patterns as Code" icon="🤓">
-          <p style={styles.cardp}>
-            FreeSewing designs are implemented as code giving you unmatched power and flexibility.
-          </p>
-          <p style={styles.cardp}>
-            You can mix and match parts from different designs, extend them,
-            or add options that turn one base design into many.
-          </p>
-        </Card>
-        <Card title="To go fast, go alone" icon="🚀">
-          <p style={styles.cardp}>
-            All you need is <a href="https://nodejs.org/">NodeJS</a>, then run:
-          </p>
-          <NewDesign />
-          <p style={styles.cardp}>
-            This command will setup the stand-alone FreeSewing development environment for you. <a
-            href="/tutorials/getting-started-linux/dev-setup">Learn more</a>.
-          </p>
-        </Card>
-        <Card title="To go far, go together" icon="🧑‍🤝‍🧑">
-          <p style={styles.cardp}>
-            First, <a href="https://github.com/freesewing/freesewing/fork">fork our monorepo on GitHub
-            </a>, then run:
-          </p>
-          <SetupMonorepo />
-          <p style={styles.cardp}>
-            This will set up the FreeSewing monorepo on your local system.
-          </p>
-        </Card>
-        <Card title="Documentation for developers & contributors" icon="📖">
-          <p style={styles.cardp}>
-            We have <a
-              href="/guides/">Guides</a>, <a
-              href="/howtos/">Howtos</a>, <a
-              href="/tutorials/">Tutorials</a>, as well as <a
-              href="/reference/">Reference Documentation</a> covering the FreeSewing ecosystem.
-          </p>
-          <p style={styles.cardp}>
-            For documentation for makers, please refer to <a href="https://freesewing.org">FreeSewing.org</a>.
-          </p>
-        </Card>
-        <Card title="Git good scrub" icon="🐙">
-          <p style={styles.cardp}>
-            If you come from a maker background -- rather than a developer background -- learning to use <a
-            href="https://git-scm.com/">git</a> may seem like a daunting task.
-          </p>
-          <p style={styles.cardp}>
-            Fear not, we have created a <a href="/training/git/">git training course
-            </a> that will take you from zero to hero in a couple of hours.
-          </p>
-        </Card>
-      </div>
+        <div style={styles.top}>
+          <img style={styles.logo} src="/img/logo.svg" />
+          <h1>FreeSewing</h1>
+          <h2>An open source Javascript library for parametric sewing patterns</h2>
+        </div>
+        <div style={styles.cards}>
+          <Card title="Custom-Fit Fashion" icon="👕">
+            <p style={styles.cardp}>
+              FreeSewing is the leading open source library for on-demand garment manufacturing.
+            </p>
+            <p style={styles.cardp}>
+              Loved by home sewers and fashion entrepreneurs alike, we provide the tech stack for
+              your creative endeavours.
+            </p>
+          </Card>
+          <Card title="Patterns as Code" icon="🤓">
+            <p style={styles.cardp}>
+              FreeSewing designs are implemented as code giving you unmatched power and flexibility.
+            </p>
+            <p style={styles.cardp}>
+              You can mix and match parts from different designs, extend them, or add options that
+              turn one base design into many.
+            </p>
+          </Card>
+          <Card title="To go fast, go alone" icon="🚀">
+            <p style={styles.cardp}>
+              All you need is <a href="https://nodejs.org/">NodeJS</a>, then run:
+            </p>
+            <NewDesign />
+            <p style={styles.cardp}>
+              This command will setup the stand-alone FreeSewing development environment for you.{' '}
+              <Link href="/tutorials/getting-started-linux/dev-setup">Learn more</Link>.
+            </p>
+          </Card>
+          <Card title="To go far, go together" icon="🧑‍🤝‍🧑">
+            <p style={styles.cardp}>
+              First,{' '}
+              <a href="https://github.com/freesewing/freesewing/fork">
+                fork our monorepo on GitHub
+              </a>
+              , then run:
+            </p>
+            <SetupMonorepo />
+            <p style={styles.cardp}>
+              This will set up the FreeSewing monorepo on your local system.
+            </p>
+          </Card>
+          <Card title="Documentation for developers & contributors" icon="📖">
+            <p style={styles.cardp}>
+              We have <Link href="/guides/">Guides</Link>, <Link href="/howtos/">Howtos</Link>,{' '}
+              <Link href="/tutorials/">Tutorials</Link>, as well as{' '}
+              <Link href="/reference/">Reference Documentation</Link> covering the FreeSewing
+              ecosystem.
+            </p>
+            <p style={styles.cardp}>
+              For documentation for makers, please refer to{' '}
+              <a href="https://freesewing.org">FreeSewing.org</a>.
+            </p>
+          </Card>
+          <Card title="Git good scrub" icon="🐙">
+            <p style={styles.cardp}>
+              If you come from a maker background -- rather than a developer background -- learning
+              to use <a href="https://git-scm.com/">git</a> may seem like a daunting task.
+            </p>
+            <p style={styles.cardp}>
+              Fear not, we have created a <Link href="/training/git/">git training course</Link>{' '}
+              that will take you from zero to hero in a couple of hours.
+            </p>
+          </Card>
+        </div>
       </MDXContent>
     </Layout>
   )
--- sites/dev/src/theme/MDXComponents/popout.mjs
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Popout as PopoutBase } from '@freesewing/react-components/popout'
-
-export const Popout = PopoutBase
-
