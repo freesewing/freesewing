aacea9ddfe7b57dd735d9c5d9756965f2a56d0ea
Joost De Cock <joost@joost.at>
2024-09-28 14:55:33 +0200

    chore: Linter warnings

--- packages/react-components/src/popout.mjs
+++ packages/react-components/src/popout.mjs
@@ -1,8 +1,7 @@
 import React from 'react'
-import { mergeProps, isObject } from './utils.mjs'
+import { mergeProps } from './utils.mjs'
 import { Popout as SwizzledPopout } from './editor/swizzle/components/popout.mjs'
 import { CloseIcon } from './editor/swizzle/components/icons.mjs'
 import { t } from '#methods'
 
-export const Popout = (props) => <SwizzledPopout {...mergeProps( props, { CloseIcon } , { t })} />
-
+export const Popout = (props) => <SwizzledPopout {...mergeProps(props, { CloseIcon }, { t })} />
