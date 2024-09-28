58932564c4ea13202ef5aab025c6574b1f487af5
Joost De Cock <joost@joost.at>
2024-09-28 13:46:01 +0200

    fix(dev): Import plugins from source

--- sites/dev/docusaurus.config.mjs
+++ sites/dev/docusaurus.config.mjs
@@ -1,4 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
+import path from 'node:path'
+import { themes as prismThemes } from 'prism-react-renderer'
 
 const config = {
   title: 'FreeSewing',
@@ -16,14 +17,14 @@ const config = {
   plugins: [
     () => ({
       name: 'mjs-loader',
-      configureWebpack(config, isServer, utils) {
+      configureWebpack(config) {
         return {
           module: {
             rules: [
               {
                 test: /\.mjs$/,
                 include: /node_modules/,
-                type: "javascript/auto",
+                type: 'javascript/auto',
               },
               {
                 test: /\.mjs$/,
@@ -34,11 +35,49 @@ const config = {
                     presets: ['@babel/preset-react'],
                   },
                 },
-              }
-            ]
-          }
+              },
+            ],
+          },
         }
-      }
+      },
+    }),
+    () => ({
+      name: 'fs-loader',
+      configureWebpack(config) {
+        const fsConfig = {
+          resolve: { alias: {} },
+        }
+        // Load plugins from source, rather than compiled package
+        for (const plugin in [
+          'plugin-annotations',
+          'plugin-bin-pack',
+          'plugin-bust',
+          'plugin-flip',
+          'plugin-gore',
+          'plugin-i18n',
+          'plugin-measurements',
+          'plugin-mirror',
+          'plugin-ringsector',
+          'plugin-round',
+          'plugin-sprinkle',
+          'plugin-svgattr',
+          'plugin-theme',
+          'plugin-timing',
+          'plugin-versionfree-svg',
+        ]) {
+          fsConfig.resolve.alias[`@freesewing/${plugin}`] = path.resolve(
+            `../../plugins/${plugin}/src/index.mjs`
+          )
+        }
+        // Load these from source, rather than compiled package
+        for (const pkg of ['core', 'i18n', 'models', 'snapseries']) {
+          fsConfig.resolve.alias[`@freesewing/${pkg}`] = path.resolve(
+            `../../packages/${pkg}/src/index.mjs`
+          )
+        }
+
+        return fsConfig
+      },
     }),
   ],
 
@@ -47,7 +86,7 @@ const config = {
   presets: [
     [
       'classic',
-      ({
+      {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
@@ -56,94 +95,93 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
-      })
-    ]
+      },
+    ],
   ],
 
-  themeConfig:
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'FreeSewing.dev',
-        logo: {
-          alt: 'FreeSewing Logo',
-          src: 'img/logo-white.svg',
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'FreeSewing.dev',
+      logo: {
+        alt: 'FreeSewing Logo',
+        src: 'img/logo-white.svg',
+      },
+      items: [
+        { to: '/guides', label: 'Guides', position: 'left' },
+        { to: '/howtos', label: 'Howtos', position: 'left' },
+        { to: '/reference', label: 'Reference', position: 'left' },
+        { to: '/tutorials', label: 'Tutorials', position: 'left' },
+        {
+          href: 'https://freesewing.org/',
+          label: 'FreeSewing.org',
+          position: 'right',
         },
-        items: [
-          {to: '/guides', label: 'Guides', position: 'left'},
-          {to: '/howtos', label: 'Howtos', position: 'left'},
-          {to: '/reference', label: 'Reference', position: 'left'},
-          {to: '/tutorials', label: 'Tutorials', position: 'left'},
-          {
-            href: 'https://freesewing.org/',
-            label: 'FreeSewing.org',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'light',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              { label: 'Guides', to: '/guides' },
-              { label: 'Howtos', to: '/howtos' },
-              { label: 'Reference', to: '/reference' },
-              { label: 'Tutorials', to: '/tutorials' },
-            ],
-          },
-          {
-            title: 'Help & Support',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.freesewing.org/',
-              },
-              {
-                label: 'GitHub Issues',
-                href: 'https://github.com/freesewing/freesewing/issues',
-              },
-              {
-                label: 'GitHub Discussions',
-                href: 'https://github.com/freesewing/freesewing/discussions',
-              },
-              {
-                label: 'All Support Options',
-                href: 'https://freesewing.org/support',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'FreeSewing.org',
-                to: 'https://freesewing.org/',
-              },
-              {
-                label: 'Blog',
-                to: 'https://freesewing.org/blog/',
-              },
-              {
-                label: 'Showcase',
-                to: 'https://freesewing.org/showccase/',
-              },
-              {
-                label: 'Code on GitHub',
-                href: 'https://github.com/freesewing/freesewing',
-              },
-            ],
-          },
-        ],
-        copyright: `<a href="https://freesewing.org/">FreeSewing</a> is brought to you by <a href="https://github.com/joostdecock">Joost De Cock</a> and <a href="https://github.com/freesewing/freesewing/blob/develop/CONTRIBUTORS.md">contributors</a> with the financial support of <a href="https://freesewing.org/patrons/join">our patrons</a>`,
-      },
-      prism: {
-        theme: prismThemes.dracula,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+      ],
+    },
+    footer: {
+      style: 'light',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            { label: 'Guides', to: '/guides' },
+            { label: 'Howtos', to: '/howtos' },
+            { label: 'Reference', to: '/reference' },
+            { label: 'Tutorials', to: '/tutorials' },
+          ],
+        },
+        {
+          title: 'Help & Support',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.freesewing.org/',
+            },
+            {
+              label: 'GitHub Issues',
+              href: 'https://github.com/freesewing/freesewing/issues',
+            },
+            {
+              label: 'GitHub Discussions',
+              href: 'https://github.com/freesewing/freesewing/discussions',
+            },
+            {
+              label: 'All Support Options',
+              href: 'https://freesewing.org/support',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'FreeSewing.org',
+              to: 'https://freesewing.org/',
+            },
+            {
+              label: 'Blog',
+              to: 'https://freesewing.org/blog/',
+            },
+            {
+              label: 'Showcase',
+              to: 'https://freesewing.org/showccase/',
+            },
+            {
+              label: 'Code on GitHub',
+              href: 'https://github.com/freesewing/freesewing',
+            },
+          ],
+        },
+      ],
+      copyright: `<a href="https://freesewing.org/">FreeSewing</a> is brought to you by <a href="https://github.com/joostdecock">Joost De Cock</a> and <a href="https://github.com/freesewing/freesewing/blob/develop/CONTRIBUTORS.md">contributors</a> with the financial support of <a href="https://freesewing.org/patrons/join">our patrons</a>`,
+    },
+    prism: {
+      theme: prismThemes.dracula,
+      darkTheme: prismThemes.dracula,
+    },
+  },
 }
 
 export default config
