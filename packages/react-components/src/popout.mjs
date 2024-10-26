76fbc5f6a480c5ea026666c869f10790d39d8fea
joostdecock <joost@joost.at>
2024-10-26 11:24:47 +0200

    chore: Dependency updates

--- packages/react-components/src/popout.mjs
+++ packages/react-components/src/popout.mjs
@@ -2,6 +2,7 @@ import React from 'react'
 import { mergeProps } from './utils.mjs'
 import { Popout as SwizzledPopout } from './editor/swizzle/components/popout.mjs'
 import { CloseIcon } from './editor/swizzle/components/icons.mjs'
-import { t } from '#methods'
+
+const t = (id) => id
 
 export const Popout = (props) => <SwizzledPopout {...mergeProps(props, { CloseIcon }, { t })} />
