8bfb2d3f65d1c71165b1e54b13b73598d2a0d848
joostdecock <joost@joost.at>
2024-11-02 10:48:11 +0100

    chore: Ported Tristan docs

--- sites/orgdocs/scripts/prebuild-step2.mjs
+++ sites/orgdocs/scripts/prebuild-step2.mjs
@@ -15,7 +15,7 @@ import { capitalize, optionsMenuStructure, optionType } from '../src/lib/utils.m
  * Run 'npm run prestart' to force it, or it will happen automatically
  * when you run `npm run start`
  */
-const skip = ['charlie', 'diana', 'holmes', 'huey', 'jane', 'lucy', 'titan', 'tristan', 'waralee']
+const skip = ['charlie', 'diana', 'holmes', 'huey', 'jane', 'lucy', 'titan', 'waralee']
 
 /*
  * mkdir helper
