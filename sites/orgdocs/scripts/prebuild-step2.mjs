9511126b6fe4d23d154222e0768c27625d078285
Joost De Cock <joost@joost.at>
2024-11-02 12:04:11 +0100

    chore: Ported more design docs (#7223)
    This ports lucy, jane, and holmes

--- sites/orgdocs/scripts/prebuild-step2.mjs
+++ sites/orgdocs/scripts/prebuild-step2.mjs
@@ -15,7 +15,7 @@ import { capitalize, optionsMenuStructure, optionType } from '../src/lib/utils.m
  * Run 'npm run prestart' to force it, or it will happen automatically
  * when you run `npm run start`
  */
-const skip = ['charlie', 'diana', 'holmes', 'huey', 'jane', 'lucy', 'titan']
+const skip = ['charlie', 'diana', 'huey', 'titan']
 
 /*
  * mkdir helper
