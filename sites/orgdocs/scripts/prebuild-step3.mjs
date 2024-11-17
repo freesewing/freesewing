868f5d8a1e32a165fee4b7abe1cd17aaf13f50da
joostdecock <joost@joost.at>
2024-11-17 15:56:10 +0100

    fix: Auto-detect post authors

--- sites/orgdocs/scripts/prebuild-step3.mjs
+++ sites/orgdocs/scripts/prebuild-step3.mjs
@@ -1,58 +1,39 @@
 import fs from 'fs'
+import { glob } from 'glob'
+import matter from 'gray-matter'
 
-const authors = [
-  1, // joost
-  8, // reseved for anynymous users
-  132, // Wouter
-  292,
-  433, // Paul
-  840, // Eleonore
-  1972, // Ferdi
-  13050, // Karen
-  19867, // Natalia
-  20165, // Saber
-  20400, // Lex
-  20650, // Bobgeorge
-  21873, // Starfirebird
-  22007, // modern_dragon
-  22141, // comixminx
-  22693, // tuesday
-  22708, // Starfetch
-  23123, // Braveness
-  23364, // Renee-mariposa
-  23386, // Ramoth
-  24546, // MagicantAce
-  27042, // Guin
-  27667, // Talatrix
-  27669, // Gaelle
-  28322, // Zag
-  28514, // Halbmoki (thomas)
-  28838, // Ben
-  28890, // Jacques
-  29576, // Ojensen
-  29683, // JenBarb_
-  31287, // Laser
-  31336, // Vili
-  31761, // JoshShabtaiCa
-  31916, // dylanwhat
-  32339, // lmunar
-  32412, // petit_lutin
-  33070, // Thrunic
-  46488, // Nisaea
-  47151, // iderias
-  49445, // Galit
-  50461, // Vlad
-  50818, // RockerKitten
-  61987, // posh tunic
-  62144, // Tinker-Tim-The-Toolman-Taylor-Soldier-Spy
-  64919, // paula_
-  65187, // user-65187
-  71794, // Karlo
-  72337, // amaan hammadi
-  73391, // user 73391
-  83003, // AuxSuivants0
-  83912, // GursMan123
-]
+const loadTagsAndAuthors = async () => {
+  const lists = {
+    blog: await glob('./blog/*/index.mdx'),
+    newsletter: await glob('./newsletter/*/index.mdx'),
+    showcase: await glob('./showcase/*/index.mdx'),
+  }
+
+  const tags = []
+  const authors = []
+  for (const type of Object.keys(lists)) {
+    for (const file of lists[type]) {
+      const content = await fs.readFileSync(file, 'utf-8')
+      const data = matter(content)
+      if (data.data.tags) {
+        if (Array.isArray(data.data.tags)) tags.push(...data.data.tags)
+        else tags.push(data.data.tags)
+      } else if (type === 'showcase')
+        console.log('Missing design tag in showcase post:', { file, data: data.data })
+      if (data.data.authors) {
+        if (Array.isArray(data.data.authors)) authors.push(...data.data.authors)
+        else authors.push(data.data.authors)
+      } else if (type === 'showcase') console.warn('Missing authors in showcase post:', file)
+      else if (type === 'blog') console.warn('Missing authors in blog post:', file)
+      else if (type === 'newsletter') console.warn('Missing authors in newsletter edition:', file)
+    }
+  }
+
+  return {
+    authors: [...new Set(authors.sort())], // Make them unique
+    tags: [...new Set(tags.sort())], // Make them unique
+  }
+}
 
 const titles = {
   admin: 'FreeSewing Maintainer',
@@ -80,12 +61,16 @@ const loadUser = async (id) => {
 
 async function prebuild() {
   const all = {}
+  const { authors, tags } = await loadTagsAndAuthors()
   for (const author of authors) {
     const user = await loadUser(author)
     if (user.profile.id) all[user.profile.id] = userAsAuthor(user)
   }
-
   fs.writeFileSync(`./authors.json`, JSON.stringify(all, null, 2))
+  fs.writeFileSync(
+    `./showcase-tags.mjs`,
+    `export const tags = ${JSON.stringify([...new Set(tags.sort())])}`
+  )
 }
 
 prebuild()
