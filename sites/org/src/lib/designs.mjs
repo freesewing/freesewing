85fde4197526a62d00da82c9ef94ed6b8bf6b5b6
Jonathan Haas <haasjona@gmail.com>
2025-03-24 16:51:42 +0100

    Update paul

--- sites/org/src/lib/designs.mjs
+++ sites/org/src/lib/designs.mjs
@@ -3,8 +3,10 @@
  * Any changes you make will be overwritten by the prebuild step.
  */
 import { designInfo } from './designinfo.mjs'
+import * as paul from '@freesewing/paul'
 
 const designs = {
+  paul,
 }
 
 const designList = Object.keys(designs)
